d56d44701c57bb0a24b21d3badd315a54793a4d3
Caleb Jones <calebj516@gmail.com>
2021-04-07 20:49:53 -0500

    Price of Mangoes

--- Challenge.js
+++ Challenge.js
@@ -1,18 +1,14 @@
-// Pre-FizzBuzz Workout #1
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
 
-// This is the first step to understanding FizzBuzz.
+// mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
 
-// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
-
-// Your expected output is an array of positive integers from 1 to n (inclusive)
-// Your job is to write an algorithm that gets you from the input to the output.
-
-const preFizz = (n) => {
-  let arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
-  }
-  return arr;
+const mango = (quantity, price) => {
+  const discount = Math.floor(quantity / 3);
+  return (quantity - discount) * price;
 };
 
-console.log(preFizz(10));
+// First we calculate the number of mangos that are free by dividing the quantity by 3.
+// Math.floor returns the largest integer less than or equal the result of quantity / 3. This comes in handy for results that have a remainder.
+// Ex: 14 / 3 = 4.6667, math.floor changes this to 4.
+// Finally, we subtract the free mango(s) from the quantity and multiply by the price.
