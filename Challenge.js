8684432c4faa3035a7721a21197aef3dfa2c6528
Caleb Jones <calebj516@gmail.com>
2021-05-17 19:42:02 -0500

    Codewars: Extract the domain name from a URL

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,10 @@
-// Split Strings Challenge - 6 kyu
+// Extract the domain name from a URL
+// This uses the URL API, which is not allowed in this particular CodeWars challenge.
 
-// solution("abc"); // should return ['ab', 'c_']
-// solution("abcdef"); // should return ['ab', 'cd', 'ef']
+function domainName(url) {
+  let domain = new URL(url);
+  domain = domain.hostname.replace(/www.|.com/gi, "");
+  return domain;
+}
 
-const solution = (str) => {
-  // if there is no input, return empty array.
-  if (!str) {
-    return [];
-  }
-  // declare empty array to store results
-  let arr = [];
-  // test if the string length is even or odd
-  // if odd, append an underscore to the end of str
-  if (str.length % 2 !== 0) {
-    str += "_";
-  }
-  // this do while loop will push pairs from str into arr while index is less than str length
-  // first iteration: str[0] + str[1]
-  // second iteration i+2 = 0+2: str[2] + str[3]...
-  let i = 0;
-  do {
-    arr.push(`${str[i] + str[i + 1]}`);
-    i += 2;
-  } while (i < str.length);
-  return arr;
-};
-
-// Tests
-console.log(solution("abc"));
-console.log(solution("abcdef"));
+console.log(domainName("https://www.google.com"));
