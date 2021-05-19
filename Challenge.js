37bf0fc33226c94f02394dc67241d8d738f8cd70
Caleb Jones <calebj516@gmail.com>
2021-05-18 19:22:35 -0500

    Codewars Challenge: Small enough? Beginner

--- Challenge.js
+++ Challenge.js
@@ -1,10 +1,32 @@
-// Extract the domain name from a URL
-// This uses the URL API, which is not allowed in this particular CodeWars challenge.
+// Small enough? - Beginner Challenge (7 kyu)
 
-function domainName(url) {
-  let domain = new URL(url);
-  domain = domain.hostname.replace(/www.|.com/gi, "");
-  return domain;
-}
+// Instructions:
 
-console.log(domainName("https://www.google.com"));
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+const smallEnough = (a, limit) => {
+  // Declare variable to keep track of if an element is true
+  let flag = false;
+  // This do while loop loops through the array.
+  // If an element is greater than the limit, go ahead and return false.
+  // Otherwise, proceed to the next element and set the flag to true.
+  // If the loop exits we know that all elements have passed the test.
+  let i = 0;
+  do {
+    if (a[i] > limit) {
+      return false;
+    } else {
+      i++;
+      flag = true;
+    }
+  } while (i < a.length);
+  return flag;
+};
+
+// Tests
+console.log(smallEnough([66, 101], 200)); // true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)); // false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)); // true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)); // true
