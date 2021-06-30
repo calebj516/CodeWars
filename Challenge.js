45db30d05816b6ad74b0b19ceb4b928addf7917b
Caleb Jones <calebj516@gmail.com>
2021-06-30 17:59:11 -0500

    Codewars: Give Me A Diamond Challenge

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,92 @@
-// Challenge: String ends with? (7 kyu)
+// Challenge: Give Me A Diamond (6 kyu)
 
 // Instructions:
 
-// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
 
-// Examples:
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
 
-// solution('abc', 'bc') // returns true
-// solution('abc', 'd') // returns false
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
 
-// My Code Below
+// Examples
+// A size 3 diamond:
 
-function solution(str, ending) {
-  // Turn str and ending into arrays and reverse them
-  let strArr = str.split("").reverse();
-  let endingArr = ending.split("").reverse();
-  // loop through strArr, reversed, up to the length of endingArr
-  for (let i = 0; i < endingArr.length; i++) {
-    if (endingArr[i] !== strArr[i]) {
-      return false;
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+// A size 5 diamond:
+
+//   *
+//  ***
+// *****
+//  ***
+//   *
+// ...that is:
+
+// "  *\n ***\n*****\n ***\n  *\n"
+
+// My code below:
+
+function diamond(n) {
+  // Declare an array to hold the results
+  let result = [];
+  // If n is odd or less than zero, return null
+  if (n % 2 === 0 || n <= 0) {
+    return null;
+    // Otherwise, proceed with the challenge since n is odd.
+  } else {
+    let asterisk = "*";
+    let space = " ";
+    let count = 0;
+    let i = 1;
+    let num = n;
+    // This while loop counts the number of times it takes to get to 1 from n.
+    // This will be used to determine the number of spaces to add in front of the newline character.
+    while (num > 1) {
+      num -= 2;
+      count += 1;
     }
+    // This while loop adds spaces and asterisks up to n
+    while (i < n) {
+      result.push(space.repeat(count) + asterisk.repeat(i) + "\n");
+      i += 2;
+      count -= 1;
+    }
+    // This while loop adds spaces and asterisks down from n to 1
+    while (i >= 1) {
+      result.push(space.repeat(count) + asterisk.repeat(i) + "\n");
+      i -= 2;
+      count += 1;
+    }
+    // Return string
+    return result.join("");
   }
-  return true;
 }
 
 // Tests below
 
-console.log(solution("abc", "bc")); // returns true
-console.log(solution("abc", "d")); // returns false
-console.log(solution("Codewars", "wars")); // returns true
+console.log(diamond(3));
+// returns:
+//  *
+// ***
+//  *
+console.log(diamond(5));
+// returns:
+//   *
+//  ***
+// *****
+//  ***
+//   *
+console.log(diamond(7));
+// returns:
+//   *
+//  ***
+// *****
+//*******
+// *****
+//  ***
+//   *
+console.log(diamond(8)); // returns null
