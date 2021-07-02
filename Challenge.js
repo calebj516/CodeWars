68a1b4cc2e15618da90ad32c3c3f6e13f3e783a5
Caleb Jones <calebj516@gmail.com>
2021-07-02 17:36:03 -0500

    Codewars: Complementary DNA Challenge

--- Challenge.js
+++ Challenge.js
@@ -1,32 +1,29 @@
-// Challenge: IQ Test (6 kyu)
+// Challenge: Complementary DNA (7 kyu)
 
 // Instructions:
 
-// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
-// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+// If you want to know more http://en.wikipedia.org/wiki/DNA
 
-// Examples:
-// iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-// iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+// More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+
+// DNAStrand ("ATTGC") // return "TAACG"
+
+// DNAStrand ("GTAT") // return "CATA"
 
 // My code below:
 
-function iqTest(numbers) {
-  // create an array with numbers
-  let nums = numbers.split(" ");
-  // test each element in the array, determining if the number is even or odd
-  const evenOrOdd = nums.map((n) => (n % 2 === 0 ? "Even" : "Odd"));
-  // Determine which element is different from the rest and return its position
-  let evenCount = 0;
-  let oddCount = 0;
-  evenOrOdd.map((n) => (n === "Even" ? evenCount++ : oddCount++));
-  // Return index of Even or Odd, depending on which one's count is equal to 1. Then add 1 to the index based on the instructions for this challenge.
-  return evenOrOdd.indexOf(evenCount === 1 ? "Even" : "Odd") + 1;
-}
+const DNAStrand = (dna) => {
+  return dna
+    .split("") // turn dna into an array
+    .map((el) => (el === "A" ? "T" : el === "T" ? "A" : el === "G" ? "C" : "G")) // nested ternary determines which letters are chosen
+    .join(""); // turn dna back into a string
+};
 
-// Tests below
-console.log(iqTest("2 4 7 8 10")); // Return 3. Third number is odd, while the rest of the numbers are even
+// Tests below:
 
-console.log(iqTest("1 2 1 1")); // Return 2. Second number is even, while the rest of the numbers are odd
+console.log(DNAStrand("ATTGC")); // return "TAACG"
+console.log(DNAStrand("GTAT")); // return "CATA"
