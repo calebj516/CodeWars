8652ec06b26941a085b83d1d443b7ba5297898bc
Caleb Jones <calebj516@gmail.com>
2021-07-18 18:46:28 -0500

    Codewars: Word Abbreviation

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,43 @@
-// Challenge: Factorial (7 kyu)
+// Challenge: Word a10n (abbreviation) (6 kyu)
 
 // Instructions:
 
-// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+// The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
 
-// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+// Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
 
-// More details about factorial can be found here.
+// A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+// The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
 
 // My code below:
 
-const factorial = (n) => {
-  if (n > 12 || n < 0) {
-    throw new RangeError("Number is invalid!");
-  } else if (n <= 1) {
-    return 1;
-  } else {
-    let result = 1;
-    for (let i = 1; i <= n; i++) {
-      result *= i;
-    }
-    return result;
-  }
-};
+function abbreviate(string) {
+  // Explanation of line 16:
+  // string.replace: on each match with the regex, call the function match
+  return string.replace(/[A-Za-z]{4,}/gi, function (match) {
+    // Explanation of line 21:
+    // match[0] returns the first char
+    // (match.length - 2) returns the number of removed chars
+    // match.slice(-1) returns the final character in the match
+    return match[0] + (match.length - 2) + match.slice(-1);
+  });
+}
+
+// Explanation of regex on line 16 (courtesy of https://regex101.com/):
+
+// /[A-Za-z]{4,}/gi
+
+// Match a single character present in the list below [A-Za-z]
+// {4,} matches the previous token between 4 and unlimited times, as many times as possible, giving back as needed (greedy)
+// A-Z matches a single character in the range between A (index 65) and Z (index 90) (case insensitive)
+// a-z matches a single character in the range between a (index 97) and z (index 122) (case insensitive)
+// Global pattern flags
+// g modifier: global. All matches (don't return after first match)
+// i modifier: insensitive. Case insensitive match (ignores case of [a-zA-Z])
 
 // Tests below:
-
-console.log(factorial(5)); // 120
-console.log(factorial(0)); // 1
-console.log(factorial(-2)); // RangeError
+console.log(abbreviate("elephant-ride")); // "e6t-r2e"
+console.log(abbreviate("elephant-rides are really fun!")); // "e6t-r3s are r4y fun!"
+console.log(abbreviate("internationalization")); // "i18n"
+console.log(abbreviate("accessibility")); // "a11y"
+console.log(abbreviate("Accessibility")); // "A11y"
