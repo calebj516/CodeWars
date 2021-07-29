2825ff28e71dec1f93f7c21d35b45877e6387c43
Caleb Jones <calebj516@gmail.com>
2021-07-28 19:06:04 -0500

    Codewars: Replace With Alphabet Position

--- Challenge.js
+++ Challenge.js
@@ -1,70 +1,30 @@
-// Challenge: The Vowel Code (6 kyu)
+// Challenge: Replace With Alphabet Position (6 kyu)
 
 // Instructions:
 
-// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
-// a -> 1
-// e -> 2
-// i -> 3
-// o -> 4
-// u -> 5
+// If anything in the text isn't a letter, ignore it and don't return it.
 
-// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+// "a" = 1, "b" = 2, etc.
 
-// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
-
-// For example, decode("h3 th2r2") would return "hi there".
-
-// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
 // My code below:
 
-function encode(string) {
-  // Pseudocode below:
-  // Split string into an array
-  // Use map to loop through each element
-  // Evaluate each element using nested ternary operators
-  return string
-    .split("")
-    .map((c) =>
-      c === "a"
-        ? 1
-        : c === "e"
-        ? 2
-        : c === "i"
-        ? 3
-        : c === "o"
-        ? 4
-        : c === "u"
-        ? 5
-        : c
-    )
-    .join("");
-}
-
-function decode(string) {
-  // Process is nearly identical to the encode function
-  // Note: using the equality operator ('==') as opposed to the strict equality operator. This will convert n to a number before making the comparison.
-  return string
-    .split("")
-    .map((n) =>
-      n == 1
-        ? "a"
-        : n == 2
-        ? "e"
-        : n == 3
-        ? "i"
-        : n == 4
-        ? "o"
-        : n == 5
-        ? "u"
-        : n
-    )
-    .join("");
+function alphabetPosition(text) {
+  // Remove all non-alphabet characters from text, and split into an array.
+  let textArr = text.replace(/[^a-zA-Z]/g, "").split("");
+  // Create an array containing the alphabet to use in the return statement
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  // We know that textArr only contains alplhabetic characters. So, return the index of each character plus one (to offset the index starting at 0, not 1) in a string.
+  return textArr
+    .map((char) => alphabet.indexOf(char.toLowerCase()) + 1)
+    .join(" ");
 }
 
 // Tests below:
 
-console.log(encode("hello")); // h2ll4
-console.log(decode("h2ll4")); // hello
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
