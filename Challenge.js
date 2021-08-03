3cb47c521e625ece73079db970181747db42a55c
Caleb Jones <calebj516@gmail.com>
2021-08-03 18:17:56 -0500

    Codewars: Data Reverse

--- Challenge.js
+++ Challenge.js
@@ -1,42 +1,47 @@
-// Challenge: Multiplication Tables (6 kyu)
+// Challenge: Data Reverse (6 kyu)
 
 // Instructions:
 
-// Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+// A stream of data is received and needs to be reversed.
 
-// Example:
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
 
-// multiplication_table(3,3)
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
 
-// 1 2 3
-// 2 4 6
-// 3 6 9
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+// The total number of bits will always be a multiple of 8.
 
-// -->[[1,2,3],[2,4,6],[3,6,9]]
+// The data is given in an array as such:
 
-// Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+// Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
 
 // My code below:
 
-function multiplicationTable(row, col) {
-  // Array to hold final result
-  let result = [];
-  for (let r = 1; r <= row; r++) {
-    // Array to hold result of one row
-    let arr = [];
-    for (let c = 1; c <= col; c++) {
-      arr.push(c * r);
+function dataReverse(data) {
+  if (data.length > 0) {
+    let result = [];
+    while (data.length) {
+      result.push(data.splice(0, 8));
     }
-    // Push result of one row to the result array and repeat
-    result.push(arr);
+    return result
+      .reverse()
+      .join(",")
+      .split(",")
+      .map((el) => Number(el));
   }
-  return result;
+  return [];
 }
 
 // Tests below:
 
-console.log(multiplicationTable(2, 2));
-console.log(multiplicationTable(3, 3));
-console.log(multiplicationTable(3, 4));
-console.log(multiplicationTable(4, 4));
-console.log(multiplicationTable(2, 5));
+console.log(
+  dataReverse([
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    0, 1, 0, 1, 0, 1, 0,
+  ])
+); // returns [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+console.log(dataReverse([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1])); // returns [0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0]
