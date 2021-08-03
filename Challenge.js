6318ec841f53fecd051fe270eb1fcd3aaccb1cf9
Caleb Jones <calebj516@gmail.com>
2021-08-02 20:10:14 -0500

    Codewars: Multiplication Tables

--- Challenge.js
+++ Challenge.js
@@ -1,47 +1,42 @@
-// Challenge: Highest Rank Number in an Array (6 kyu)
+// Challenge: Multiplication Tables (6 kyu)
 
 // Instructions:
 
-// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+// Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
 
-// Note: no empty arrays will be given.
+// Example:
 
-// Examples
-// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
-// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
-// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+// multiplication_table(3,3)
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// -->[[1,2,3],[2,4,6],[3,6,9]]
+
+// Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
 
 // My code below:
 
-function highestRank(arr) {
-  let mf = 1; // most frequent set to 1 because each element in the array appears once.
-  let m = 0; // this is a counter to track the number of appearances
-  let item; // this will hold the most frequent element
-  for (
-    // first for loop iterates over each element in arr
-    let i = 0;
-    i < arr.length;
-    i++
-  ) {
-    for (
-      // second for loop compares each element in arr to current value of arr[i]
-      let j = i;
-      j < arr.length;
-      j++
-    ) {
-      if (arr[i] == arr[j]) m++; // if the element in arr[i] appears again, increment the counter
-      if (mf < m) {
-        mf = m; // if counter m is greater than most frequent, assign value of m to most frequent.
-        item = arr[i]; // the current value of arr[i] being compared to each value in arr is the most frequent.
-      }
+function multiplicationTable(row, col) {
+  // Array to hold final result
+  let result = [];
+  for (let r = 1; r <= row; r++) {
+    // Array to hold result of one row
+    let arr = [];
+    for (let c = 1; c <= col; c++) {
+      arr.push(c * r);
     }
-    m = 0; // reset counter to 0, increment to next element in arr (first for loop), and compare it to each element (second for loop).
+    // Push result of one row to the result array and repeat
+    result.push(arr);
   }
-  return item; // at the end of the day item will hold the most frequent number; return it.
+  return result;
 }
 
 // Tests below:
 
-console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12])); // 12
-console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10])); // 12
-console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10])); // 3
+console.log(multiplicationTable(2, 2));
+console.log(multiplicationTable(3, 3));
+console.log(multiplicationTable(3, 4));
+console.log(multiplicationTable(4, 4));
+console.log(multiplicationTable(2, 5));
