8f35da663e6b216e1d522aab8c7ccdc9ca554c85
Caleb Jones <calebj516@gmail.com>
2021-08-17 21:29:20 -0500

    Codewars: Panagram Detector

--- Challenge.js
+++ Challenge.js
@@ -1,43 +1,29 @@
-// Challenge: Extract the domain name from a URL (5 kyu)
+// Challenge: Panagram Detector (6 kyu)
 
 // Instructions:
 
-// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 
-// domainName("http://github.com/carbonfive/raygun") == "github"
-// domainName("http://www.zombie-bites.com") == "zombie-bites"
-// domainName("https://www.cnet.com") == "cnet"
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
 // My code below:
 
-function domainName(url) {
-  // first period position
-  let firstPeriodPos = url.indexOf(".");
-  // second period position
-  let secondPeriodPos = url.indexOf(".", firstPeriodPos + 1);
-  // forward slash position
-  let forwardSlashPos = url.indexOf("/");
-  let secondForwardSlashPos = forwardSlashPos + 1;
-  let domainName;
-
-  // if url contains 'www', extract from after first period up to the last period
-  if (url.includes("www")) {
-    domainName = url.slice(firstPeriodPos + 1, secondPeriodPos);
+function isPangram(str) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  str = str.toLowerCase();
+  for (let i = 0; i < alphabet.length; i++) {
+    // if a character of the alphabet is not found in str, this immediately disqualifies str from being a panagram. Return false.
+    if (str.indexOf(alphabet[i]) < 0) {
+      return false;
+    }
   }
-  // if there is a forward slash and no 'www', extract from after second forward slash up to first period
-  if (forwardSlashPos && !url.includes("www")) {
-    domainName = url.slice(secondForwardSlashPos + 1, firstPeriodPos);
-  }
-  // if there is no 'http' and 'www', extract from beginning up to first period
-  if (!url.includes("http") && !url.includes("www")) {
-    domainName = url.slice(0, firstPeriodPos);
-  }
-  return domainName;
+  // otherwise if we exit the for loop without returning false, we know str is a panagram. Return true.
+  return true;
 }
 
 // Tests below:
 
-console.log(domainName("http://github.com/carbonfive/raygun")); // github
-console.log(domainName("http://www.zombie-bites.com")); // zombie-bites
-console.log(domainName("https://www.cnet.com")); // cnet
-console.log(domainName("https://www.facebook.com")); // facebook
+console.log(isPangram("This isn't a pangram!")); // false
+console.log(isPangram("abcdefghijklmopqrstuvwxyz ")); // false
+console.log(isPangram("Pack my box with five dozen liquor jugs.")); // true
+console.log(isPangram("How quickly daft jumping zebras vex.")); // true
