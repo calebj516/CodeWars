1cb93078712597bece0fb3484e48589776cc827b
Caleb Jones <calebj516@gmail.com>
2021-08-24 17:55:59 -0500

    Codewars: Unique In Order

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,28 @@
-// Challenge: Exclamation marks series #17: Put the exclamation marks and question marks to the balance, Are they balanced? (6 kyu)
+// Challenge: Unique In Order (6 kyu)
 
 // Instructions:
 
-// Each exclamation mark weight is 2; Each question mark weight is 3. Put two string left and right to the balance, Are they balanced?
-
-// If the left side is more heavy, return "Left"; If the right side is more heavy, return "Right"; If they are balanced, return "Balance".
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 // My code below:
 
-function balance(left, right) {
-  let leftSum = 0;
-  let rightSum = 0;
+const uniqueInOrder = (iterable) => {
+  let result = [];
 
-  for (let i = 0; i < left.length; i++) {
-    left[i] === "?" ? (leftSum += 3) : (leftSum += 2);
+  // loop through string
+  // If i is not equal to i  1, push to result
+
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      result.push(iterable[i]);
+    }
   }
 
-  for (let i = 0; i < right.length; i++) {
-    right[i] === "?" ? (rightSum += 3) : (rightSum += 2);
-  }
-
-  return leftSum === rightSum
-    ? "Balance"
-    : leftSum > rightSum
-    ? "Left"
-    : "Right";
-}
+  return result;
+};
 
 // Tests below:
 
-console.log(balance("!!", "??")); // Right
-console.log(balance("!??", "?!!")); // Left
-console.log(balance("!?!!", "?!?")); // Left
-console.log(balance("!!???!????", "??!!?!!!!!!!")); // Balancce
+console.log(uniqueInOrder("AAAABBBCCDAABBB")); // ["A", "B", "C", "D", "A", "B"]
+console.log(uniqueInOrder("ABBCcAD")); // ["A", "B", "C", "c", "A", "D"]
+console.log(uniqueInOrder([1, 2, 2, 3, 3])); // [1, 2, 3]
