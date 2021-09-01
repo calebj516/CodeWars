c58e0c77b9d462daac503bcf2f7836d3c42d437c
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-08-31 22:26:35 -0500

    Codewars: Multiplication Table Challenge

--- Challenge.js
+++ Challenge.js
@@ -1,41 +1,35 @@
-// Challenge: Round by 0.5 steps (6 kyu)
+// Challenge: Multiplication table (6 kyu)
 
 // Instructions:
 
-// Round any given number to the closest 0.5 step
+// Your task, is to create NxN multiplication table, of size provided in parameter.
 
-// I.E.
+// for example, when given size is 3:
 
-// solution(4.2) = 4
-// solution(4.3) = 4.5
-// solution(4.6) = 4.5
-// solution(4.8) = 5
-// Round up if number is as close to previous and next 0.5 steps.
-
-// solution(4.75) == 5
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 
 // My code below:
 
-function solution(n){
+const multiplicationTable = size => {
 
-  // split at period, but prepend to the result to maintain decimal form
-  let decimal = '.' + n.toString().split(".")[1];
-  // If decimal is greater than or equal to .75, add 1 to Math.floor(n)
-  // If greater than or equal to .25, add 0.5 to Math.floor(n)
-  // Else round down by returning the result of Math.floor(n)
-  if(decimal >= 0.75){
-    return Math.floor(n) + 1;
-  } else if(decimal >= 0.25){
-    return Math.floor(n) + 0.5;
-  } else {
-    return Math.floor(n);
-  }
-  
+  let result = [];
+
+  for(let row = 1; row <= size; row++){
+    let innerArr = [];
+      for(let col = 1; col <= size; col++){
+        innerArr.push(col*row);
+      }
+      result.push(innerArr);
+    }
+
+  return result;
+
 }
 
 // Tests below:
 
-console.log(solution(4.2)); // 4
-console.log(solution(4.3)); // 4.5
-console.log(solution(4.6)); // 4.5
-console.log(solution(4.8)); // 5
+console.log(multiplicationTable(3)); // [[1,2,3],[2,4,6],[3,6,9]]
+console.log(multiplicationTable(4)); // [[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]]
