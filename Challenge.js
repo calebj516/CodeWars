0a40c691f584751de3795c11e7104574a8f474f4
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-10-11 19:48:48 -0500

    Challenge: Row Weights

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,46 @@
-// Challenge: Find the Missing Number (7 kyu)
+// Challenge: Row Weights (JavaScript) (7 kyu)
 
 // Instructions:
 
-// The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+// Scenario
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
 
-// You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+// Notes
+// Array size is at least 1.
+// All numbers will be positive.
 
 // My code below:
 
-function missingNo(nums) {
+function rowWeights(array){
   
-  nums = nums.sort();
-  let numsArr = [];
-  
-  for(let i = 0; i <= 100; i++){
-    numsArr[i] = i;
+  // declare variables for team 1 and team 2 total weights, and result array to hold the final result.
+  let team1 = 0;
+  let team2 = 0;
+  let resultArr = [];
+  // loop through array, adding even-numbered elements to team1 and odd-numbered elements to team2.
+  for(let i = 0; i < array.length; i++){
+    i % 2 === 0 ? team1 += array[i] : team2 += array[i];
   }
-    
-  return +numsArr.filter(n => nums.indexOf(n) < 0); 
+  // push team1 and team2 total weights to the result array.
+  resultArr.push(team1, team2);
+  // return result array.
+  return resultArr;
+  
 }
 
 // Tests below:
 
-let arr1 = [9, 45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18, 71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13, 64, 56, 39, 48, 92, 84, 94, 87, 90, 77, 63, 32, 68, 37, 96, 23, 0, 95, 1, 52, 78, 6, 57, 50, 2, 46, 19, 76, 47, 14, 4, 3, 29, 17, 11, 21, 24, 74, 65, 12, 83, 28, 41, 66, 7, 58, 55, 51, 43, 97, 42, 86, 49, 31, 20, 75, 70, 34, 33, 38, 8, 15, 62, 35, 61, 99, 16] // 26
-let arr2 = [26, 0, 75, 87, 33, 52, 37, 59, 27, 4, 54, 15, 24, 7, 21, 82, 98, 79, 34, 25, 1, 99, 5, 10, 96, 97, 65, 85, 47, 28, 81, 70, 74, 11, 38, 45, 84, 13, 41, 2, 86, 39, 29, 43, 19, 31, 18, 58, 14, 77, 69, 32, 6, 66, 61, 62, 50, 53, 8, 80, 72, 9, 68, 35, 42, 73, 83, 71, 92, 95, 63, 51, 16, 17, 64, 22, 44, 91, 30, 76, 12, 3, 46, 60, 36, 56, 88, 89, 100, 78, 90, 49, 55, 48, 23, 93, 57, 67, 20, 94] // 40
-let arr3 = [15, 60, 61, 95, 46, 38, 68, 11, 47, 45, 27, 23, 3, 16, 8, 81, 76, 63, 62, 57, 59, 22, 55, 78, 28, 54, 74, 39, 79, 65, 48, 82, 17, 2, 98, 90, 18, 9, 56, 34, 7, 75, 10, 93, 35, 5, 73, 77, 85, 71, 13, 91, 83, 70, 89, 4, 84, 14, 52, 99, 53, 33, 49, 42, 40, 58, 30, 36, 67, 72, 41, 26, 87, 97, 25, 29, 50, 64, 21, 88, 66, 24, 94, 51, 1, 100, 0, 96, 69, 12, 92, 31, 37, 6, 86, 32, 19, 44, 20, 43] // 80
-
-console.log(missingNo(arr1)); // 26
-console.log(missingNo(arr2)); // 40
-console.log(missingNo(arr3)); // 80
+console.log(rowWeights([80])); // [80,0]
+console.log(rowWeights([100,50])); // [100,50]
+console.log(rowWeights([50,60,70,80]));  // [120,140]
+console.log(rowWeights([13,27,49])); // [62,27]
+console.log(rowWeights([70,58,75,34,91])); // [236,92]
+console.log(rowWeights([29,83,67,53,19,28,96])); // [211,164]
+console.log(rowWeights([0])); // [0,0]
+console.log(rowWeights([100,51,50,100])); // [150,151]
+console.log(rowWeights([39,84,74,18,59,72,35,61])); // [207,235];
+console.log(rowWeights([0,1,0])); // [0,1]
