c2689b297d90db169e85ecd2bbd6666f35b4f09e
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-11-03 20:33:30 -0500

    Challenge: Fix String Case

--- Challenge.js
+++ Challenge.js
@@ -1,23 +1,26 @@
-// Challenge: Filter the number (JavaScript) (7 kyu)
+// Challenge: Fix String Case (7 kyu)
 
-// Oh no! The number has been mixed up with the text. Your goal is to retreive the number from the text, can you return the number back to it's original state?
-
-// Task:
-// Your task is to return a number from a string.
-
-// Details:
-// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
 
 // My code below:
 
-const FilterString = value => {
-  // Remove all non-digit characters, prepend result with '+' to change from string to number
-  return +value.replace(/[^\d]/g, "");
+const solve = (s) => {
+
+  // Compare length of s with all lowercase removed (leaving only uppercase) to length with all uppercase removed (leaving only lowercase)
+  // If the length of the uppercase is greater, change s to uppercase, otherwise change to lowercase.
+
+  let sLower = s.replace(/[A-Z]/g, "");
+  let sUpper = s.replace(/[a-z]/g, "");
+  
+  return sUpper.length > sLower.length ? s.toUpperCase() : s.toLowerCase();
+  
 }
 
 // Tests below:
 
-console.log(FilterString("123")); // 123
-console.log(FilterString("a1b2c3")); // 123
-console.log(FilterString("aa1bb2cc3dd")); // 123
+console.log(solve("coDe")); // code
+console.log(solve("CODe")); // CODE
+console.log(solve("coDE")); // code
 
