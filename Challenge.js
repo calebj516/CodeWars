89c0502d38353b3c9029a0a90e1b7592d047a2c6
Caleb Jones <calebj516@gmail.com>
2021-11-11 19:35:29 -0600

    Challenge: Merge Two Arrays

--- Challenge.js
+++ Challenge.js
@@ -1,19 +1,44 @@
-// Challenge: sPoNgEbOb MeMe (7 kyu)
+// Challenge: Merge Two Arrays (7 kyu)
 
 // Description:
 
-// Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+// Write a function that combines two arrays by alternatingly taking elements from each array in turn.
 
-// You need to create a function that converts the input into this format, with the output being the same string except there is a pattern of uppercase and lowercase letters.
+// Examples:
+
+// [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+// [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+// Points:
+
+// The arrays may be of different lengths, with at least one character/digit.
+// One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+// Inspired by https://adriann.github.io/programming_problems.html
 
 // My code below:
 
-function spongeMeme(sentence) {
-  // Change even-numbered indexed elements to uppercase, and odd-numbered indexed elements to lowercase
-  return sentence.split('').map((c, i) => i % 2 === 0 ? c.toUpperCase(): c.toLowerCase()).join('');
-}
+const mergeArrays = (a, b) => {
+  // array to hold final result
+  let newArr = [];
+  // Determine which array is longer and then store the length to use in the for loop later on
+  let len = a.length > b.length ? a.length : b.length;
+
+  for (let i = 0; i < len; i++) {
+    // if the value in array a is truthy, push it to the new array
+    if (a[i]) {
+      newArr.push(a[i]);
+    }
+    // if the value in array b is truthy, push it to the new array
+    if (b[i]) {
+      newArr.push(b[i]);
+    }
+  }
+  // return final result
+  return newArr;
+};
 
 // Tests below:
 
-console.log(spongeMeme("stop Making spongebob Memes!")); // 'StOp mAkInG SpOnGeBoB MeMeS!'
-console.log(spongeMeme("You can't listen to Christmas music until after Thanksgiving!")); // 'yOu CaN'T LiStEn To cHrIsTmAs MuSiC UnTiL AfTeR tHaNkSgIvInG!'
+console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ["a", "b", "c", "d", "e"])); // [1, "a", 2, "b", 3, "c", 4, "d", 5, "e", 6, 7, 8]
+
+console.log(mergeArrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])); // ["a",1,"b",2,"c",3,"d",4,"e",5
