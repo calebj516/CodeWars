507754a100da116a64e4928502901cefa83fde35
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-11-13 20:38:25 -0600

    Challenge: Larger Product or Sum

--- Challenge.js
+++ Challenge.js
@@ -1,24 +1,28 @@
-// Challenge: Sort by Last Char (7 kyu)
+// Challenge: Larger Product or Sum (7 kyu)
 
 // Description:
 
-// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// For this Kata you will be given an array of numbers and another number n. You have to find the sum of the n largest numbers of the array and the product of the n smallest numbers of the array, and compare the two.
 
-// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+// If the sum of the n largest numbers is higher, return "sum"
+// If the product of the n smallest numbers is higher, return "product"
+// If the 2 values are equal, return "same"
 
-// All inputs will be valid.
+// Note The array will never be empty and n will always be smaller than the length of the array.
 
 // My code below:
 
-// const last = (x) => {
-//   return x.split(" ").sort((a, b) => a.slice(-1) > b.slice(-1));
-// };
-
-function last(x) {
-  return x.split(" ").sort((a, b) => a.slice(-1) > b.slice(-1));
+const sumOrProduct = (array, n) => {
+  // sort the array in ascending order. 
+  array.sort((a, b) => a - b);
+  // calculate largest and smallest using array reduce
+  let largest = array.slice(-n).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+  let smallest = array.slice(0, n).reduce((previousValue, currentValue) => previousValue * currentValue, 1);
+  // return value determined using nested ternary 
+  return largest > smallest ? "sum" : largest < smallest ? "product" : "same";
 }
 
 // Tests below:
-console.log(last("man i need a taxi up to ubud")); // ["a","need","ubud","i","taxi","man","to","up"]
-console.log(last("what time are we climbing up the volcano")); // ["time","are","we","the","climbing","volcano","up","what"]
-console.log(last("take me to semynak")); // ["take","me","semynak","to"]
+console.log(sumOrProduct([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)); // "sum"
+console.log(sumOrProduct([10, 41, 8, 16, 20, 36, 9, 13, 20], 3)); // "product"
+console.log(sumOrProduct([10, 20, 3, 30, 5, 4], 3)); // "same"
