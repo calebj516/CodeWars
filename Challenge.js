d56ba641a3ac3040a039200c90de5e851cd96c0b
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-11-16 20:35:37 -0600

    Challenge: Convert an array of strings to array of numbers

--- Challenge.js
+++ Challenge.js
@@ -1,28 +1,24 @@
-// Challenge: Larger Product or Sum (7 kyu)
+// Challenge: Convert an array of strings to array of numbers (7 kyu)
 
 // Description:
 
-// For this Kata you will be given an array of numbers and another number n. You have to find the sum of the n largest numbers of the array and the product of the n smallest numbers of the array, and compare the two.
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
 
-// If the sum of the n largest numbers is higher, return "sum"
-// If the product of the n smallest numbers is higher, return "product"
-// If the 2 values are equal, return "same"
+// You need to cast the whole array to the correct type.
 
-// Note The array will never be empty and n will always be smaller than the length of the array.
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
 
 // My code below:
 
-const sumOrProduct = (array, n) => {
-  // sort the array in ascending order. 
-  array.sort((a, b) => a - b);
-  // calculate largest and smallest using array reduce
-  let largest = array.slice(-n).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-  let smallest = array.slice(0, n).reduce((previousValue, currentValue) => previousValue * currentValue, 1);
-  // return value determined using nested ternary 
-  return largest > smallest ? "sum" : largest < smallest ? "product" : "same";
+const toNumberArray = (stringarray) => {
+  return stringarray.map(n => +n);
 }
 
 // Tests below:
-console.log(sumOrProduct([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)); // "sum"
-console.log(sumOrProduct([10, 41, 8, 16, 20, 36, 9, 13, 20], 3)); // "product"
-console.log(sumOrProduct([10, 20, 3, 30, 5, 4], 3)); // "same"
+console.log(toNumberArray(["1.1","2.2","3.3"])); // [1.1,2.2,3.3]
+console.log(toNumberArray(["1","2","3"])); // [1,2,3]
