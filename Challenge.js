96785b1da8de8582c1da84e8bf3c3305e7959946
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-11-22 20:19:16 -0600

    Challenge: Reverse a Number

--- Challenge.js
+++ Challenge.js
@@ -1,14 +1,26 @@
-// Challenge: Round up to the next multiple of 5 (7 kyu)
+// Challenge: Reverse a Number (7 kyu)
 
 // Description:
-// Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
 
 // My code below:
 
-function roundToNext5(n) {
-  return Math.ceil(n / 5) * 5;
+function reverseNumber(n) {
+  // reverse the string.
+  n = n.toString().split("").reverse();
+  // if the last character is a minus sign, remove it from the array and add it to the beginning instead.
+  if(n[n.length - 1] === "-"){
+    n.pop();
+    n.unshift("-");
+  }
+  // return the array joined into a string, and converted into a number by prepending the final result with a plus sign.
+  return +(n.join(""));
 }
 
 // Tests below:
-console.log(roundToNext5(8)); // 10
-console.log(roundToNext5(-8)); // -10
+console.log(reverseNumber("123")); // 321
+console.log(reverseNumber("-456")); // -654
+console.log(reverseNumber("1000")); // 1
