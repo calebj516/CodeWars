858e857a1c69f799f95e6ec15a0a8bd8fe241fe4
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-11-29 18:55:38 -0600

    Challenge: Number of Decimal Digits
    Count the number of digits in any number n.

--- Challenge.js
+++ Challenge.js
@@ -1,29 +1,19 @@
-// Challenge: Count the Digit (7 kyu)
+// Challenge: Number of Decimal Digits (7 kyu)
 
 // Description:
 
-// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
 
-// Square all numbers k (0 <= k <= n) between 0 and n.
-
-// Count the numbers of digits d used in the writing of all the k**2.
-
-// Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+// All inputs will be valid.
 
 // My code below:
 
-const nbDig = (n, d) => {
-  // variable to hold total number of digits d
-  let totalOfDigits = 0;
-  // loop through from 0 to n, squaring each number, converting the result into a string, changing that string into an array, and filtering out the numbers that don't match d.
-  for(let i = 0; i <= n; i++){
-    totalOfDigits += (i * i).toString().split('').filter(num => num == d).length;
-  }
-
-  return totalOfDigits;
+const digits = (n) => {
+  // code goes here
+  return n.toString().length;
 }
 
 // Tests below:
-console.log(nbDig(5750, 0)); // 4700
-console.log(nbDig(11011, 2)); // 9481
-console.log(nbDig(12224, 8)); // 7733
+console.log(digits(12409512)); // 8 
+console.log(digits(1234)); // 4
+console.log(digits(412)); // 3
