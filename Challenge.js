b279f282c66a7e7247b81dd7a2e6f146ef297a13
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-12-02 10:10:44 -0600

    Challenge: Odd/Even number of divisors

--- Challenge.js
+++ Challenge.js
@@ -1,23 +1,30 @@
-// Challenge: Sum of array singles (7 kyu)
+// Challenge: Odd/Even number of divisors (7 kyu)
 
 // Description:
 
-// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+// Given an integer n return "odd" if the number of its divisors is odd. Otherwise return "even".
 
-// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+// Note: big inputs will be tested.
 
-// More examples in the test cases.
+// Examples:
 
-// Good luck!
+// All prime numbers have exactly two divisors (hence "even").
+
+// For n = 12 the divisors are [1, 2, 3, 4, 6, 12] – "even".
+
+// For n = 4 the divisors are [1, 2, 4] – "odd".
 
 // My code below:
 
-function repeats(arr){
-  // Filter for the elements that occur only once with array filter, and then total them up with array reduce
-  return arr.filter(x => arr.filter(y => y === x).length === 1).reduce((prev, current) => prev + current);
-};
+const oddity = n => {
+  // only perfect squares have an odd number of divisors. Example: 16 has 1, 2, 4, 8, and 16, a total of 5.
+  // Note: raising a number to the 0.5 power is the same as taking the square root of the number.
+  return (n ** 0.5) % 1 === 0 ? "odd" : "even";
+}
 
 // Tests below:
-console.log(repeats([4,5,7,5,4,8])); // 15
-console.log(repeats([9, 10, 19, 13, 19, 13])); // 19
-console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11])); // 12
+Console.log(oddity(1)); // 'odd'
+Console.log(oddity(5)); // 'even'
+Console.log(oddity(16)); // 'odd'
+Console.log(oddity(27)); // 'even'
+Console.log(oddity(121)); // 'odd'
