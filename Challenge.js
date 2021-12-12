050aeb706e71a3d9fb0d1b5a3e24cfd980b4de57
Caleb Jones <calebj516@gmail.com>
2021-12-12 17:55:54 -0600

    Challenge: Bumps in the Road

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,28 @@
-// Challenge: Maximum Product (7 kyu)
+// Challenge: Bumps in the Road (7 kyu)
 
 // Description:
 
-// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
 
-// Notes
-// Array/list size is at least 2.
-
-// Array/list numbers could be a mixture of positives, negatives also zeroes.
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
 
 // My code below:
 
-function adjacentElementsProduct(array) {
-  let newArr = [];
-  // push the product of each element and its proceeding value to the newArr array.
-  for (let i = 0; i < array.length - 1; i++) {
-    newArr.push(array[i] * array[i + 1]);
+const bump = (x) => {
+  // count number of bumps, if greater than 15 return Car Dead
+  let count = 0;
+  x = x.split("");
+  // loop through x
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === "n") {
+      count++;
+    }
   }
-  // the spread operator lists out all the values contained in newArr, which Math.max operates on to get the maximum value.
-  return Math.max(...newArr);
-}
+  return count > 15 ? "Car Dead" : "Woohoo!";
+};
 
-// Tests below:
-console.log(adjacentElementsProduct([5, 8])); // 40
-console.log(adjacentElementsProduct([1, 2, 3])); // 6
-console.log(adjacentElementsProduct([1, 5, 10, 9])); // 90
-console.log(adjacentElementsProduct([4, 12, 3, 1, 5])); // 48
-console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4])); // 6
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // 21
+// Tests
+console.log(bump("n")); // "Woohoo!"
+console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); // "Car Dead"
+console.log(bump("______n___n_")); // "Woohoo!"
+console.log(bump("nnnnnnnnnnnnnnnnnnnnn")); // "Car Dead";
