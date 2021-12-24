5c118ecb1db5a9386f2bfa8549e72532e8f6f443
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2021-12-24 16:32:51 -0600

    Challenge: Get the integers between two numbers

--- Challenge.js
+++ Challenge.js
@@ -1,38 +1,32 @@
-// Challenge: Meeting (6 kyu)
+// Challenge: Get the integers between two numbers (7 kyu)
 
 // Description:
 
-// John has invited some friends. His list is:
+// Build a function that can get all the integers between two given numbers.
 
-// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
-// Could you make a program that
+// Example:
 
-// makes this string uppercase
-// gives it sorted in alphabetical order by last name.
-// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+// (2,9)
 
-// So the result of function meeting(s) will be:
+// Should give you this output back:
 
-// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
-// It can happen that in two distinct families with the same family name two people have the same first name too.
+// [ 3, 4, 5, 6, 7, 8 ]
+
+// If startNum is the same as endNum, return an empty array.
 
 // My code below:
 
-const meeting = s => {
-  // regex explanation: there are two capturing groups enclosed in parenthesis, within which any number of word characters \w+.
-  // These contain the first name, followed by a colon separating the two groups, then the last name.
-  // The $2 is a placeholder for the last name and the $1 is for the first name. Putting the $2 first means the last name comes first.
-  return s.toUpperCase()
-         .replace(/(\w+):(\w+)/g, "($2, $1)")
-         .split(';')
-         .sort()
-         .join('')
-};
+const range = (startNum, endNum) =>  
+{  
+ //code goes in here
+  let result = [];
+  for(let i = startNum + 1; i < endNum; i++){
+    result.push(i);
+  }
+  return result;
+};  
 
 // Tests
 
-console.log(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")); // "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");
-
-console.log(meeting("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell")); // "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
-
-console.log(meeting("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern")); // "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
+console.log(range(0, 100)); // 1 to 99
+console.log(range(-100, 0)); // -99 to -1
