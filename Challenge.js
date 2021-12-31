049b4c5427c25c1b5c4dacc036ccfc0576a2289c
Caleb Jones <calebj516@gmail.com>
2021-12-31 16:54:16 -0600

    Challenge: Noonerize Me

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,36 @@
-// Challenge: Spoonerize Me (7 kyu)
+// Challenge: Noonerize Me (7 kyu)
 
 // Description:
 
-// A spoonerism is a spoken phrase in which the first letters of two of the words are swapped around, often with amusing results.
+// Spoonerize... with numbers... numberize?... numboonerize?... noonerize? ...anyway! If you don't yet know what a spoonerism is and haven't yet tried my spoonerism kata, please do check it out first.
 
-// In its most basic form a spoonerism is a two word phrase in which only the first letters of each word are swapped:
+// You will create a function which takes an array of two positive integers, spoonerizes them, and returns the positive difference between them as a single number or 0 if the numbers are equal:
 
-// "not picking" --> "pot nicking"
+// [123, 456] = 423 - 156 = 267
+// Your code must test that all array items are numbers and return "invalid array" if it finds that either item is not a number. The provided array will always contain 2 elements.
 
-// Your task is to create a function that takes a string of two words, separated by a space: words and returns a spoonerism of those words in a string, as in the above example.
-
-// NOTE: All input strings will contain only two words. Spoonerisms can be more complex. For example, three-word phrases in which the first letters of the first and last words are swapped: "pack of lies" --> "lack of pies" or more than one letter from a word is swapped: "flat battery --> "bat flattery" You are NOT expected to account for these, or any other nuances involved in spoonerisms.
-
-// Once you have completed this kata, a slightly more challenging take on the idea can be found here: http://www.codewars.com/kata/56dbed3a13c2f61ae3000bcd
+// When the inputs are valid, they will always be integers, no floats will be passed. However, you must take into account that the numbers will be of varying magnitude, between and within test cases.
 
 // My code below:
 
-function spoonerize(words) {
-  let word = words.split(' ');
-  // Note on slice() from MDN: if endIndex is omitted slice() extracts to the end of the string. (E.g. "test".slice(1) returns "est")
-  // So, word[0].slice(1) and word[1].slice(1) returns the entire word except for the first character. Adding the first character in front of these gives us the answer.
-  return [word[1][0] + word[0].slice(1), word[0][0] + word[1].slice(1)].join(' ');
+function noonerize(numbers) {
+  // ...mathoonerize, maybe?
+  if (numbers.every((el) => typeof el === "number")) {
+    numbers = numbers.map(String);
+    let [a, b] = numbers;
+    [a, b] = [b[0] + a.slice(1), a[0] + b.slice(1)];
+    numbers = numbers.map(Number);
+    return Math.abs([a - b]);
+  }
+  return "invalid array";
 }
 
 // Tests
 
-console.log(spoonerize("not picking")); // "pot nicking"
-console.log(spoonerize("wedding bells")); // "bedding wells"
-console.log(spoonerize("jelly beans")); // "belly jeans"
-console.log(spoonerize("pop corn")); // "cop porn"
+console.log(noonerize([12, 34])); // 18
+console.log(noonerize([55, 63])); // 12
+console.log(noonerize([357, 579])); // 178
+console.log(noonerize(["hello", "world"])); // "invalid array"
+console.log(noonerize([1000000, 9999999])); // 7000001
+console.log(noonerize([1000000, "hello"])); // "invalid array"
+console.log(noonerize(["world", 63])); // "invalid array"
