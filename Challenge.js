7513b63e0040ca70e3b9a2a2ce0e8f8b856e4eb9
Caleb Jones <calebj516@gmail.com>
2022-01-03 18:46:59 -0600

    Challenge: Two to One

--- Challenge.js
+++ Challenge.js
@@ -1,36 +1,33 @@
-// Challenge: Noonerize Me (7 kyu)
+// Challenge: Two to One (7 kyu)
 
 // Description:
 
-// Spoonerize... with numbers... numberize?... numboonerize?... noonerize? ...anyway! If you don't yet know what a spoonerism is and haven't yet tried my spoonerism kata, please do check it out first.
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
-// You will create a function which takes an array of two positive integers, spoonerizes them, and returns the positive difference between them as a single number or 0 if the numbers are equal:
+// Examples:
 
-// [123, 456] = 423 - 156 = 267
-// Your code must test that all array items are numbers and return "invalid array" if it finds that either item is not a number. The provided array will always contain 2 elements.
-
-// When the inputs are valid, they will always be integers, no floats will be passed. However, you must take into account that the numbers will be of varying magnitude, between and within test cases.
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
 // My code below:
 
-function noonerize(numbers) {
-  // ...mathoonerize, maybe?
-  if (numbers.every((el) => typeof el === "number")) {
-    numbers = numbers.map(String);
-    let [a, b] = numbers;
-    [a, b] = [b[0] + a.slice(1), a[0] + b.slice(1)];
-    numbers = numbers.map(Number);
-    return Math.abs([a - b]);
-  }
-  return "invalid array";
+function longest(s1, s2) {
+  // your code
+  return (s1 + s2)
+    .split("")
+    .sort()
+    .filter((el, i, arr) => el !== arr[i + 1])
+    .join("");
 }
 
 // Tests
 
-console.log(noonerize([12, 34])); // 18
-console.log(noonerize([55, 63])); // 12
-console.log(noonerize([357, 579])); // 178
-console.log(noonerize(["hello", "world"])); // "invalid array"
-console.log(noonerize([1000000, 9999999])); // 7000001
-console.log(noonerize([1000000, "hello"])); // "invalid array"
-console.log(noonerize(["world", 63])); // "invalid array"
+console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
+console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
+console.log(longest("lordsofthefallen", "gamekult")); // "adefghklmnorstu"
+console.log(longest("codewars", "codewars")); // "acdeorsw"
+console.log(longest("agenerationmustconfrontthelooming", "codewarrs")); // "acdefghilmnorstuw"
