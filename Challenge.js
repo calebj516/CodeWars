2072419c934b3b33e514b3f15d5645cf619e7e1e
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-01-04 20:40:25 -0600

    Challenge: Digitize

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,27 @@
-// Challenge: Two to One (7 kyu)
+// Challenge: Digitize (7 kyu)
 
 // Description:
 
-// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Given a non-negative integer, return an array / a list of the individual digits in order.
 
 // Examples:
 
-// a = "xyaabbbccccdefww"
-// b = "xxxxyyyyabklmopq"
-// longest(a, b) -> "abcdefklmopqwxy"
-// a = "abcdefghijklmnopqrstuvwxyz"
-// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
 
 // My code below:
 
-function longest(s1, s2) {
-  // your code
-  return (s1 + s2)
-    .split("")
-    .sort()
-    .filter((el, i, arr) => el !== arr[i + 1])
-    .join("");
+function digitize(n) {
+  return n.toString().split('').map(Number);
 }
 
 // Tests
 
-console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
-console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
-console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
-console.log(longest("lordsofthefallen", "gamekult")); // "adefghklmnorstu"
-console.log(longest("codewars", "codewars")); // "acdeorsw"
-console.log(longest("agenerationmustconfrontthelooming", "codewarrs")); // "acdefghilmnorstuw"
+console.log(digitize(123)); // [1,2,3]
+console.log(digitize(1)); // [1]
+console.log(digitize(0)); // [0]
+console.log(digitize(1230)); // [1,2,3,0]
+console.log(digitize(8675309)); // [8,6,7,5,3,0,9]
