735bfaba021290c84688cae90a492be8ec4ee573
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-01-05 20:17:27 -0600

    Challenge: Identical Elements

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,35 @@
-// Challenge: Digitize (7 kyu)
+// Challenge: Identical Elements (7 kyu)
 
 // Description:
 
-// Given a non-negative integer, return an array / a list of the individual digits in order.
+// Given two arrays of integers m and n, test if they contain at least one identical element. Return true if they do; false if not.
 
-// Examples:
-
-// 123 => [1,2,3]
-
-// 1 => [1]
-
-// 8675309 => [8,6,7,5,3,0,9]
+// Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
 
 // My code below:
 
-function digitize(n) {
-  return n.toString().split('').map(Number);
+function duplicateElements(m, n) {
+  // some will return true if it at least one test passes.
+  // includes will return true if an array contains a certain value among its entries.
+  // putting it all together: test each element in m using some, to see if it is included in n (n.includes(v), with v referring to each element in m)
+  return m.some(v => n.includes(v));
 }
 
 // Tests
 
-console.log(digitize(123)); // [1,2,3]
-console.log(digitize(1)); // [1]
-console.log(digitize(0)); // [0]
-console.log(digitize(1230)); // [1,2,3,0]
-console.log(digitize(8675309)); // [8,6,7,5,3,0,9]
+// It should handle duplicates
+
+console.log(duplicateElements([1, 2, 3, 4, 5], [1, 6, 7, 8, 9])); // true
+console.log(duplicateElements([9, 8, 7], [8, 1, 3])); // true
+console.log(duplicateElements([-2, -4, -6, -8], [-2, -3, -5, -7])); // true
+console.log(duplicateElements([-9, -8, -7], [-8, -1, -3])); // true
+
+// It should handle no duplicates
+
+console.log(duplicateElements([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])); // false
+
+// It should handle empty arrays
+
+console.log(duplicateElements([], [9, 8, 7, 6, 5])); // false
+console.log(duplicateElements([9, 8, 7, 6, 5], [])); // false
+console.log(duplicateElements([],[])); // false
