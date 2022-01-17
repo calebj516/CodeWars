89678a54b328a6e71e555a254c65ce6dc6af2049
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-01-17 12:12:16 -0600

    Challenge: Shortest Word

--- Challenge.js
+++ Challenge.js
@@ -1,44 +1,29 @@
-// Challenge: Coin Combination Calculator (7 kyu)
+// Challenge: Shortest Word (7 kyu)
 
 // Description:
 
-// The function takes cents value (int) and needs to return the minimum number of coins combination of the same value.
+// Simple, given a string of words, return the length of the shortest word(s).
 
-// The function should return an array where
-// coins[0] = pennies ==> $00.01
-// coins[1] = nickels ==> $00.05
-// coins[2] = dimes ==> $00.10
-// coins[3] = quarters ==> $00.25
-
-// So for example:
-// coinCombo(6) --> [1, 1, 0, 0]
+// String will never be empty and you do not need to account for different data types.
 
 // My code below:
 
-function coinCombo(cents) {
-  let coins = [1,5,10,25];
-  let result = [0,0,0,0];
-  for (let i = 3; i >= 0 && cents > 0; i--) {
-    // if cents is less than coins then the result will be zero and the cents total will not be affected.
-    // on the other hand, if cents is greater than coins, then Math.floor will correctly calculate the minimum number of coins to contribute towards cents.
-    result[i] = Math.floor(cents / coins[i]);
-    // subtract the result multiplied by the value of currency. Once cents hits zero the loop stops and returns result.
-      cents -= result[i] * coins[i];
-  }
-  return result;
+function findShort(s){
+  
+  // split string into an array
+  // sort array by length of words
+  // return length of first element
+  
+  return s.split(' ').sort((a, b) => a.length - b.length)[0].length;
+  
 }
 
 // Tests
 
-console.log(coinCombo(1)); // [1, 0, 0, 0]
-console.log(coinCombo(2)); // [2, 0, 0, 0]
-console.log(coinCombo(5)); // [0, 1, 0, 0]
-console.log(coinCombo(6)); // [1, 1, 0, 0]
-console.log(coinCombo(10)); // [0, 0, 1, 0]
-console.log(coinCombo(11)); // [1, 0, 1, 0]
-console.log(coinCombo(15)); // [0, 1, 1, 0]
-console.log(coinCombo(17)); // [2, 1, 1, 0]
-console.log(coinCombo(25)); // [0, 0, 0, 1]
-console.log(coinCombo(30)); // [0, 1, 0, 1]
-console.log(coinCombo(36)); // [1, 0, 1, 1]
-console.log(coinCombo(43)); // [3, 1, 1, 1]
+console.log(findShort("bitcoin take over the world maybe who knows perhaps")); // 3
+console.log(findShort("turns out random test cases are easier than writing out basic ones")); // 3 
+console.log(findShort("lets talk about javascript the best language")); // 3 
+console.log(findShort("i want to travel the world writing code one day")); // 1 
+console.log(findShort("Lets all go on holiday somewhere very cold")); // 2 
+console.log(findShort("Test where final word shortest see")); // 3 
+console.log(findShort("Let's travel abroad shall we")); // 2
