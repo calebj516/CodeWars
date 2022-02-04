a325a6fd8e732e1942ca9ab6a5f102d1358dae6d
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-04 16:16:48 -0600

    Challenge: Ones' Complement

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,26 @@
-// Challenge: Remove the minimum (7 kyu)
+// Challenge: Ones' Complement (7 kyu)
 
 // Description:
 
-// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s. For example:
 
-// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
-
-// Task:
-
-// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
-
-// Don't change the order of the elements that are left.
+// onesComplement(1001) = 0110
+// onesComplement(1001) = 0110
+// For any given binary number,formatted as a string, return the Ones' Complement of that number.
 
 // My code below:
 
-function removeSmallest(numbers) {
-  // obtain the index of the minimum number by using Math.min.
-  // the spread operator allows us to call this on an array.
-  let minNumIndex = numbers.indexOf(Math.min(...numbers));
-  // return elements of numbers up to the position of the minimum number, and then from one position after, effectively removing the min number from numbers
-  return [...numbers.slice(0, minNumIndex), ...numbers.slice(minNumIndex + 1)];
+function onesComplement(n) {
+    // split n into an array
+    // map each element as follows: if 0, then 1; otherwise, 0
+    // join back into a string for the final result
+    return n.split('').map(num => num === "0" ? "1" : "0").join('');
 }
 
 // Tests
 
-console.log(removeSmallest([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
-console.log(removeSmallest([5, 3, 2, 1, 4])); // [5, 3, 2, 4]
-console.log(removeSmallest([2, 2, 1, 2, 1])); // [2, 2, 2, 1]
-console.log(removeSmallest([])); // []
+console.log(onesComplement("0")); // "1"
+console.log(onesComplement("1")); // "0"
+console.log(onesComplement("10")); // "01"
+console.log(onesComplement("01")); // "10"
+console.log(onesComplement("1101")); // "0010"
