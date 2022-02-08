27d5696c7af7fba2e5bc5fd140fae8b6472fab39
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-08 10:49:29 -0600

    Challenge: Get the Middle Character

--- Challenge.js
+++ Challenge.js
@@ -1,39 +1,43 @@
-// Challenge: Adding remainders to a list (7 kyu)
+// Challenge: Get the Middle Character (7 kyu)
 
 // Description:
 
-// Your job is to implement a function which returns the last D digits of an integer N as a list.
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
-// Special cases:
+// #Examples:
 
-// If D > (the number of digits of N), return all the digits.
-// If D <= 0, return an empty list.
+// Kata.getMiddle("test") should return "es"
 
-// Examples:
-// N = 1
-// D = 1
-// result = [1]
+// Kata.getMiddle("testing") should return "t"
 
-// N = 1234
-// D = 2
-// result = [3, 4]
+// Kata.getMiddle("middle") should return "dd"
 
-// N = 637547
-// D = 6
-// result = [6, 3, 7, 5, 4, 7]
+// Kata.getMiddle("A") should return "A"
+
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
 
 // My code below:
 
-function lastDigit(n, d) {
-  const len = n.toString().length;
-  const nArr = n.toString().split("").map(Number);
-  // include only the elements with an index that is greater than or equal to length - d
-  return nArr.filter((num, idx) => idx >= len - d);
+function getMiddle(s)
+{
+  // Math.floor rounds the result down to the closest whole number and prevents an error in the return statement
+  // the subscript for s needs to be a whole number; it can't have decimals
+  const position = Math.floor(s.length / 2);
+
+  // If length is odd (truthy) return the middle element. 
+  // Otherwise, if even (falsy), return two elements, the one before the middle and the middle element.
+  return s.length % 2 ? s[position] : s[position - 1] + s[position];
 }
 
 // Tests
 
-console.log(lastDigit(1, 1)); // [1]
-console.log(lastDigit(123767, 4)); // [3, 7, 6, 7]
-console.log(lastDigit(0, 1)); // [0]
-console.log(lastDigit(34625647867585, 10)); // [5, 6, 4, 7, 8, 6, 7, 5, 8, 5]
+console.log(getMiddle("test")); // "es"
+console.log(getMiddle("testing")); // "t"
+console.log(getMiddle("middle")); // "dd"
+console.log(getMiddle("A")); // "A"
