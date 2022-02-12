572c24d7f57cfbeb792f8c52e782b5c1c6b801dd
Caleb Jones <calebj516@gmail.com>
2022-02-11 20:38:58 -0600

    Challenge: Create Phone Number

--- Challenge.js
+++ Challenge.js
@@ -1,43 +1,23 @@
-// Challenge: Get the Middle Character (7 kyu)
+// Challenge: Create Phone Number (6 kyu)
 
 // Description:
 
-// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 
-// #Examples:
-
-// Kata.getMiddle("test") should return "es"
-
-// Kata.getMiddle("testing") should return "t"
-
-// Kata.getMiddle("middle") should return "dd"
-
-// Kata.getMiddle("A") should return "A"
-
-// #Input
-
-// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
-
-// #Output
-
-// The middle character(s) of the word represented as a string.
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+// Don't forget the space after the closing parentheses!
 
 // My code below:
 
-function getMiddle(s)
-{
-  // Math.floor rounds the result down to the closest whole number and prevents an error in the return statement
-  // the subscript for s needs to be a whole number; it can't have decimals
-  const position = Math.floor(s.length / 2);
-
-  // If length is odd (truthy) return the middle element. 
-  // Otherwise, if even (falsy), return two elements, the one before the middle and the middle element.
-  return s.length % 2 ? s[position] : s[position - 1] + s[position];
+function createPhoneNumber(numbers) {
+  numbers = numbers.join("");
+  return `(${numbers.slice(0, 3)}) ${numbers.slice(3, 6)}-${numbers.slice(6)}`;
 }
 
 // Tests
 
-console.log(getMiddle("test")); // "es"
-console.log(getMiddle("testing")); // "t"
-console.log(getMiddle("middle")); // "dd"
-console.log(getMiddle("A")); // "A"
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // "(111) 111-1111"
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
