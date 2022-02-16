630e20492fb35cef573ca1f1f6bf36a57eb68d45
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-15 19:17:25 -0600

    Challenge: Defining getters and setters on an existing class

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,57 @@
-// Challenge: Create Phone Number (6 kyu)
+// Challenge: Defining getters and setters on an existing class (6 kyu)
 
 // Description:
 
-// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// There is a preloaded class of Person and it needs a new getter and setter. Person is defined with a constructor that takes a first name and a last name, and provides a mean to get the first name, last name, and full name.
 
-// Example
-// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
-// The returned format must be correct in order to complete this challenge.
-// Don't forget the space after the closing parentheses!
+// Add a getter of name that returns the full name
+// Add a setter of name that modifies first name and last name
 
 // My code below:
 
-function createPhoneNumber(numbers) {
-  let format = "(xxx) xxx-xxxx";
-  // use a for loop to replace each 'x' in format with the number from numbers
-  for (let i = 0; i < format.length; i++) {
-    format = format.replace("x", numbers[i]);
+// Note - I have added a property of my own, middleInitial, to this challenge.
+
+class Person {
+  constructor(firstName, middleName, lastName) {
+    this.firstName = firstName;
+    this.middleInitial = middleName[0].toUpperCase(); // this will work whether a full middle name is passed in, or just the middle initial
+    this.lastName = lastName;
   }
-  return format;
+  
+  getName() {
+    return `${this.firstName} ${this.middleInitial} ${this.lastName}`;
+  }
 }
+
+// define a new property called "name"
+// using the prototype ensures that all objects instantiated using the Person class follow the pattern below
+Object.defineProperty(Person.prototype, "name", {
+
+  get() { 
+    return this.getName();
+  },
+
+  set(name) {
+    // the line below uses destructuring to set the properties
+    return [firstName, middleInitial, lastName] = name.split(" ");
+  }
+
+})
 
 // Tests
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // "(111) 111-1111"
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+let calebJones = new Person('Caleb', 'D', 'Jones');
+
+console.log(calebJones.firstName); // 'Caleb'
+console.log(calebJones.middleInitial); // 'D'
+console.log(calebJones.lastName);  // 'Jones'
+console.log(calebJones.getName()); // 'Caleb D Jones'
+console.log(calebJones.name); // 'Caleb D Jones'
+
+let joeSchmoe = new Person('Joseph', 'Moe', 'Schmoe');
+
+console.log(joeSchmoe.firstName); // 'Joseph'
+console.log(joeSchmoe.middleInitial); // 'M'
+console.log(joeSchmoe.lastName);  // 'Schmoe'
+console.log(joeSchmoe.getName()); // 'Joseph M Schmoe'
+console.log(joeSchmoe.name); // 'Joseph M Schmoe'
