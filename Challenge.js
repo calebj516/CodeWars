fcdeb977909063c49c9ce896037fe1b92a1fc85a
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-18 22:45:06 -0600

    Challenge: Count by X

--- Challenge.js
+++ Challenge.js
@@ -1,45 +1,32 @@
-// Challenge: Fizz Buzz - Without ifs (7 kyu)
+// Challenge: Count by X (8 kyu)
 
 // Description:
 
-// I know that there are quite a few fizz-buzz problems already out there but this one does have a twist (you may find this useful)
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
 
-// Task:
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
 
-// You need to implement a function that applies the standard fizz buzz test:
-
-// If a number is divisible by 3, return Fizz
-// If it is divisible by 5, return Buzz
-// If it is divisible by both 3 and 5, return FizzBuzz
-// If it is not divisible by 3 or 5, return the number itself
-// However, when making the function, you cannot use if (meaning else is also not an option).
-
-// Input:
-
-// A number will always be provided
-// The number provided will always be positive and an integer
-
-// Examples:
-
-// 6 should return Fizz
-// 10 should return Buzz
-// 15 should return FizzBuzz
-// 17 should return 17
+// Return the results as an array (or list in Python, Haskell or Elixir).
 
 // My code below:
 
-// The code does the following:
-// if n is only divisible by 3... return Fizz + an empty string, which evaluates to Fizz
-// if n is only divisible by 5... return an empty string + Buzz, which evaluates to Buzz
-// if n is divisible by both 3 and 5... return an Fizz + Buzz, which evaluates to FizzBuzz
-// if n is divisible by neither 3 nor 5, the two "halves" of the expression evaluate to two empty strings, which added together are stil an empty string,
-// which evaluates to false. Due to the logical OR operator, the truthy value will be returned, which is n.
+function countBy(x, n) {
 
-const fizzBuzz = n => ((n % 3 === 0 ? 'Fizz' : '') + (n % 5 === 0 ? 'Buzz' : '') || n );
+  let z = [];
+  let max = x * n;
+  
+  // start at x, work up to and including the maximum value of x which will be the multiple x multiplied by n, and increment each iteration by the value of the multiple x
+  for(let i = x; i <= max; i += x){
+    z.push(i);
+  }
+  
+  return z;
+}
 
 // Tests
 
-console.log(fizzBuzz(3)) // "Fizz"
-console.log(fizzBuzz(5)) // "Buzz"
-console.log(fizzBuzz(15)) // "FizzBuzz"
-console.log(fizzBuzz(17)) // "17"
+console.log(countBy(1,10)); // [1,2,3,4,5,6,7,8,9,10]
+console.log(countBy(2,5)); // [2,4,6,8,10]
+console.log(countBy(3,7)); // [3,6,9,12,15,18,21]
+console.log(countBy(50,5)); // [50,100,150,200,250]
+console.log(countBy(100,6)); // [100,200,300,400,500,600]
