7048c0ed4bfc33ec902a4bb3b88951e7edfb6880
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-22 21:31:51 -0600

    Challenge: Find the unique number: Solution Using Array Method Find

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,28 @@
-// Challenge: Is n divisible by (...)? (7 kyu)
+// Challenge: Find the unique number (6 kyu)
 
 // Description:
 
-// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
-// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
 
-// Return the results as an array (or list in Python, Haskell or Elixir).
+// The tests contain some very huge arrays, so think about performance.
 
 // My code below:
 
-function isDivisible(){
-  // make use of the arguments object paired with the Object values method to create an array containing all arguments passed in
-  const nums = Object.values(arguments);
-  // divisor is the first number
-  const divisor = nums[0];  
-  // use the every method which returns true if and only if all elements of the array pass the test
-  return nums.every(num => divisor % num === 0);
+function findUniq(arr) {
+  
+  return arr.find(num => arr.indexOf(num) === arr.lastIndexOf(num));
+
 }
 
 // Tests
 
-console.log(isDivisible(3,3,4)); // false
-console.log(isDivisible(12,3,4)); // true
-console.log(isDivisible(8,3,4,2,5)); // false
-console.log(isDivisible(48,3,4,2)); // true
-console.log(isDivisible(100,5,10,20,25)); // true
-console.log(isDivisible(100,5)); // true
-console.log(isDivisible(4,4,2,4,4,4,4,4,4)); // true
-console.log(isDivisible(5,2)); // false
-console.log(isDivisible(17,17,17,17)); // true
-console.log(isDivisible(17,1)); // true
+console.log(findUniq([ 1, 0, 0 ])); // 1
+console.log(findUniq([ 0, 1, 0 ])); // 1
+console.log(findUniq([ 0, 0, 1 ])); // 1
+console.log(findUniq([ 1, 1, 1, 2, 1, 1 ])); // 2
+console.log(findUniq([ 1, 1, 2, 1, 1 ])); // 2
+console.log(findUniq([ 3, 10, 3, 3, 3 ])); // 10
