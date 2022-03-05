86b732cbe53c7273479f12d79eb68fd7ea6d879f
Caleb Jones <calebj516@gmail.com>
2022-03-04 22:23:40 -0600

    Challenge: Battle of the characters (Easy)

--- Challenge.js
+++ Challenge.js
@@ -1,20 +1,17 @@
-// Challenge: Battle of the characters (Medium) (7 kyu)
+// Challenge: Battle of the characters (Easy) (7 kyu)
 
 // Description:
 
-// Groups of characters decided to make a battle. Help them to figure out what group is more powerful. Create a function that will accept 2 variables and return the one who's stronger.
+// Groups of characters decided to make a battle. Help them to figure out which group is more powerful. Create a function that will accept 2 variables and return the one who's stronger.
 
 // Rules:
-// Each character has its own power:
-//   A = 1, B = 2, ... Y = 25, Z = 26
-//   a = 0.5, b = 1, ... y = 12.5, z = 13
-// Only alphabetical characters can and will participate in a battle.
+// Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+// Only capital characters can and will participate a battle.
 // Only two groups to a fight.
-// Group whose total power (a + B + c + ...) is bigger wins.
+// Group whose total power (A + B + C + ...) is bigger wins.
 // If the powers are equal, it's a tie.
-
 // Examples:
-//   battle("One", "Two"); // => "Two"`
+//   battle("ONE", "TWO"); // => "TWO"`
 //   battle("ONE", "NEO"); // => "Tie!"
 
 // My code below:
@@ -22,36 +19,20 @@
 function battle(x, y) {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  // Logic: use reduce to determine each character's value and reduce the values into one sum. Compare the two values and determine the result.
-  // Inside the reduce: if the current char is located in alphabet, we know it is uppercase. The value is determined by returning the index and adding one to offset it correctly. Otherwise, it is lowercase and we take the same value as if it were uppercase, but divide by two.
   let xScore = x
     .split("")
-    .reduce(
-      (total, current) =>
-        total +
-        (alphabet.indexOf(current) !== -1
-          ? alphabet.indexOf(current) + 1
-          : (alphabet.indexOf(current.toUpperCase()) + 1) / 2),
-      0
-    );
+    .reduce((total, current) => total + (alphabet.indexOf(current) + 1), 0);
   let yScore = y
     .split("")
-    .reduce(
-      (total, current) =>
-        total +
-        (alphabet.indexOf(current) !== -1
-          ? alphabet.indexOf(current) + 1
-          : (alphabet.indexOf(current.toUpperCase()) + 1) / 2),
-      0
-    );
-  // This statement uses ternary operators to determine which value is greater, or if there is a tie.
+    .reduce((total, current) => total + (alphabet.indexOf(current) + 1), 0);
+
   return xScore > yScore ? x : xScore === yScore ? "Tie!" : y;
 }
 
 // Tests
 
-console.log(battle("One", "Two")); // "Two"
-console.log(battle("One", "Neo")); // "One"
-console.log(battle("One", "neO")); // "Tie!"
-console.log(battle("Foo", "BAR")); // "Tie!"
-console.log(battle("Four", "Five")); // "Four"
+console.log(battle("ONE", "TWO")); // "Two"
+console.log(battle("ONE", "NEO")); // "Tie!"
+console.log(battle("ONE", "NEO")); // "Tie!"
+console.log(battle("FOO", "BAR")); // "FOO"
+console.log(battle("FOUR", "FIVE")); // "FOUR"
