ee2d335efd5b937d870022b9e74809c011e78619
Caleb Jones <calebj516@gmail.com>
2022-03-18 23:59:02 -0500

    Challenge: Filter Unused Digits

--- Challenge.js
+++ Challenge.js
@@ -2,38 +2,28 @@
 
 // Description:
 
-// Given a list of integers, return the digits that are not present in any of them.
+// Welcome to the Codewars Bar!
+// Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
 
-// Example:
+// Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
 
-// [12, 34, 56, 78]  =>  "09"
-// [2015, 8, 26]     =>  "3479"
-// Note: the digits in the resulting string should be sorted.
+// Examples
+// "1 beer"  -->  "1 glass of water"
+// because you drank one standard drink
+
+// "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+// because you drank ten standard drinks
 
 // My code below:
 
-function unusedDigits() {
-  const nums = Object.values(arguments);
-  const ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function hydrate(s) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const formatGlass = (n) => (n > 1 ? "glasses" : "glass");
+  const numberOfDrinks = s.match(/\d/g).map(Number).reduce(reducer);
 
-  let numsSorted = nums
-    .join("")
-    .split("")
-    .sort((num1, num2) => num1 > num2)
-    .map(Number);
-
-  return ints.filter((num) => numsSorted.includes(num) === false).join("");
+  return `${numberOfDrinks} ${formatGlass(numberOfDrinks)} of water`;
 }
 
 // Tests
 
-console.log(unusedDigits([12, 34, 56, 78])); // "09"
-console.log(unusedDigits([2015, 8, 26])); //"3479"
-console.log(unusedDigits([276, 575])); // "013489"
-console.log(unusedDigits([643])); // "0125789"
-console.log(unusedDigits([864, 896, 744])); // "01235"
-console.log(unusedDigits([364, 500, 715, 730])); // "289"
-console.log(unusedDigits([93, 10, 11, 40])); // "25678"
-console.log(unusedDigits([])); // "0123456789"
-console.log(unusedDigits([1, 11, 111, 1111])); // "023456789"
-console.log(unusedDigits([9, 87, 654, 3210])); // ""
+console.log(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
