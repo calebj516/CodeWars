0395021c6bbb3009ab6bf50c40d2a42960c5360c
Caleb Jones <calebj516@gmail.com>
2022-03-21 21:45:03 -0500

    Challenge: Arabian String

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,35 @@
-// Challenge: Filter Unused Digits (7 kyu)
+// Challenge: Arabian String (6 kyu)
 
 // Description:
 
-// Welcome to the Codewars Bar!
-// Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+// You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
 
-// Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+// Don't presume the separators too much or you could be surprised.
 
-// Examples
-// "1 beer"  -->  "1 glass of water"
-// because you drank one standard drink
+// For example: (Input --> Output)
 
-// "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
-// because you drank ten standard drinks
+// "example name" --> "ExampleName"
+// "your-NaMe-here" --> "YourNameHere"
+// "testing ABC" --> "TestingAbc"
 
 // My code below:
 
-function hydrate(s) {
-  // replace all non digit characters with '' which removes them from the string
-  // split into an array
-  // convert each element into a number
-  // reduce the numbers into one number (summing up)
-  // Finally, based on the number of glasses, return a template literal stating 'glasses' (if greater than 1 glass) or 'glass'
-  const numGlasses = s
-    .replace(/\D/g, "")
-    .split("")
-    .map(Number)
-    .reduce((total, currentNum) => total + currentNum);
-  return numGlasses > 1 ? `${numGlasses} glasses of water` : `1 glass of water`;
+function camelize(str) {
+  if (str) {
+    return str
+      .match(/[a-z0-9]+/gi)
+      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join("");
+  }
+  return str;
 }
 
 // Tests
 
-console.log(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"));
+console.log(camelize("john doe")); // "JohnDoe"
+console.log(camelize("frank peas")); // "FrankPeas"
+console.log(camelize("Rugby:Club:2013")); // "RugbyClub2013"
+console.log(camelize("Arabian_String-Test")); // "ArabianStringTest"
+console.log(camelize("Ninja-Test--01")); // "NinjaTest01"
+console.log(camelize("'quOted' => 'What'")); // "QuotedWhat"
+console.log(camelize("dir/for/data")); // "DirForData"
