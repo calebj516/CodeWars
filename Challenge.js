2dc179fe1edf8030b3ddbf5a19ad3160c42a2a03
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-03-24 11:48:29 -0500

    Challenge: Max Diff

--- Challenge.js
+++ Challenge.js
@@ -1,28 +1,29 @@
-// Challenge: Case Swapping (7 kyu)
+// Challenge: Max Diff (7 kyu)
 
 // Description:
 
-// Given a string, swap the case for each of the letters.
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
 
-// e.g. CodEwArs --> cODeWaRS
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
 
 // My code below:
 
-function swap(str) {
-  return str
-    .split("")
-    .map((letter) =>
-      letter === letter.toUpperCase()
-        ? letter.toLowerCase()
-        : letter.toUpperCase()
-    )
-    .join("");
-}
+function maxDiff(list) {
+  
+    if(list.length > 1){
+      let sortedList = list.sort((firstNum, secondNum) => firstNum - secondNum);
+      return sortedList[sortedList.length - 1] - sortedList[0];
+    }
+    
+    return 0;
+  };
 
 // Tests
 
-console.log(swap("")); // ""
-console.log(swap("CodeWars")); // "cODEwARS"
-console.log(swap("abc")); // "ABC"
-console.log(swap("ABC")); // "abc"
-console.log(swap("123235")); // "123235"
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])); // 11
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 16])); // 16
+console.log(maxDiff([16])); // 0
+console.log(maxDiff([])); // 0
