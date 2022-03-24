ee4273bd32f82003a8d4e1bea5c2eae87358277b
Caleb Jones <calebj516@gmail.com>
2022-03-23 22:49:59 -0500

    Challenge: Case Swapping

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,28 @@
-// Challenge: Arabian String (6 kyu)
+// Challenge: Case Swapping (7 kyu)
 
 // Description:
 
-// You must create a method that can convert a string from any format into PascalCase. This must support symbols too.
+// Given a string, swap the case for each of the letters.
 
-// Don't presume the separators too much or you could be surprised.
-
-// For example: (Input --> Output)
-
-// "example name" --> "ExampleName"
-// "your-NaMe-here" --> "YourNameHere"
-// "testing ABC" --> "TestingAbc"
+// e.g. CodEwArs --> cODeWaRS
 
 // My code below:
 
-function camelize(str) {
-  if (str) {
-    return str
-      .match(/[a-z0-9]+/gi)
-      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-      .join("");
-  }
-  return str;
+function swap(str) {
+  return str
+    .split("")
+    .map((letter) =>
+      letter === letter.toUpperCase()
+        ? letter.toLowerCase()
+        : letter.toUpperCase()
+    )
+    .join("");
 }
 
 // Tests
 
-console.log(camelize("john doe")); // "JohnDoe"
-console.log(camelize("frank peas")); // "FrankPeas"
-console.log(camelize("Rugby:Club:2013")); // "RugbyClub2013"
-console.log(camelize("Arabian_String-Test")); // "ArabianStringTest"
-console.log(camelize("Ninja-Test--01")); // "NinjaTest01"
-console.log(camelize("'quOted' => 'What'")); // "QuotedWhat"
-console.log(camelize("dir/for/data")); // "DirForData"
+console.log(swap("")); // ""
+console.log(swap("CodeWars")); // "cODEwARS"
+console.log(swap("abc")); // "ABC"
+console.log(swap("ABC")); // "abc"
+console.log(swap("123235")); // "123235"
