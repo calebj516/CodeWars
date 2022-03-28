f1efd7f2fe8b00816713ce67d2cc0695b7459db7
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-03-27 22:12:58 -0500

    Challenge: Dot Calculator

--- Challenge.js
+++ Challenge.js
@@ -1,22 +1,51 @@
-// Challenge: Max Diff (7 kyu)
+// Challenge: Dot Calculator (7 kyu)
 
 // Description:
 
-// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+// Dot Calculator
+// You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, and dots again after the operator. The dots and the operator will be separated by one space.
 
-// lst contains integers, that means it may contain some negative numbers
-// if lst is empty or contains a single element, return 0
-// lst is not sorted
+// Here are the following valid operators :
+
+// + Addition
+// - Subtraction
+// * Multiplication
+// // Integer Division
+// Your Work (Task)
+// You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+
+// Examples (Input => Output)
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
 
 // My code below:
 
-const maxDiff = (list) =>
-  list.length > 1 ? Math.max(...list) - Math.min(...list) : 0;
+function dotCalculator (equation) {
+  
+  // Use destructuring to obtain the first part, the operator, and the last part of the equation
+  let [ a, op, b ] = equation.split(' ');
+  
+  console.log(op[0]);
+  // repeat the '.' character as many times as the result of the equation
+  return '.'.repeat(eval(a.length + op[0] + b.length));
+  
+}
 
 // Tests
 
-console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])); // 6
-console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])); // 11
-console.log(maxDiff([0, 1, 2, 3, 4, 5, 16])); // 16
-console.log(maxDiff([16])); // 0
-console.log(maxDiff([])); // 0
+console.log(dotCalculator("..... + ...............")); // "...................."
+console.log(dotCalculator("..... - ...")); // ".."
+console.log(dotCalculator("..... - .")); // "...."
+console.log(dotCalculator("..... * ...")); // "..............."
+console.log(dotCalculator("..... * ..")); // ".........."
+console.log(dotCalculator("..... // ..")); // ".."
+console.log(dotCalculator("..... // .")); // "....."
+console.log(dotCalculator(". // ..")); // ""
+console.log(dotCalculator(". - .")); // ""
