4e223b1eed1f7483197064d37edf8cabd0f7c620
Caleb Jones <calebj516@gmail.com>
2022-04-02 21:42:54 -0500

    Codewars Challenge: Mumbling

--- Challenge.js
+++ Challenge.js
@@ -1,52 +1,25 @@
-// Challenge: Numerical Palindrome #2 (6 kyu)
+// Challenge: Mumbling (7 kyu)
 
 // Description:
 
-// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+// This time no story, no theory. The examples below show you how to write function accum:
 
-// 2332
-// 110011
-// 54322345
-
-// For this kata, single digit numbers will not be considered numerical palindromes.
-
-// For a given number num, write a function to test if the number contains a numerical palindrome or not and return a boolean (true if it does and false if does not). Return "Not valid" if the input is not an integer or is less than 0.
-
-// Note: Palindromes should be found without permutating num.
-
-// palindrome(5) => false
-// palindrome(1221) => true
-// palindrome(141221001) => true
-// palindrome(1215) => true 
-// palindrome(1294) => false 
-// palindrome("109982") => "Not valid"
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 // My code below:
 
-function palindrome(num) {
-  if (typeof num !== 'number' || num < 0 || !Number.isInteger(num)) {
-    return 'Not valid';
-  }
-  let arr = String(num).split('');
- 
-   if(arr.length > 1){
-      for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-          if (arr.slice(i, j + 1).reverse().join('') === arr.slice(i, j + 1).join('')) {
-            return true;
-          }
-        }
-     }
-   }
- 
-  return false;
- }
+const accum = (s) =>
+  s
+    .split("") // split s into an array
+    .map((el, idx) => el[0].toUpperCase() + el.repeat(idx).toLowerCase()) // change the first character of each element to uppercase, and concatenate it with the same character in lowercase, repeated the same number of times as the current index
+    .join("-"); // join back into a string, separated by hyphens
 
 // Tests
 
-console.log(palindrome(5)); // => false
-console.log(palindrome(1221)); // => true
-console.log(palindrome(141221001)); // => true
-console.log(palindrome(1215)); // => true 
-console.log(palindrome(1294)); // => false 
-console.log(palindrome("109982")); // => "Not valid"
+console.log(accum("ZpglnRxqenU")); //"Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB")); //"N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU")); //"M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
