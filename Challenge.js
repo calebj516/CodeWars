81bab6c4b5b0522aa78ccda485bc8d99ac0560ff
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-04-04 16:49:47 -0500

    Challenge: Numerical Palindrome #3.5

--- Challenge.js
+++ Challenge.js
@@ -1,54 +1,58 @@
-// Challenge: Numerical Palindrome #3 (6 kyu)
+// Challenge: Numerical Palindrome #3.5 (6 kyu)
 
 // Description:
 
-// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are: 2332, 110011, 54322345
 
-// 2332
-// 110011
-// 54322345
+// For a given number num, write a function which returns an array of all the numerical palindromes contained within each number. The array should be sorted in ascending order and any duplicates should be removed.
 
-// For a given number num, write a function which returns the number of numerical palindromes within each number. For this kata, single digit numbers will NOT be considered numerical palindromes.
+// In this kata, single digit numbers and numbers which start or end with zeros (such as 010 and 00) are NOT considered valid numerical palindromes.
 
-// Return "Not valid" if the input is not an integer or is less than 0.
+// If num contains no valid palindromes, return "No palindromes found". Otherwise, return "Not valid" if the input is not an integer or is less than 0.
 
-// palindrome(5) => 0
-// palindrome(1221) => 2
-// palindrome(141221001) => 5
-// palindrome(1294) => 0
-// palindrome("1221") => "Not valid"
+// Examples
+// palindrome(1221)      =>  [22, 1221]
+// palindrome(34322122)  =>  [22, 212, 343, 22122]
+// palindrome(1001331)   =>  [33, 1001, 1331]
+// palindrome(1294)      =>  "No palindromes found"
+// palindrome("1221")    =>  "Not valid"
 
 // My code below:
 
 function palindrome(num) {
-  if (typeof num !== "number" || num < 0 || !Number.isInteger(num)) {
-    return "Not valid";
+  
+  if (typeof num !== 'number' || num < 0 || !Number.isInteger(num)) {
+    return 'Not valid';
   }
-
-  let arr = [...num.toString()];
-  let count = 0;
-
-  if (arr.length > 1) {
+   
+  let arr = String(num).split('');
+  let result = [];
+ 
+ 
     for (let i = 0; i < arr.length - 1; i++) {
       for (let j = i + 1; j < arr.length; j++) {
-        if (
-          arr
-            .slice(i, j + 1)
-            .reverse()
-            .join("") === arr.slice(i, j + 1).join("")
-        ) {
-          count++;
+        if (arr.slice(i, j + 1).reverse().join('') === arr.slice(i, j + 1).join('')) {
+          let palindrome = arr.slice(i, j + 1).join('');
+          // If the starting and ending numbers are not zero and the result array does not already include the palindrome...
+          // ...push it to the result array.
+          if(palindrome[0] !== '0' && palindrome[palindrome.length - 1] !== '0' && !result.includes(+palindrome)){
+             result.push(+arr.slice(i, j + 1).join(''));
+          }
         }
       }
-    }
-  }
-
-  return count;
-}
+   }
+   // sort results in ascending order
+   result = result.sort((num1, num2) => num1 - num2);
+ 
+   return result.length > 0 ? result : "No palindromes found";
+ }
 
 // Tests
-console.log(palindrome(5)); // 0
-console.log(palindrome(1221)); // 2
-console.log(palindrome(141221001)); // 5
-console.log(palindrome(1294)); // 0
-console.log(palindrome("1221")); // "Not valid"
+console.log(palindrome(2)); // "No palindromes found"
+console.log(palindrome(1551)); // [55,1551]
+console.log(palindrome(221122)); // [11,22,2112,221122]
+console.log(palindrome(10015885)); // [88,1001,5885]
+console.log(palindrome(13598)); // "No palindromes found"
+console.log(palindrome("ACCDDCCA")); // "Not valid"
+console.log(palindrome("1551")); // "Not valid"
+console.log(palindrome(-4505)); // "Not valid"
