1cc3896589eeebd8dc69aea030cf8fc36d117c2b
Caleb Jones <calebj516@gmail.com>
2022-04-16 23:15:07 -0500

    Challenge: Dropcaps

--- Challenge.js
+++ Challenge.js
@@ -1,29 +1,45 @@
-// Challenge: Duplicate Encoder (6 kyu)
+// Challenge: Dropcaps (7 kyu)
 
 // Description:
 
-// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
 
-// Examples
-// "din"      =>  "((("
-// "recede"   =>  "()()()"
-// "Success"  =>  ")())())"
-// "(( @"     =>  "))(("
+// But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+// *should work also on Leading and Trailing Spaces and caps.
+
+// "apple"            => "Apple"
+// "apple of banana"  => "Apple of Banana"
+// "one   space"      => "One   Space
+// "   space WALK   " => "   Space Walk   "
+// Note: you will be provided atleast one word and should take string as input and return string as output.
 
 // My code below:
 
-function duplicateEncode(word) {
-  return word
-    .toLowerCase()
-    .split("")
-    .map((letter, idx, arr) =>
-      arr.indexOf(letter) == arr.lastIndexOf(letter) ? "(" : ")"
-    )
-    .join("");
+function dropCap(n) {
+  n = n.split(" ");
+
+  for (let i = 0; i < n.length; i++) {
+    if (n[i].length > 2) {
+      n[i] = n[i][0].toUpperCase() + n[i].slice(1).toLowerCase();
+    }
+  }
+
+  return n.join(" ");
 }
 
 // Tests
-console.log(duplicateEncode("din")); // "((("
-console.log(duplicateEncode("recede")); // "()()()"
-console.log(duplicateEncode("Success")); // ")())())"
-console.log(duplicateEncode("(( @")); // "))(("
+console.log(dropCap("Apple Banana")); // "Apple Banana"
+console.log(dropCap("Apple")); // "Apple"
+console.log(dropCap("")); // ""
+console.log(dropCap("of")); // "of"
+console.log(
+  dropCap(
+    "Revelation of the contents outraged American public opinion, and helped generate"
+  )
+); //"Revelation of The Contents Outraged American Public Opinion, And Helped Generate"
+console.log(dropCap("more  than    one space between words")); // "More  Than    One Space Between Words"
+console.log(dropCap("  leading spaces")); // "  Leading Spaces"
+console.log(dropCap("trailing spaces   ")); // "Trailing Spaces   "
+console.log(dropCap("ALL CAPS CRAZINESS")); // "All Caps Craziness"
+console.log(dropCap("rAnDoM CaPs CrAzInEsS")); // "Random Caps Craziness"
