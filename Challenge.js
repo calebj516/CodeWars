7256f628ff93e61763afbf8c35006f1028a91e22
Caleb Jones <calebj516@gmail.com>
2022-04-24 22:45:27 -0500

    Challenge: Sum of numerous arguments

--- Challenge.js
+++ Challenge.js
@@ -1,23 +1,35 @@
-// Challenge: Narcissistic Numbers (7 kyu)
+// Challenge: Sum of numerous arguments (7 kyu)
 
 // Description:
 
-// A Narcissistic Number is a number of length n in which the sum of its digits to the power of n is equal to the original number. If this seems confusing, refer to the example below.
+// After calling the function findSum() with any number of non-negative integer arguments, it should return the sum of all those arguments. If no arguments are given, the function should return 0, if negative arguments are given, it should return -1.
 
-// Ex: 153, where n = 3 (number of digits in 153)
-// 13 + 53 + 33 = 153
+// Example
+// findSum(1,2,3,4); // returns 10
+// findSum(1,-2);    // returns -1
+// findSum();        // returns 0
+// Hint: research the arguments object on google or read Chapter 4 from Eloquent Javascript
 
-// Write a method is_narcissistic(i) (in Haskell: isNarcissistic :: Integer -> Bool) which returns whether or not i is a Narcissistic Number.
-
-// My code below:
-
-// Compare n to the result of n converted to a string, split into an array, changing each digit into a number, and using reduce to sum the result of each digit raised to the power of the length of the array.
-
-const isNarcissistic = n => n == n.toString().split('').map(Number).reduce((total, currentNum, idx, arr) => total + currentNum ** arr.length, 0);
+function findSum(...args) {
+  // if there is a negative number, return -1
+  if (args.filter((num) => num < 0).length > 0) {
+    return -1;
+  }
+  // if there are no arguments passed in, return []
+  if (args === []) {
+    return [];
+  }
+  // otherwise return the sum of the numbers passed in
+  return args.reduce((total, currentNum) => total + currentNum, 0);
+}
 
 // Tests
-console.log(isNarcissistic(234229983));       // false
-console.log(isNarcissistic(9926315));         // true
-console.log(isNarcissistic(24678050));        // true
-console.log(isNarcissistic(88593477));        // true
-console.log(isNarcissistic(11513221922401));  // false
+console.log(findSum(1, 3, 5)); // 9, "1 + 3 + 5 = 9"
+console.log(findSum()); // 0, "If no arguments, function should return 0"
+console.log(findSum(1, -2, 4)); // -1, "If negative arguments are passed, function should return -1"
+console.log(findSum(0)); // 0, "The sum of 0 is 0"
+console.log(findSum(1, 1, 5)); // 7, "Your sum is incorrect"
+console.log(findSum(1, 1, 1, 1, 1, 1, 1, 1, 0)); // 8,"Your sum is incorrect"
+
+console.log(findSum(-1, -1, 5)); // -1, "Your sum is incorrect"
+console.log(findSum(-1, -1, -5)); // -1, "Your sum is incorrect"
