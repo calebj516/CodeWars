6fc41ecd12c47d5942b1c2d2d31186fb3cf490cd
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-04-25 22:25:04 -0500

    Challenge: Sum of numerous arguments - Alternate Solution

--- Challenge.js
+++ Challenge.js
@@ -10,26 +10,15 @@
 // findSum();        // returns 0
 // Hint: research the arguments object on google or read Chapter 4 from Eloquent Javascript
 
-function findSum(...args) {
-  // if there is a negative number, return -1
-  if (args.filter((num) => num < 0).length > 0) {
-    return -1;
-  }
-  // if there are no arguments passed in, return []
-  if (args === []) {
-    return [];
-  }
-  // otherwise return the sum of the numbers passed in
-  return args.reduce((total, currentNum) => total + currentNum, 0);
+function findSum(...args){
+  return args.reduce((total, current) => total < 0 || current < 0 ? -1 : total + current, 0);
 }
 
 // Tests
 console.log(findSum(1, 3, 5)); // 9, "1 + 3 + 5 = 9"
-console.log(findSum()); // 0, "If no arguments, function should return 0"
-console.log(findSum(1, -2, 4)); // -1, "If negative arguments are passed, function should return -1"
-console.log(findSum(0)); // 0, "The sum of 0 is 0"
-console.log(findSum(1, 1, 5)); // 7, "Your sum is incorrect"
-console.log(findSum(1, 1, 1, 1, 1, 1, 1, 1, 0)); // 8,"Your sum is incorrect"
-
-console.log(findSum(-1, -1, 5)); // -1, "Your sum is incorrect"
-console.log(findSum(-1, -1, -5)); // -1, "Your sum is incorrect"
+console.log(findSum()); // 0
+console.log(findSum(1, -2, 4)); // -1
+console.log(findSum(0)); // 0
+console.log(findSum(1, 1, 5)); // 7
+console.log(findSum(1, 1, 1, 1, 1, 1, 1, 1, 0)); // 8
+console.log(findSum(-1, -1, 5)); // -1
