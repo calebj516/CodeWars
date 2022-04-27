01e39cbe008852ca454c317142f8c09e38161a4d
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-04-27 12:26:07 -0500

    Challenge: You're a square!

--- Challenge.js
+++ Challenge.js
@@ -1,22 +1,28 @@
-// Challenge: Sort Numbers (7 kyu)
+// Challenge: You're a square! (7 kyu)
 
-// Description:
+// Description: A square of squares
 
-// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
-// For example:
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
 
-// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
-// solution(null); // should return []
+// Task: Given an integral number, determine if it's a square number:
 
-function solution(nums){
-  // if nums is truthy, sort it; otherwise, return an empty array.
-  return nums ? nums.sort((num1, num2) => num1 - num2) : [];
-}
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// My code below:
+
+// Raising n to the power of 0.5 is the same as taking the square root of n.
+// If this results in a whole number ( % 1 will result in 0 if it is a whole number) then we know that n is the square of the result and is therefore a perfect square.
+const isSquare = n => (n ** 0.5) % 1 === 0;
 
 // Tests
-console.log(solution([1,2,3,10,5])); // [1,2,3,5,10]
-console.log(solution(null)); // []
-console.log(solution([])); // []
-console.log(solution([20, 2, 10])); // [2,10,20]
-console.log(solution([2, 20, 10])); // [2,10,20]
+
+console.log(isSquare(-1)); // false
+console.log(isSquare( 0)); // true
+console.log(isSquare( 3)); // false
+console.log(isSquare( 4)); // true
+console.log(isSquare(25)); // true
+console.log(isSquare(26)); // false
