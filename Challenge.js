0fd9a62a36719f34631fbeaf17d47af6d5c098ca
Caleb Jones <calebj516@gmail.com>
2022-05-01 16:58:31 -0500

    Challenge: Credit Card Mask

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,18 @@
-// Challenge: Sum of numerous arguments (7 kyu)
+// Challenge: Credit Card Mask (7 kyu)
 
 // Description:
 
-// After calling the function findSum() with any number of non-negative integer arguments, it should return the sum of all those arguments. If no arguments are given, the function should return 0, if negative arguments are given, it should return -1.
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 
-// Example
-// findSum(1,2,3,4); // returns 10
-// findSum(1,-2);    // returns -1
-// findSum();        // returns 0
-// Hint: research the arguments object on google or read Chapter 4 from Eloquent Javascript
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
-function findSum(...args) {
-  // if there is a negative number, return -1
-  if (args.filter((num) => num < 0).length > 0) {
-    return -1;
-  }
-  // if there are no arguments passed in, return []
-  if (args === []) {
-    return [];
-  }
-  // otherwise return the sum of the numbers passed in
-  return args.reduce((total, currentNum) => total + currentNum, 0);
-}
+const maskify = (cc) =>
+  cc
+    .split("")
+    .map((el, idx, arr) => (idx < arr.length - 4 ? "#" : el))
+    .join("");
 
 // Tests
-console.log(findSum(1, 3, 5)); // 9, "1 + 3 + 5 = 9"
-console.log(findSum()); // 0, "If no arguments, function should return 0"
-console.log(findSum(1, -2, 4)); // -1, "If negative arguments are passed, function should return -1"
-console.log(findSum(0)); // 0, "The sum of 0 is 0"
-console.log(findSum(1, 1, 5)); // 7, "Your sum is incorrect"
-console.log(findSum(1, 1, 1, 1, 1, 1, 1, 1, 0)); // 8,"Your sum is incorrect"
-
-console.log(findSum(-1, -1, 5)); // -1, "Your sum is incorrect"
-console.log(findSum(-1, -1, -5)); // -1, "Your sum is incorrect"
+console.log(maskify("4556364607935616")); // "############5616"
+console.log(maskify("1")); // "1"
+console.log(maskify("11111")); // "#1111"
