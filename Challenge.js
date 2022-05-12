062c59bf16afd8bcc4c0d591926cd58ebc68e71c
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-05-12 11:41:15 -0500

    Challenge: Chess piece values

--- Challenge.js
+++ Challenge.js
@@ -1,24 +1,79 @@
-// Challenge: Find the smallest integer in the array (8 kyu)
+// Challenge: Chess piece values (6 kyu)
 
 // Description:
 
-// Given an array of integers your solution should find the smallest integer.
+// Complete the function piecesValue/pieces_value that accepts two arguments, an 8x8 array (arr),representing a chess board and a string (s). Depending on the value of the string s (which can be either "white" or "black") calculate the value of the pieces on the table for the corresponding player(white or black).
+// Empty fields will be marked as a space " " while the fields with pieces look like this:
 
-// For example:
+// "w-king"   //meaning white king
+// "b-bishop" //a black bishop
+// "w-pawn"   //white pawn
+// ...and so on. Preloaded for you there is an object called hash (values in python):
 
-// Given [34, 15, 88, 2] your solution will return 2
-// Given [34, -345, -1, 100] your solution will return -345
-// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+// const hash = Object.freeze({
+//   queen: 9, rook: 5, bishop: 3, knight: 3, pawn: 1
+// });
+
+// Having the estimated value of each piece. This is a rough estimation and the real piece value depends on other factors in game as well ,such as the game being a closed or open one, which can favor either knights or bishops. (If you are interested more about that here: open vs closed game.) But for our purposes we will use the mentioned values.
+// Note: the value of a king cannot be estimated because without it the game would be over, so DO NOT take into consideration the value of the king.You will not be tested for invalid input.
 
 // My code below:
 
-// spread operator expands the array that is passed in via "args", and Math.min returns the smallest integer
-findSmallestInt = (args) => Math.min(...args);
+function piecesValue(arr, s) {
+  
+    let total = 0;
+    
+    // use a nested for loop to calculate totals
+    // outer loop: each array
+    for(let i = 0; i < arr.length; i++){
+      // filter out spaces in array
+      arr[i] = arr[i].filter(el => el !== ' ');
+      // inner loop: elements in each array
+      for(let j = 0; j < arr[i].length; j++){
+        let piece = arr[i][j];
+        // if the first character of the piece (w or b) matches the first character of s, we add to our total
+        if(piece[0] === s[0]){
+          // if the element passed in to hash produces a number, add it to the total
+          total += (typeof hash[piece.slice(2)] === 'number' ? hash[piece.slice(2)] : 0);
+        }
+      }
+    }
+    
+    return total;
+}
 
 // Tests
 
-console.log(findSmallestInt([78, 56, 232, 12, 8])); // 8
-console.log(findSmallestInt([78, 56, 232, 12, 18])); // 12
-console.log(findSmallestInt([78, 56, 232, 412, 228])); // 56
-console.log(findSmallestInt([78, 56, 232, 12, 0])); // 0
-console.log(findSmallestInt([1, 56, 232, 12, 8])); // 1
+let r1 = [['b-bishop',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ','b-queen',' ',' ',' ',' ','w-queen'],
+[' ','b-king',' ','b-pawn','w-rook',' ',' ',' '],
+[' ',' ',' ',' ','w-pawn',' ',' ',' '],
+[' ',' ',' ',' ',' ','w-bishop',' ',' '],
+['w-king',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ','b-pawn','b-pawn',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ']];
+
+let r2 = [[' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ','b-king',' ',' '],
+[' ','b-knight',' ',' ','w-pawn',' ',' ',' '],
+[' ',' ','w-bishop',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' '],
+[' ','w-pawn',' ',' ',' ','w-pawn',' ',' '],
+[' ',' ',' ',' ','b-pawn',' ',' ',' '],
+[' ','w-rook',' ',' ',' ','w-king',' ',' ']];
+
+let r3 = [[' ',' ',' ',' ','b-king',' ',' ',' '],
+[' ','b-bishop',' ',' ','b-pawn','b-pawn',' ',' '],
+[' ',' ',' ',' ','b-knight',' ',' ',' '],
+[' ',' ','w-queen',' ',' ',' ',' ',' '],
+[' ','w-bishop',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ','b-rook'],
+[' ','w-pawn','w-pawn',' ',' ',' ',' ',' '],
+[' ',' ','w-king',' ',' ',' ',' ',' ']];
+
+console.log(piecesValue(r1,'white')); // 18
+console.log(piecesValue(r1,'black')); // 15
+console.log(piecesValue(r2,'white')); // 11
+console.log(piecesValue(r2,'black')); // 4
+console.log(piecesValue(r3,'white')); // 14
+console.log(piecesValue(r3,'black')); // 13
