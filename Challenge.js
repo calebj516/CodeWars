0eafdc63c61cceea4ec209c09356a89c3b85b38a
Caleb Jones <calebj516@gmail.com>
2022-06-08 22:24:46 -0500

    Challenge: Digital Cypher

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,62 @@
-// Challenge: Ghostbusters (whitespace removal) (7 kyu)
+// Challenge: Digital Cypher (7 kyu)
 
 // Description:
 
-// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+// Digital Cypher assigns to each letter of the alphabet unique number. For example:
 
-// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+//  a  b  c  d  e  f  g  h  i  j  k  l  m
+//  1  2  3  4  5  6  7  8  9 10 11 12 13
+//  n  o  p  q  r  s  t  u  v  w  x  y  z
+// 14 15 16 17 18 19 20 21 22 23 24 25 26
 
-// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+// Instead of letters in encrypted word we write the corresponding number, eg. The word scout:
 
-// Example:
+//  s  c  o  u  t
+// 19  3 15 21 20
+// Then we add to each obtained digit consecutive digits from the key. For example. In case of key equal to 1939 :
 
-// ghostBusters("Sky scra per");
-// Should return:
+//    s  c  o  u  t
+//   19  3 15 21 20
+//  + 1  9  3  9  1
+//  ---------------
+//   20 12 18 30 21
 
-// "Skyscraper"
-// If the building contains no ghosts, return the string:
+//    m  a  s  t  e  r  p  i  e  c  e
+//   13  1 19 20  5 18 16  9  5  3  5
+// +  1  9  3  9  1  9  3  9  1  9  3
+//   --------------------------------
+//   14 10 22 29  6 27 19 18  6  12 8
 
-// "You just wanted my autograph didn't you?"
+// Task
+
+// Write a function that accepts str string and key number and returns an array of integers representing encoded str.
+
+// Input / Output
+
+// The str input string consists of lowercase characters only.
+// The key input number is a positive integer.
+
+// Example
+
+// Encode("scout",1939);  ==>  [ 20, 12, 18, 30, 21]
+// Encode("masterpiece",1939);  ==>  [ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]
 
 // My code below:
 
-// Logic: if the building string is not equal to the same string with whitespace removed, then we know that whitespace characters are present that need to be removed. Else, return the string specified in the instructions.
-const ghostBusters = (building) =>
-  building.replace(/\s/g, "") !== building
-    ? building.replace(/\s/g, "")
-    : "You just wanted my autograph didn't you?";
+function encode(str, n) {
+  // split str into an array and map each code to its position in the alphabet
+  let alphabetPositionArray = str
+    .toLowerCase()
+    .split("")
+    .map((el) => el.charCodeAt(0) - 96);
+  let keyArray = String(n).split("").map(Number);
+  // loop through the result from step 1, adding the value from the key and then repeating
+  return alphabetPositionArray.map(
+    (num, idx) => num + keyArray[idx % keyArray.length]
+  );
+}
 
 // Tests
 
-console.log(ghostBusters("Factor y")); // "Factory"
-console.log(ghostBusters("O  f fi ce")); // "Office"
-console.log(ghostBusters("BusStation")); // "You just wanted my autograph didn't you?"
+console.log(encode("scout", 1939)); // [20, 12, 18, 30, 21]
+console.log(encode("masterpiece", 1939)); // [14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]
