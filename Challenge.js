9386c9e317f2886641dfcb3be8a17d8b843605a1
Caleb Jones <calebj516@gmail.com>
2022-07-01 22:59:25 -0500

    Challenge: Prime Numbers

--- Challenge.js
+++ Challenge.js
@@ -1,22 +1,42 @@
-// Challenge: Evens and Odds (7 kyu)
+// Challenge: Prime Numbers (7 kyu)
 
 // Description:
 
-// This kata is about converting numbers to their binary or hexadecimal representation:
+// In this kata you will create a function to check a non-negative input to see if it is a prime number.
 
-// If a number is even, convert it to binary.
-// If a number is odd, convert it to hex.
-// Numbers will be positive. The hexadecimal string should be lowercased.
+// The function will take in a number and will return True if it is a prime number and False if it is not.
+
+// A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// Examples(input --> output)
+// 0 --> false
+// 1 --> false
+// 2 --> true
+// 11 --> true
+// 12 --> false
 
 // My code below:
 
-const evensAndOdds = (num) => (num % 2 ? num.toString(16) : num.toString(2));
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  // Primality Test: given an input number, n, check whether it is evenly divisible by any prime number between 2 and √n
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 // Tests
 
-console.log(evensAndOdds(2)); // "10"
-console.log(evensAndOdds(13)); // "d"
-console.log(evensAndOdds(47)); // "2f"
-console.log(evensAndOdds(0)); // "0"
-console.log(evensAndOdds(12800)); // "11001000000000"
-console.log(evensAndOdds(8172381723)); // "1e71ca61b"
+console.log(isPrime(0)); // F
+console.log(isPrime(1)); // F
+console.log(isPrime(2)); // T
+console.log(isPrime(11)); // T
+console.log(isPrime(12)); // F
+console.log(isPrime(277)); // T
+console.log(isPrime(281)); // T
+console.log(isPrime(283)); // T
