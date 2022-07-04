d2c0c1d2b7c6618582a9a85fc6a08ea949fa75c6
Caleb Jones <calebj516@gmail.com>
2022-07-03 23:24:28 -0500

    Challenge: Odd-Even String Sort

--- Challenge.js
+++ Challenge.js
@@ -1,22 +1,46 @@
-// Challenge: Simple beads count (7 kyu)
+// Challenge: Odd-Even String Sort (7 kyu)
 
 // Description:
 
-// Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+// Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
 
-// @ @@ @ @@ @ @@ @ @@ @ @@ @
+// Note:
+// 0 is considered to be an even index.
+// All input strings are valid with no spaces
+// input: 'CodeWars'
+// output 'CdWr oeas'
 
-// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// S[0] = 'C'
+// S[1] = 'o'
+// S[2] = 'd'
+// S[3] = 'e'
+// S[4] = 'W'
+// S[5] = 'a'
+// S[6] = 'r'
+// S[7] = 's'
+// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+// And the final string to return is 'Cdwr oeas'
 
-// If there are less than 2 blue beads return 0.
+// Enjoy.
 
 // My code below:
 
-const countRedBeads = (n) => (n < 2 ? 0 : (n - 1) * 2);
+function sortMyString(S) {
+  let evenStr = S.split("")
+    .filter((el, idx) => idx % 2 === 0)
+    .join("");
+  let oddStr = S.split("")
+    .filter((el, idx) => idx % 2 !== 0)
+    .join("");
+  return `${evenStr} ${oddStr}`;
+}
 
 // Tests
 
-console.log(countRedBeads(0)); // 0
-console.log(countRedBeads(1)); // 0
-console.log(countRedBeads(3)); // 4
-console.log(countRedBeads(5)); // 8
+console.log(sortMyString("Wolfeschlegelsteinhausenbergerdorff")); // "Wleclgltihuebredrf ofsheesenasnegrof"
+console.log(sortMyString("METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL")); //"MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY"
+console.log(sortMyString("PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS")); //"PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI"
+console.log(sortMyString("PSEUDOPSEUDOHYPOPARATHYROIDISM")); // "PEDPEDHPPRTYODS SUOSUOYOAAHRIIM"
+console.log(sortMyString("FLOCCINAUCINIHILIPILIFICATION")); // "FOCNUIIIIIIIAIN LCIACNHLPLFCTO"
+console.log(sortMyString("SUBDERMATOGLYPHIC")); // "SBEMTGYHC UDRAOLPI"
