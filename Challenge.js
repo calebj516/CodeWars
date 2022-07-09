9dd52355e5ae11d9fc4ae6edd2acb12698feaec0
Caleb Jones <calebj516@gmail.com>
2022-07-08 21:39:38 -0500

    Challenge: List Filtering

--- Challenge.js
+++ Challenge.js
@@ -1,46 +1,22 @@
-// Challenge: Odd-Even String Sort (7 kyu)
+// Challenge: List Filtering (7 kyu)
 
 // Description:
 
-// Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
-// Note:
-// 0 is considered to be an even index.
-// All input strings are valid with no spaces
-// input: 'CodeWars'
-// output 'CdWr oeas'
-
-// S[0] = 'C'
-// S[1] = 'o'
-// S[2] = 'd'
-// S[3] = 'e'
-// S[4] = 'W'
-// S[5] = 'a'
-// S[6] = 'r'
-// S[7] = 's'
-// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
-// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
-// And the final string to return is 'Cdwr oeas'
-
-// Enjoy.
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 // My code below:
 
-function sortMyString(S) {
-  let evenStr = S.split("")
-    .filter((el, idx) => idx % 2 === 0)
-    .join("");
-  let oddStr = S.split("")
-    .filter((el, idx) => idx % 2 !== 0)
-    .join("");
-  return `${evenStr} ${oddStr}`;
-}
+const filter_list = (l) => l.filter((el) => typeof el === "number");
 
 // Tests
 
-console.log(sortMyString("Wolfeschlegelsteinhausenbergerdorff")); // "Wleclgltihuebredrf ofsheesenasnegrof"
-console.log(sortMyString("METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL")); //"MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY"
-console.log(sortMyString("PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS")); //"PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI"
-console.log(sortMyString("PSEUDOPSEUDOHYPOPARATHYROIDISM")); // "PEDPEDHPPRTYODS SUOSUOYOAAHRIIM"
-console.log(sortMyString("FLOCCINAUCINIHILIPILIFICATION")); // "FOCNUIIIIIIIAIN LCIACNHLPLFCTO"
-console.log(sortMyString("SUBDERMATOGLYPHIC")); // "SBEMTGYHC UDRAOLPI"
+console.log(filter_list([1, 2, "a", "b"])); // [1, 2]
+console.log(filter_list([1, "a", "b", 0, 15])); // [1, 0, 15]
+console.log(filter_list([1, 2, "aasf", "1", "123", 123])); // [1, 2, 123]
+console.log(filter_list(["a", "b", "1"])); // []
+console.log(filter_list([1, 2, "a", "b"])); // [1, 2]
