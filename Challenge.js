f8484aa869537d363d0d5fd5806519ed3e0412f2
Caleb Jones <calebj516@gmail.com>
2022-07-22 21:57:19 -0500

    Challenge: Convert a Boolean to a String

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,16 @@
-// Challenge: All Star Code Challenge #18 (8 kyu)
+// Challenge: Convert a Boolean to a String (8 kyu)
 
 // Description:
 
-// This Kata is intended as a small challenge for my students
+// Implement a function which convert the given boolean value into its string representation.
 
-// All Star Code Challenge #18
-
-// Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
-
-// If no occurrences can be found, a count of 0 should be returned.
-
-// ("Hello", "o")  ==>  1
-// ("Hello", "l")  ==>  2
-// ("", "z")       ==>  0
-// Notes:
-
-// The first argument can be an empty string
-// The second string argument will always be of length 1
+// Note: Only valid inputs will be given.
 
 // My code below:
 
-const strCount = (str, letter) =>
-  str.split("").filter((c) => c === letter).length;
+const booleanToString = (b) => String(b);
 
 // Tests
 
-console.log(strCount("Hello", "o")); // 1
-console.log(strCount("Hello", "l")); // 2
-console.log(strCount("abcedfghijklmnopqrstuvwxyz", "z")); // 1
+console.log(booleanToString(true)); // "true"
+console.log(booleanToString(false)); // "false"
