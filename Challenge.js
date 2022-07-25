b6be41207b037d7878296d7524f8e52b0e519034
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-07-25 14:27:37 -0500

    Challenge: altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- Challenge.js
+++ Challenge.js
@@ -1,16 +1,33 @@
-// Challenge: Convert a Boolean to a String (8 kyu)
+// Challenge: altERnaTIng cAsE <=> ALTerNAtiNG CaSe (8 kyu)
 
 // Description:
 
-// Implement a function which convert the given boolean value into its string representation.
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
 
-// Note: Only valid inputs will be given.
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
 // My code below:
 
-const booleanToString = (b) => String(b);
+String.prototype.toAlternatingCase = function () {
+    // split string into an array, perform operations on each character (lower becomes upper, upper becomes lower), and then join back into a string.
+    return this.split('').map(char => char == char.toLowerCase() ? char.toUpperCase() : char.toLowerCase()).join('');
+}
 
 // Tests
 
-console.log(booleanToString(true)); // "true"
-console.log(booleanToString(false)); // "false"
+console.log("hello world".toAlternatingCase()); // "HELLO WORLD"
+console.log("HELLO WORLD".toAlternatingCase()); // "hello world"
+console.log("hello WORLD".toAlternatingCase()); // "HELLO world"
+console.log("HeLLo WoRLD".toAlternatingCase()); // "hEllO wOrld"
+console.log("12345".toAlternatingCase()); // "12345"
+console.log("1a2b3c4d5e".toAlternatingCase()); // "1A2B3C4D5E"
+console.log("String.prototype.toAlternatingCase".toAlternatingCase()); // "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+console.log("Hello World".toAlternatingCase().toAlternatingCase()); // "Hello World"
