6c39bd97f272daced6c1ad22512d44c31cc9d617
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-07-28 09:36:13 -0500

    Challenge: Bit Counting

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,36 @@
-// Challenge: Reverse list (7 kyu)
+// Challenge: Bit Counting (6 kyu)
 
 // Description:
 
-// Write reverseList function that simply reverses lists.
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
 
-// My code below (solved without using array reverse method):
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
-function reverseList(arr) {
+// My code below:
 
-    const reversedArr = [];
-    
-    if(arr.length > 0){
-      
-      for(let i = arr.length - 1; i >= 0; i--){
-        reversedArr.push(arr[i]);
-      }
-      
-      return reversedArr;
-    }
-  
-    return [];
-}
+const countBits = n => n.toString(2).split('').filter(num => num === '1').length;
 
 // Tests
 
-console.log(reverseList([])); //  []
-console.log(reverseList([1,2,3])); //  [3,2,1]
-console.log(reverseList([1,2,3,4])); //  [4,3,2,1]
-console.log(reverseList([1,2,3,4,5])); //  [5,4,3,2,1]
+console.log(countBits(0)); // 0
+console.log(countBits(4)); // 1
+console.log(countBits(7)); // 3
+console.log(countBits(9)); // 2
+console.log(countBits(10)); // 2
+console.log(countBits(26)); // 3
+console.log(countBits(77231418)); // 14
+console.log(countBits(12525589)); // 11
+console.log(countBits(3811)); // 8
+console.log(countBits(392902058)); // 17
+console.log(countBits(1044)); // 3
+console.log(countBits(10030245)); // 10
+console.log(countBits(183337941)); // 16
+console.log(countBits(20478766)); // 14
+console.log(countBits(103021)); // 9
+console.log(countBits(287)); // 6
+console.log(countBits(115370965)); // 15
+console.log(countBits(31)); // 5
+console.log(countBits(417862)); // 7
+console.log(countBits(626031)); // 12
+console.log(countBits(89)); // 4
+console.log(countBits(674259)); // 10
