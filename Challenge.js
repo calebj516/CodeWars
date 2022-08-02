3ea93034e012bd34cea9f6400f3561fc7ade30ea
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-08-01 21:28:17 -0500

    Challenge: Title Case

--- Challenge.js
+++ Challenge.js
@@ -1,36 +1,36 @@
-// Challenge: Bit Counting (6 kyu)
+// Challenge: Title Case (6 kyu)
 
 // Description:
 
-// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
 
-// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+// ###Arguments (Haskell)
+
+// First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+// Second argument: the original string to be converted.
+// ###Arguments (Other languages)
+
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
 
 // My code below:
 
-const countBits = n => n.toString(2).replace(/0/g, '').length;
+function titleCase(title, minorWords) {
+  const minorArr = minorWords ? minorWords.toLowerCase().split(' ') : [];
+  
+  return title.toLowerCase().split(' ').map((word, idx) => {
+    // if the word is falsy, OR is included in minor words AND is not the first word, return word as-is
+      if (!word || minorArr.includes(word) && idx !== 0) return word;
+    // otherwise return the word with the first letter capitalized
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+}
 
 // Tests
 
-console.log(countBits(0)); // 0
-console.log(countBits(4)); // 1
-console.log(countBits(7)); // 3
-console.log(countBits(9)); // 2
-console.log(countBits(10)); // 2
-console.log(countBits(26)); // 3
-console.log(countBits(77231418)); // 14
-console.log(countBits(12525589)); // 11
-console.log(countBits(3811)); // 8
-console.log(countBits(392902058)); // 17
-console.log(countBits(1044)); // 3
-console.log(countBits(10030245)); // 10
-console.log(countBits(183337941)); // 16
-console.log(countBits(20478766)); // 14
-console.log(countBits(103021)); // 9
-console.log(countBits(287)); // 6
-console.log(countBits(115370965)); // 15
-console.log(countBits(31)); // 5
-console.log(countBits(417862)); // 7
-console.log(countBits(626031)); // 12
-console.log(countBits(89)); // 4
-console.log(countBits(674259)); // 10
+console.log(titleCase('a clash of KINGS', 'a an the of')); // should return: 'A Clash of Kings'
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In')); // should return: 'The Wind in the Willows'
+console.log(titleCase('the quick brown fox')); // should return: 'The Quick Brown Fox'
