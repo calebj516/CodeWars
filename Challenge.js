4b800dfc00697b80f5cb98c466591e6d2641c458
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-08-02 12:07:48 -0500

    Challenge: Message Validator

--- Challenge.js
+++ Challenge.js
@@ -1,36 +1,43 @@
-// Challenge: Title Case (6 kyu)
+// Challenge: Message Validator (6 kyu)
 
 // Description:
 
-// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+// In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
 
-// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+// For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
 
-// ###Arguments (Haskell)
+// Notes:
 
-// First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
-// Second argument: the original string to be converted.
-// ###Arguments (Other languages)
-
-// First argument (required): the original string to be converted.
-// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+// Messages are composed of only letters and digits
+// Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
+// Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
+// If the message is an empty string, you should return true
 
 // My code below:
 
-function titleCase(title, minorWords) {
-  const minorArr = minorWords ? minorWords.toLowerCase().split(' ') : [];
-  
-  return title.toLowerCase().split(' ').map((word, idx) => {
-    // if the word is falsy, OR is included in minor words AND is not the first word, return word as-is
-      if (!word || minorArr.includes(word) && idx !== 0) return word;
-    // otherwise return the word with the first letter capitalized
-      return word[0].toUpperCase() + word.slice(1);
-    })
-    .join(' ');
+function isAValidMessage(message){
+    // if there is a message, proceed
+    if(message){
+      let messageArr = message.split(/(\d+)/);
+    // loop through the message
+      for(let i = 0; i < messageArr.length - 1; i++){
+        // if the element is truthy AND a number, test if its length is equal to the length of the following string
+        if(messageArr[i] && Number.isInteger(+messageArr[i])){
+          if(messageArr[i + 1].length !== +messageArr[i]) return false;
+        }
+      }
+      // if the message is processed without returning false, we know the message is valid and this return statement will run.
+      return true;    
+    }
+    // if the message is falsy (i.e. an empty string), this return statement will run
+    return true;  
 }
 
 // Tests
 
-console.log(titleCase('a clash of KINGS', 'a an the of')); // should return: 'A Clash of Kings'
-console.log(titleCase('THE WIND IN THE WILLOWS', 'The In')); // should return: 'The Wind in the Willows'
-console.log(titleCase('the quick brown fox')); // should return: 'The Quick Brown Fox'
+console.log(isAValidMessage("3hey5hello2hi")); // true
+console.log(isAValidMessage("4code13hellocodewars")); // true
+console.log(isAValidMessage("3hey5hello2hi5")); // false
+console.log(isAValidMessage("code4hello5")); // false
+console.log(isAValidMessage("1a2bb3ccc4dddd5eeeee")); // true
+console.log(isAValidMessage("")); // true
