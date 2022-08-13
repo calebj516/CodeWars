91af736695713c15522515150530519359ede556
Caleb Jones <calebj516@gmail.com>
2022-08-12 21:29:40 -0500

    Challenge: Return the closest number multiple of 10

--- Challenge.js
+++ Challenge.js
@@ -1,38 +1,27 @@
-// Challenge: Sum of the first nth term of Series (7 kyu)
+// Challenge: Return the closest number multiple of 10 (7 kyu)
 
 // Description:
 
-// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// Given a number return the closest number to it that is divisible by 10.
 
-// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
-// Rules:
-// You need to round the answer to 2 decimal places and return it as String.
+// Example input:
 
-// If the given value is 0 then it should return 0.00
+// 22
+// 25
+// 37
 
-// You will only be given Natural Numbers as arguments.
+// Expected output:
 
-// Examples:(Input --> Output)
-
-// 1 --> 1 --> "1.00"
-// 2 --> 1 + 1/4 --> "1.25"
-// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+// 20
+// 30
+// 40
 
 // My code below:
 
-function SeriesSum(n) {
-  let result = [];
-
-  // increment i by three each iteration and therefore up to n * 3 times
-  for (let i = 1; i <= n * 3; i += 3) {
-    result.push(1 / i);
-  }
-
-  return result.reduce((total, current) => total + current, 0).toFixed(2);
-}
+const closestMultiple10 = (num) => Math.round(num / 10) * 10;
 
 // Tests
 
-console.log(SeriesSum(1)); // 1.00
-console.log(SeriesSum(2)); // 1.25
-console.log(SeriesSum(5)); // 1.57
+console.log(closestMultiple10(22)); // 20
+console.log(closestMultiple10(25)); // 30
+console.log(closestMultiple10(37)); // 40
