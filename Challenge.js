f2bd69e962995a9017f73c39907234229771d03a
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-08-15 22:31:02 -0500

    Challenge: Remove First and Last Character

--- Challenge.js
+++ Challenge.js
@@ -1,44 +1,19 @@
-// Challenge: Sum of a sequence (7 kyu)
+// Challenge: Remove First and Last Character (8 kyu)
 
 // Description:
 
-// Your task is to make function, which returns the sum of a sequence of integers.
-
-// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
-
-// If begin value is greater than the end, function should returns 0
-
-// Examples
-
-// 2,2,2 --> 2
-// 2,6,2 --> 12 (2 + 4 + 6)
-// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
-// 1,5,3  --> 5 (1 + 4)
-
-// This is the first kata in the series:
-
-// Sum of a sequence (this kata)
-// Sum of a Sequence [Hard-Core Version]
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
 // My code below:
 
-const sequenceSum = (begin, end, step) => {
-  if (begin > end) return 0;
+// The second paramater in slice counts from the end of the string, so a -1 works (it's equivalent to str.length - 1)
 
-  let result = [];
-
-  for (let i = begin; i <= end; i += step) {
-    result.push(i);
-  }
-
-  return result.reduce((total, current) => total + current, 0);
-};
+const removeChar = str => str.slice(1, -1);
 
 // Tests
 
-console.log(sequenceSum(2, 6, 2)); // 12
-console.log(sequenceSum(1, 5, 1)); // 15
-console.log(sequenceSum(1, 5, 3)); // 5
-console.log(sequenceSum(0, 15, 3)); // 45
-console.log(sequenceSum(16, 15, 3)); // 0
-console.log(sequenceSum(2, 24, 22)); // 26
+console.log(removeChar('eloquent')); // 'loquen'
+console.log(removeChar('country')); // 'ountr'
+console.log(removeChar('person')); // 'erso'
+console.log(removeChar('place')); // 'lac'
+console.log(removeChar('ooopsss')); // 'oopss'
