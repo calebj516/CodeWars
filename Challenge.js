fd237e483730974f5e884927c14206efac54053f
Caleb Jones <calebj516@gmail.com>
2022-09-05 22:05:04 -0400

    Challenge: Exclamation marks series #1: Remove an exclamation mark from the end of string (8 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,32 +1,24 @@
-// Challenge: Ordered Count of Characters (7 kyu)
+// Challenge: Exclamation marks series #1: Remove an exclamation mark from the end of string (8 kyu)
 
 // Description:
 
-// Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
 
-// Example:
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
 
-// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
-
-const orderedCount = function (text) {
-  // Implement me!
-  let textArr = text.split("");
-  let result = [];
-
-  let i = 0;
-
-  while (textArr.length > 0) {
-    // Push an array to result consisting of the character and the character count
-    result.push([textArr[i], textArr.filter((el) => el === textArr[i]).length]);
-    // filter out all occurrences of textArr[i] before the next iteration
-    textArr = textArr.filter((el) => el !== textArr[i]);
-  }
-
-  return result;
-};
+const remove = (s) => s.replace(/!$/, "");
 
 // Tests
 
-console.log(orderedCount("abracadabra")); // [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
-console.log(orderedCount("Code Wars")); // [['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]]
-console.log(orderedCount("233312")); // [['2', 2], ['3', 3], ['1', 1 ]]
+console.log(remove("Hi!")); // "Hi"
+console.log(remove("Hi!!!")); // "Hi!!"
+console.log(remove("!Hi")); // "!Hi"
+console.log(remove("!Hi!")); // "!Hi"
+console.log(remove("Hi! Hi!")); // "Hi! Hi"
+console.log(remove("Hi")); // "Hi"
