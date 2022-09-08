e0a165f64a803b0298e6d4da06334487346c300c
Caleb Jones <calebj516@gmail.com>
2022-09-08 09:42:33 -0500

    Challenge: Exclamation marks series #5: Remove all exclamation marks from the end of words

--- Challenge.js
+++ Challenge.js
@@ -1,26 +1,27 @@
-// Challenge: Exclamation marks series #3: Remove all exclamation marks from sentence except at the end (7 kyu)
+// Challenge: Exclamation marks series #5: Remove all exclamation marks from the end of words
 
 // Description:
 
-// Remove all exclamation marks from sentence except at the end.
+// Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
 
-// Examples
-// remove("Hi!") == "Hi!"
-// remove("Hi!!!") == "Hi!!!"
-// remove("!Hi") == "Hi"
-// remove("!Hi!") == "Hi!"
-// remove("Hi! Hi!") == "Hi Hi!"
-// remove("Hi") == "Hi"
+// Examples:
+
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi Hi"
+// remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
 
 // My code below:
 
-const remove = (s) => s.replace(/!+(?!!*$)/g, "");
+const remove = (s) => s.replace(/\b!+/g, "");
 
 // Tests
 
-console.log(remove("Hi!")); // "Hi!"
-console.log(remove("Hi!!!")); // "Hi!!!"
-console.log(remove("!Hi")); // "Hi"
-console.log(remove("!Hi!")); // "Hi!"
-console.log(remove("Hi! Hi")); // "Hi Hi"
-console.log(remove("Hi")); // "Hi"
+console.log(remove("Hi!")); // "Hi"
+console.log(remove("Hi!!!")); // "Hi"
+console.log(remove("!Hi")); // "!Hi"
+console.log(remove("!Hi!")); // "!Hi"
+console.log(remove("Hi! Hi!")); // "Hi Hi"
+console.log(remove("!!!Hi !!hi!!! !hi")); // "!!!Hi !!hi !hi"
