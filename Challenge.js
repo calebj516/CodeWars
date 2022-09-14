9b5f1911041f5eaeb972fd745d900dc98eb02841
Caleb Jones <calebj516@gmail.com>
2022-09-13 22:30:07 -0500

    Challenge: Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right (8 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,29 @@
-// Challenge: Exclamation marks series #5: Remove all exclamation marks from the end of words
+// Challenge: Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right (8 kyu)
 
 // Description:
 
-// Remove all exclamation marks from the end of words. Words are separated by a single space. There are no exclamation marks within a word.
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
 
 // Examples:
 
-// remove("Hi!") === "Hi"
-// remove("Hi!!!") === "Hi"
-// remove("!Hi") === "!Hi"
-// remove("!Hi!") === "!Hi"
-// remove("Hi! Hi!") === "Hi Hi"
-// remove("!!!Hi !!hi!!! !hi") === "!!!Hi !!hi !hi"
+// remove("Hi!", 1) === "Hi";
+// remove("Hi!", 100) === "Hi";
+// remove("Hi!!!", 1) === "Hi!!";
+// remove("Hi!!!", 100) === "Hi";
+// remove("!Hi", 1) === "Hi";
+// remove("!Hi!", 1) === "Hi!";
+// remove("!Hi!", 100) === "Hi";
+// remove("!!!Hi !!hi!!! !hi", 1) === "!!Hi !!hi!!! !hi";
+// remove("!!!Hi !!hi!!! !hi", 3) === "Hi !!hi!!! !hi";
+// remove("!!!Hi !!hi!!! !hi", 5) === "Hi hi!!! !hi";
+// remove("!!!Hi !!hi!!! !hi", 100) === "Hi hi hi";
 
 // My code below:
 
-const remove = (s) => s.replace(/\b!+/g, "");
+const remove = (s, n) => (n ? remove(s.replace("!", ""), n - 1) : s);
 
 // Tests
 
-console.log(remove("Hi!")); // "Hi"
-console.log(remove("Hi!!!")); // "Hi"
-console.log(remove("!Hi")); // "!Hi"
-console.log(remove("!Hi!")); // "!Hi"
-console.log(remove("Hi! Hi!")); // "Hi Hi"
-console.log(remove("!!!Hi !!hi!!! !hi")); // "!!!Hi !!hi !hi"
+console.log(remove("Hi!", 1)); // "Hi";
+console.log(remove("Hi!!!", 100)); // "Hi";
+console.log(remove("Hi!!!", 1)); // "Hi!!"
