2c2fd18de4d6a26426af499ac95c84df8ccfb183
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-09-15 12:23:44 -0500

    Challenge: Complete The Pattern #1 (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,29 +1,66 @@
-// Challenge: Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right (8 kyu)
+// Challenge: Complete The Pattern #1 (7 kyu)
 
 // Description:
 
-// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+// Note:Returning the pattern is not the same as Printing the pattern.
+// Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+// There are no whitespaces in the pattern.
+
+// Pattern:
+// 1
+// 22
+// 333
+// ....
+// .....
+// nnnnnn
 
 // Examples:
+// pattern(5):
 
-// remove("Hi!", 1) === "Hi";
-// remove("Hi!", 100) === "Hi";
-// remove("Hi!!!", 1) === "Hi!!";
-// remove("Hi!!!", 100) === "Hi";
-// remove("!Hi", 1) === "Hi";
-// remove("!Hi!", 1) === "Hi!";
-// remove("!Hi!", 100) === "Hi";
-// remove("!!!Hi !!hi!!! !hi", 1) === "!!Hi !!hi!!! !hi";
-// remove("!!!Hi !!hi!!! !hi", 3) === "Hi !!hi!!! !hi";
-// remove("!!!Hi !!hi!!! !hi", 5) === "Hi hi!!! !hi";
-// remove("!!!Hi !!hi!!! !hi", 100) === "Hi hi hi";
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// pattern(11):
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+// 10101010101010101010
+// 1111111111111111111111
+
+// Hint: Use \n in string to jump to next line
 
 // My code below:
 
-const remove = (s, n) => (n ? remove(s.replace("!", ""), n - 1) : s);
+function pattern(n){
+    if(n < 1) return "";
+    if(n == 1) return "1";
+    
+    let output="";
+  
+    for(let i = 1; i <= n; i++){
+     i != n ? output += `${i.toString().repeat(i)}\n` : output += `${i.toString().repeat(i)}`;
+    }
+  
+    return output;
+}
 
 // Tests
 
-console.log(remove("Hi!", 1)); // "Hi";
-console.log(remove("Hi!!!", 100)); // "Hi";
-console.log(remove("Hi!!!", 1)); // "Hi!!"
+console.log(pattern(1)); // "1"
+console.log(pattern(2)); // "1\n22"
+console.log(pattern(5)); // "1\n22\n333\n4444\n55555"
+console.log(pattern(0)); // ""
+console.log(pattern(-25)); // ""
+console.log(pattern(-99)); // ""
