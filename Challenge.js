22eac45e0e5e337c941af6eae1e209baa3dd3bd4
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-09-21 16:26:44 -0500

    Challenge: Sum of all arguments (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,27 @@
-// Challenge: Exclamation marks series #9: Remove or add a exclamation mark at the end of words of the sentence (7 kyu)
+// Challenge: Sum of all arguments (7 kyu)
 
 // Description:
 
-// Remove or add a exclamation mark at the end of words of the sentence. Words are separated by spaces in the sentence. That is: If a word has one ! at the end, remove it; If a word has no ! at the end, add a ! to the end; If there are more than one ! at the end of word, keep it.
+// Write a function that finds the sum of all its arguments.
 
-// Examples
-// removeOrAdd("Hi!") === "Hi"
-// removeOrAdd("Hi! Hi!") === "Hi Hi"
-// removeOrAdd("Hi! Hi") === "Hi Hi!"
-// removeOrAdd("Hi! Hi Hi!!") === "Hi Hi! Hi!!"
-// removeOrAdd("!Hi! !Hi !Hi!!") === "!Hi !Hi! !Hi!!"
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+// TIPS:
+
+// ruby/python : http://lmgtfy.com/?q=Ruby+splat+operator
+// JS/Coffeescript : http://lmgtfy.com/?q=Javascript+arguments+variable
+// C: https://www.geeksforgeeks.org/variadic-functions-in-c/
 
 // My code below:
 
-const removeOrAdd = (str) =>
-  str.replace(/\w\b!*/g, (x) =>
-    x.length === 1 ? x + "!" : x.length === 2 ? x[0] : x
-  );
+const sum = (...args) => [...args].reduce((total, currentVal) => total + currentVal);
 
 // Tests
 
-console.log(removeOrAdd("Hi!")); // "Hi"
-console.log(removeOrAdd("Hi! Hi!")); // "Hi Hi"
-console.log(removeOrAdd("Hi! Hi")); // "Hi Hi!"
-console.log(removeOrAdd("Hi! Hi Hi!!")); // "Hi Hi! Hi!!"
-console.log(removeOrAdd("!Hi! !Hi !Hi!!")); // "!Hi !Hi! !Hi!!"
+console.log(sum(1, 2, 3)); // => 6
+console.log(sum(8, 2)); // => 10
+console.log(sum(1, 2, 3, 4, 5)); // => 15
