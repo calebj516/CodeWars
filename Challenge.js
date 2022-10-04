660b53e6d92e0e2b01bed8147b0e53fbb32107d6
Caleb Jones <calebj516@gmail.com>
2022-10-03 21:43:18 -0500

    Challenge: Strong Number (Special Numbers Series #2) - Using Recursion  (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -41,12 +41,7 @@ function strong(n) {
 }
 
 function factorialize(num) {
-  if (num === 0 || num === 1) return 1;
-
-  for (let i = num - 1; i >= 1; i--) {
-    num *= i;
-  }
-  return num;
+  return num > 1 ? num * factorialize(num - 1) : 1;
 }
 
 // Tests
