c9370384e16a0f27959f5bd7a1ec29b606b2a079
Caleb Jones <calebj516@gmail.com>
2022-11-01 20:01:23 -0500

    Challenge: Remove anchor from URL (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,19 @@
-// Challenge: Persistent Bugger (6 kyu)
+// Challenge: Remove anchor from URL (7 kyu)
 
 // Description:
 
-// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
 
-// For example (Input --> Output):
-
-// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
-// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
-// 4 --> 0 (because 4 is already a one-digit number)
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
 // My code below:
 
-function persistence(num) {
-  
-  let count = 0;
-  num = num.toString();
-  
-  while(num.length > 1) {
-    num = num.split('').map(Number).reduce((total, current) => total * current).toString();
-    count++;
-  }
-  
-  return count;
-}
+const removeUrlAnchor = (url) => url.split("#")[0];
 
 // Tests
 
-console.log(persistence(39)); // 3
-console.log(persistence(4)); // 0
-console.log(persistence(25)); // 2
-console.log(persistence(999)); // 4
+console.log(removeUrlAnchor("www.codewars.com#about")); // "www.codewars.com"
+console.log(removeUrlAnchor("www.codewars.com/katas/?page=1#about")); // "www.codewars.com/katas/?page=1"
+console.log(removeUrlAnchor("www.codewars.com/katas/")); // "www.codewars.com/katas/"
