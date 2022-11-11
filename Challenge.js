adcc5c3a93de47f88797464021dde0f6b0232c5e
Caleb Jones <calebj516@gmail.com>
2022-11-10 21:33:17 -0600

    Challenge: Find the missing letter (6 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,36 @@
-// Challenge: Squares sequence (7 kyu)
+// Challenge: Find the missing letter (6 kyu)
 
 // Description:
 
-// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+// Find the missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
 
-// Examples
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
 
-// 2, 5  -->  [2, 4, 16, 256, 65536]
-// 3, 3  -->  [3, 9, 81]
+// Example:
+
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
 
 // My code below:
 
-function squares(x, n) {
-  const result = [];
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let currentCharCode = array[i].charCodeAt();
+    let nextCharCode = array[i + 1].charCodeAt();
 
-  for (let i = 0; i < n; i++) {
-    result.push(x);
-    x *= x;
+    if (nextCharCode - currentCharCode > 1) {
+      currentCharCode++;
+      return String.fromCharCode(currentCharCode);
+    }
   }
 
-  return result;
+  throw new Error("Invalid Input!");
 }
 
 // Tests
 
-console.log(squares(2, 5)); // [2, 4, 16, 256, 65536]
-console.log(squares(3, 3)); // [3, 9, 81]
-console.log(squares(5, 3)); // [5, 25, 625]
-console.log(squares(10, 4)); // [10, 100, 10000, 100000000]
+console.log(findMissingLetter(["a", "b", "c", "d", "f"])); // "e"
+console.log(findMissingLetter(["O", "Q", "R", "S"])); // "P"
