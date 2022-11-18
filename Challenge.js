046ea95be653113757606a5bf9ffc858a22207c9
Caleb Jones <calebj516@gmail.com>
2022-11-17 22:22:38 -0600

    Challenge: Good vs Evil (6 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,36 +1,75 @@
-// Challenge: Find the missing letter (6 kyu)
+// Challenge: Good vs Evil (6 kyu)
 
 // Description:
 
-// Find the missing letter
-// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different races will certainly be involved. Each race has a certain worth when battling against others. On the side of good we have the following races, with their associated worth:
 
-// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
-// The array will always contain letters in only one case.
+// Hobbits: 1
+// Men: 2
+// Elves: 3
+// Dwarves: 3
+// Eagles: 4
+// Wizards: 10
+// On the side of evil we have:
 
-// Example:
+// Orcs: 1
+// Men: 2
+// Wargs: 2
+// Goblins: 2
+// Uruk Hai: 3
+// Trolls: 5
+// Wizards: 10
+// Although weather, location, supplies and valor play a part in any battle, if you add up the worth of the side of good and compare it with the worth of the side of evil, the side with the larger worth will tend to win.
 
-// ['a','b','c','d','f'] -> 'e'
-// ['O','Q','R','S'] -> 'P'
-// (Use the English alphabet with 26 letters!)
+// Thus, given the count of each of the races on the side of good, followed by the count of each of the races on the side of evil, determine which side wins.
+
+// Input:
+// The function will be given two parameters. Each parameter will be a string of multiple integers separated by a single space. Each string will contain the count of each race on the side of good and evil.
+
+// The first parameter will contain the count of each race on the side of good in the following order:
+
+// Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+// The second parameter will contain the count of each race on the side of evil in the following order:
+
+// Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+// All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+
+// Output:
+// Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
 
 // My code below:
 
-function findMissingLetter(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    let currentCharCode = array[i].charCodeAt();
-    let nextCharCode = array[i + 1].charCodeAt();
+function goodVsEvil(good, evil) {
+  const goodValues = [1, 2, 3, 3, 4, 10];
+  const evilValues = [1, 2, 2, 2, 3, 5, 10];
 
-    if (nextCharCode - currentCharCode > 1) {
-      currentCharCode++;
-      return String.fromCharCode(currentCharCode);
-    }
+  const sumGood = good
+    .split(" ")
+    .map((el, idx) => +el * goodValues[idx])
+    .reduce((prev, current) => prev + current);
+  const sumEvil = evil
+    .split(" ")
+    .map((el, idx) => +el * evilValues[idx])
+    .reduce((prev, current) => prev + current);
+
+  if (sumGood > sumEvil) {
+    return "Battle Result: Good triumphs over Evil";
+  } else if (sumGood < sumEvil) {
+    return "Battle Result: Evil eradicates all trace of Good";
+  } else {
+    return "Battle Result: No victor on this battle field";
   }
-
-  throw new Error("Invalid Input!");
 }
 
 // Tests
 
-console.log(findMissingLetter(["a", "b", "c", "d", "f"])); // "e"
-console.log(findMissingLetter(["O", "Q", "R", "S"])); // "P"
+console.log(goodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1")); // "Battle Result: Evil eradicates all trace of Good"
+console.log(goodVsEvil("1 1 1 1 1 1", "0 1 1 1 1 1 1")); // "Battle Result: Evil eradicates all trace of Good"
+console.log(goodVsEvil("1 0 0 0 0 0", "0 1 0 0 0 0 0")); // "Battle Result: Evil eradicates all trace of Good"
+console.log(goodVsEvil("0 1 0 0 0 0", "0 0 0 0 1 0 0")); // "Battle Result: Evil eradicates all trace of Good"
+console.log(goodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0")); // "Battle Result: Good triumphs over Evil"
+console.log(goodVsEvil("1 1 1 1 1 1", "0 0 1 1 1 1 1")); // "Battle Result: Good triumphs over Evil"
+console.log(goodVsEvil("0 1 0 0 0 0", "1 0 0 0 0 0 0")); // "Battle Result: Good triumphs over Evil"
+console.log(goodVsEvil("1 0 0 0 0 0", "1 0 0 0 0 0 0")); // "Battle Result: No victor on this battle field"
+console.log(goodVsEvil("0 0 0 0 0 0", "0 0 0 0 0 0 0")); // "Battle Result: No victor on this battle field"
+console.log(goodVsEvil("1 1 1 1 1 1", "1 0 1 1 1 1 1")); // "Battle Result: No victor on this battle field"
