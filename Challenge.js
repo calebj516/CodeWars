a94d75604cb77f164313803a8cf20a3718897869
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-01-30 21:25:12 -0600

    Challenge: Growth of a Population (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,28 +1,54 @@
-// Challenge: Isograms (7 kyu)
+// Challenge: Growth of a Population (7 kyu)
 
 // Description:
 
-// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
 
-// Example: (Input --> Output)
+// At the end of the first year there will be: 
+// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
 
-// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// At the end of the 2nd year there will be: 
+// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
 
-// isIsogram "Dermatoglyphics" = true
-// isIsogram "moose" = false
-// isIsogram "aba" = false
+// At the end of the 3rd year there will be:
+// 1141 + 1141 * 0.02 + 50 => 1213
+
+// It will need 3 entire years.
+// More generally given parameters:
+
+// p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+// Examples:
+// nb_year(1500, 5, 100, 5000) -> 15
+// nb_year(1500000, 2.5, 10000, 2000000) -> 10
+// Note:
+// Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
 
 // My code below:
 
-const isIsogram = str => new Set(str.toLowerCase()).size == str.length;
+function nbYear(p0, percent, aug, p) {
+
+  let years = 0;
+  const multiplier = 1 + (percent / 100);
+  
+  while (p0 < p) {
+    p0 = Math.floor(p0 * multiplier + aug);
+    years++;
+  }
+  
+  return years;
+}
 
 // Tests
 
-console.log(isIsogram("Dermatoglyphics")); // true 
-console.log(isIsogram("isogram")); // true
-console.log(isIsogram("moose")); // false
-console.log(isIsogram("isIsogram")); // false
-console.log(isIsogram("aba")); // false
-console.log(isIsogram("moOse")); // false
-console.log(isIsogram("thumbscrewjapingly")); // true
-console.log(isIsogram("")); // true
+console.log(nbYear(1500, 5, 100, 5000)); // 15
+console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
+console.log(nbYear(1500000, 0.25, 1000, 2000000)); // 94
+console.log(nbYear(1500000, 0.25, -1000, 2000000)); // 151
+console.log(nbYear(1500000, 0.25, 1, 2000000)); // 116
+console.log(nbYear(1500000, 0.0, 10000, 2000000)); // 50
+console.log(nbYear(1000, 2.0, 50, 1214)); // 4
