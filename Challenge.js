9b85d95416bcee9ce17cc023e2e42edcfb641c05
Caleb Jones <calebj516@gmail.com>
2023-02-07 22:06:44 -0600

    Challenge: Detect Pangram (6 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,52 +1,22 @@
-// Challenge: Growth of a Population (7 kyu)
+// Challenge: Detect Pangram (6 kyu)
 
 // Description:
 
-// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 
-// At the end of the first year there will be: 
-// 1000 + 1000 * 0.02 + 50 => 1070 inhabitants
-
-// At the end of the 2nd year there will be: 
-// 1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
-
-// At the end of the 3rd year there will be:
-// 1141 + 1141 * 0.02 + 50 => 1213
-
-// It will need 3 entire years.
-// More generally given parameters:
-
-// p0, percent, aug (inhabitants coming or leaving each year), p (population to equal or surpass)
-
-// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
-
-// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
-
-// Examples:
-// nb_year(1500, 5, 100, 5000) -> 15
-// nb_year(1500000, 2.5, 10000, 2000000) -> 10
-// Note:
-// Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
 // My code below:
 
-function nbYear(p0, percent, aug, p) {
-  
-  let years = 0, multiplier = 1 + (percent / 100);
+function isPangram(string) {
+  const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
 
-  for(years; p0 < p; years++) {
-    p0 = Math.floor(p0 * multiplier + aug);
-  }
-
-  return years;
+  return alphabet.every((char) => string.toLowerCase().includes(char));
 }
 
 // Tests
 
-console.log(nbYear(1500, 5, 100, 5000)); // 15
-console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
-console.log(nbYear(1500000, 0.25, 1000, 2000000)); // 94
-console.log(nbYear(1500000, 0.25, -1000, 2000000)); // 151
-console.log(nbYear(1500000, 0.25, 1, 2000000)); // 116
-console.log(nbYear(1500000, 0.0, 10000, 2000000)); // 50
-console.log(nbYear(1000, 2.0, 50, 1214)); // 4
+console.log(isPangram("This isn't a pangram!")); // false
+console.log(isPangram("abcdefghijklmopqrstuvwxyz ")); // false
+console.log(isPangram("Cwm fjord bank glyphs vext quiz")); // true
+console.log(isPangram("How quickly daft jumping zebras vex.")); // true
