8fb2694321b4b87fa9c547667bc63d5a0ec1d490
Caleb Jones <calebj516@gmail.com>
2023-02-24 20:52:31 -0600

    Challenge: Sum a list but ignore any duplicates (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,44 +1,39 @@
-// Challenge: Race Ceremony (7 kyu)
+// Challenge: Sum a list but ignore any duplicates (7 kyu)
 
 // Description:
 
-// The national go-kart racing competition is taking place at your local town and you've been called for building the winners podium with the available wooden blocks. Thankfully you are in a wood-rich area, number of blocks are always at least 6.
+// Please write a function that sums a list, but ignores any duplicate items in the list.
 
-// Remember a classic racing podium has three platforms for first, second and third place. First place is the highest and second place is higher than third. Also notice that platforms are arranged as 2nd - 1st - 3rd.
-
-// The organizers want a podium that satisfies:
-
-// The first place platform has the minimum height possible
-// The second place platform has the closest height to first place
-// All platforms have heights greater than zero.
-// Task
-// Given the numbers of blocks available, return an array / tuple or another data structure depending on the language (refer sample tests) with the heights of 2nd, 1st, 3rd places platforms.
-
-// Examples (input -> output)
-// 11 ->   [4, 5, 2]
-// 6  ->   [2, 3, 1]
-// 10 ->   [4, 5, 1]
+// For instance, for the list [3, 4, 3, 6] , the function should return 10.
 
 // My code below:
 
-const racePodium = (blocks) => {
-  let first = Math.ceil(blocks / 3) + 1;
-  let second = first - 1;
-  let third = blocks - (first + second);
+const sumNoDuplicates = (numList) => {
+  const numListFiltered = numList.filter(
+    (num, idx, arr) => arr.indexOf(num) == arr.lastIndexOf(num)
+  );
 
-  if (third === 0) {
-    third++;
-    second--;
-  }
-
-  return [second, first, third];
+  return numListFiltered.reduce((total, current) => total + current, 0);
 };
+
+// Notes on logic
+
+// including only the numbers that have an equal index and last index (i.e. the number in question) excludes duplicates
 
 // Tests
 
-console.log(racePodium(11)); // [4, 5, 2]
-console.log(racePodium(6)); // [2, 3, 1]
-console.log(racePodium(10)); // [4, 5, 1]
-console.log(racePodium(100000)); // [33334, 33335, 33331]
-console.log(racePodium(7)); // [2, 4, 1]
-console.log(racePodium(8)); // [3, 4, 1]
+console.log(
+  sumNoDuplicates([0, 1, 4, 4, 0, 4, 2, 5, 9, 0, 10, 9, 0, 1, 2]) // 15
+);
+
+console.log(
+  sumNoDuplicates([7, 2, 10, 9, 10, 2, 7, 3, 10, 8, 2, 5]) // 25
+);
+
+console.log(
+  sumNoDuplicates([7, 2, 0, 3, 5, 7, 8, 3, 2, 10, 9, 5]) // 27
+);
+
+console.log(
+  sumNoDuplicates([1, 9, 9, 5, 7, 7, 6, 1, 5, 6]) // 0
+);
