4a4f3e6e9a291e68f7e7c383b50128046c3a9373
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-03-01 15:15:50 -0600

    Challenge: Page replacement algorithms: FIFO (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,19 +1,44 @@
-// Challenge: Sum a list but ignore any duplicates (7 kyu)
+// Challenge: Page replacement algorithms: FIFO (7 kyu)
 
 // Description:
 
-// Please write a function that sums a list, but ignores any duplicate items in the list.
+// In a computer operating system that uses paging for virtual memory management, page replacement algorithms decide which memory pages to page out when a page of memory needs to be allocated. Page replacement happens when a requested page is not in memory (page fault) and a free page cannot be used to satisfy the allocation, either because there are none, or because the number of free pages is lower than some threshold.
 
-// For instance, for the list [3, 4, 3, 6] , the function should return 10.
+// The FIFO page replacement algorithm
+// The first-in, first-out (FIFO) page replacement algorithm is a low-overhead algorithm that requires little bookkeeping on the part of the operating system. The idea is obvious from the name: the operating system keeps track of all the pages in memory in a queue, with the most recent arrival at the back, and the oldest arrival in front. When a page needs to be replaced, the oldest page is selected. Note that a page already in the queue is not pushed at the end of the queue if it is referenced again.
+
+// Your task is to implement this algorithm. The fifo function will take two parameters as input: the number of maximum pages that can be kept in memory at the same time n and a reference list containing numbers. Every number represents a page request for a specific page (you can see this number as the unique ID of a page). The expected output is the status of the memory after the application of the algorithm. Note that when a page is inserted in the memory, it stays at the same position until it is removed from the memory by a page fault.
+
+// Example:
+// Given:
+
+// N = 3,
+// REFERENCE LIST = [1, 2, 3, 4, 2, 5],
+
+//   * 1 is read, page fault --> memory = [1];
+//   * 2 is read, page fault --> memory = [1, 2];
+//   * 3 is read, page fault --> memory = [1, 2, 3];
+//   * 4 is read, page fault --> memory = [4, 2, 3];
+//   * 2 is read, already in memory, nothing happens;
+//   * 5 is read, page fault --> memory = [4, 5, 3].
+
+// So, at the end we have the list [4, 5, 3], which is what you have to return. If not all the slots in the memory get occupied after applying the algorithm, fill the remaining slots (at the end of the list) with -1 to represent emptyness (note that the IDs will always be >= 1).
 
 // My code below:
 
-const sumNoDuplicates = (numList) => {
-  const numListFiltered = numList.filter(
-    (num, idx, arr) => arr.indexOf(num) == arr.lastIndexOf(num)
-  );
+const fifo = (n, referenceList) => {
+  const result = new Array(n).fill(-1);
+  let index = 0;
 
-  return numListFiltered.reduce((total, current) => total + current, 0);
+  referenceList.foreach(num => {
+    if(!result.includes(num)) {
+      result[index] = num;
+      // determine value of next index
+      index = (index + 1) % n;
+    }
+  });
+
+  return result;
 };
 
 // Notes on logic
@@ -22,18 +47,10 @@ const sumNoDuplicates = (numList) => {
 
 // Tests
 
-console.log(
-  sumNoDuplicates([0, 1, 4, 4, 0, 4, 2, 5, 9, 0, 10, 9, 0, 1, 2]) // 15
-);
-
-console.log(
-  sumNoDuplicates([7, 2, 10, 9, 10, 2, 7, 3, 10, 8, 2, 5]) // 25
-);
-
-console.log(
-  sumNoDuplicates([7, 2, 0, 3, 5, 7, 8, 3, 2, 10, 9, 5]) // 27
-);
-
-console.log(
-  sumNoDuplicates([1, 9, 9, 5, 7, 7, 6, 1, 5, 6]) // 0
-);
+console.log(fifo(3, [1, 2, 3, 4, 2, 5])); // [4, 5, 3]
+console.log(fifo(5, [])); // [-1, -1, -1, -1, -1]
+console.log(fifo(4, [1, 2, 3, 3, 4, 5, 1])); // [5, 1, 3, 4]
+console.log(fifo(4, [1, 1, 1, 2, 2, 3])); // [1, 2, 3, -1]
+console.log(fifo(1, [5, 4, 3, 3, 4, 10],)); // [10]
+console.log(fifo(3, [1, 1, 1, 1, 1, 1, 1, 1])); // [1, -1, -1]
+console.log(fifo(5, [10, 9, 8, 7, 7, 8, 7, 6, 5, 4, 3, 4, 3, 4, 5, 6, 5])); // [5, 4, 3, 7, 6]
