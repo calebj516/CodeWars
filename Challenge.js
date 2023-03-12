bf6581f6f43c6fc5c0788eac0a87b577cc5e0cf1
Caleb Jones <calebj516@gmail.com>
2023-03-11 22:00:16 -0600

    Challenge: Factorial (Recursive)

--- Challenge.js
+++ Challenge.js
@@ -1,19 +1,13 @@
-// Challenge: Calculate Greatest Common Denominator
+// Challenge: Factorial
 
 // Description: n/a
 
 // My code below:
 
-function calcGcd(a, b) {
-  if (b != 0) {
-    return calcGcd(b, a % b);
-  } else {
-    return a;
-  }
-}
+const factorial = (n) => (n >= 1 ? n * factorial(n - 1) : 1);
 
 // Tests
 
-console.log(calcGcd(108, 48)); // 12
-console.log(calcGcd(632, 150)); // 2
-console.log(calcGcd(256, 60)); // 4
+console.log(factorial(6)); // 720
+console.log(factorial(5)); // 120
+console.log(factorial(4)); // 24
