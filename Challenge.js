d8d2afb53f4b2676fb1c215f20b62d269df163ba
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-03-18 14:35:18 -0500

    Challenge: IPv4 to int32 (6 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,13 +1,27 @@
-// Challenge: Factorial
+// Challenge: IPv4 to int32 (6 kyu)
 
-// Description: n/a
+// Description:
+
+// Take the following IPv4 address: 128.32.10.1 This address has 4 octets where each octet is a single byte (or 8 bits).
+
+// 1st octet 128 has the binary representation: 10000000
+// 2nd octet 32 has the binary representation: 00100000
+// 3rd octet 10 has the binary representation: 00001010
+// 4th octet 1 has the binary representation: 00000001
+// So 128.32.10.1 == 10000000.00100000.00001010.00000001
+
+// Because the above IP address has 32 bits, we can represent it as the 32 bit number: 2149583361.
+
+// Write a function ip_to_int32(ip) ( JS: ipToInt32(ip) ) that takes an IPv4 address and returns a 32 bit number.
+
+//   ipToInt32("128.32.10.1") => 2149583361
 
 // My code below:
 
-const factorial = (n) => (n >= 1 ? n * factorial(n - 1) : 1);
+const ipToInt32 = (ip) => parseInt(ip.split('.').map(n => Number(n).toString(2).padStart(8, '0')).join('') , 2);
 
 // Tests
 
-console.log(factorial(6)); // 720
-console.log(factorial(5)); // 120
-console.log(factorial(4)); // 24
+console.log(ipToInt32("128.32.10.1")); // 2149583361
+console.log(ipToInt32("128.114.17.104")); // 2154959208
+console.log(ipToInt32("0.0.0.0")); // 0
