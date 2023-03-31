002a4b6cf43ec42e1ccc40e36282be72b1c71185
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-03-30 20:13:12 -0500

    Challenge: Insert Dashes (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,27 @@
-// Challenge: Decreasing Inputs (7 kyu)
+// Challenge: Insert Dashes (7 kyu)
 
 // Description:
 
-// This kata is all about adding numbers.
+// Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
 
-// You will create a function named add. It will return the sum of all the arguments. Sounds easy, doesn't it?
-
-// Well Here's the Twist. The inputs will gradually decrease with their index as parameter to the function.
-
-//   add(3,4,6); 
-//   /*
-//   returns ( 3 / 1 ) + ( 4 / 2 ) + ( 6 / 3 ) = 7
-//   */
-// Remember the function will return 0 if no arguments are passed and it must round the result if sum is a float.
-
-// Example
-
-//   add(); //=> 0
-//   add(1,2,3); //=> 3
-//   add(1,4,-6,20); //=> 6
-// Check my another kata here!! http://www.codewars.com/kata/555b73a81a6285b6ce000047
+// Note that the number will always be non-negative (>= 0).
 
 // My code below:
 
-const add = (...args) => Math.round(args.reduce((total, current, idx) => total + current / (idx + 1), 0));
+const insertDash = num => {
+  num = num.toString().split('');
+
+  for(let i = 0; i < num.length; i++) {
+    if(parseInt(num[i]) % 2 > 0 && parseInt(num[i + 1]) % 2 > 0) {
+      num[i] += '-';
+    }
+  }
+  
+  return num.join('');
+}
 
 // Tests
 
-console.log(add(100, 200, 300)); // 300
-console.log(add(2)); // 2
-console.log(add(4, -3, -2)); // 2
-console.log(add(-1, -2, -3, -4)); // -4
+console.log(insertDash(454793)); // '4547-9-3'
+console.log(insertDash(123456)); // '123456'
+console.log(insertDash(1003567)); // '1003-567'
