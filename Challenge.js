f79f45a7384786373811a93723253a68b37bd644
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-04-01 20:58:53 -0500

    Challenge: Keep the Order (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,20 +1,36 @@
-// Challenge: How many consecutive numbers are needed? (7 kyu)
+// Challenge: Keep the Order (7 kyu)
 
 // Description:
 
-// Create the function consecutive(arr) that takes an array of integers and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number.
+// Your job here is to write a function (keepOrder in JS/CoffeeScript, keep_order in Ruby/Crystal/Python, keeporder in Julia), which takes a sorted array ary and a value val, and returns the lowest index where you could insert val to maintain the sorted-ness of the array. The input array will always be sorted in ascending order. It may contain duplicates.
 
-// For example:
+// Do not modify the input.
 
-// If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Numbers in arr will be unique.
+// Some examples:
+// keepOrder([1, 2, 3, 4, 7], 5) //=> 4
+//                       ^(index 4)
+// keepOrder([1, 2, 3, 4, 7], 0) //=> 0
+//           ^(index 0)
+// keepOrder([1, 1, 2, 2, 2], 2) //=> 2
+//                 ^(index 2)
+// Also check out my other creations — Naming Files, Elections: Weighted Average, Identify Case, Split Without Loss, Adding Fractions, Random Integers, Implement String#transpose, Implement Array#transpose!, Arrays and Procs #1, and Arrays and Procs #2.
+
+// If you notice any issues or have any suggestions/comments whatsoever, please don't hesitate to mark an issue or just comment. Thanks!
 
 // My code below:
 
-const consecutive = arr => arr.length ? Math.max(...arr) - Math.min(...arr) - arr.length + 1 : 0;
+const keepOrder = (arr, val) => {
+  let idx = arr.findIndex(n => val <= n);
+  return idx < 0 ? arr.length : idx;
+}
+
+// Logic:
+
+// The findIndex() method returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.
+// If -1 is returned, we know all the values in arr are greater than val, and the first index for which that is true would be 0.
 
 // Tests
 
-console.log(consecutive([4,8,6])); //2
-console.log(consecutive([1,2,3,4])); // 0
-console.log(consecutive([])); // 0
-console.log(consecutive([1])); // 0
+console.log(keepOrder([1, 2, 3, 4, 7], 5)); //=> 4
+console.log(keepOrder([1, 2, 3, 4, 7], 0)); //=> 0
+console.log(keepOrder([1, 1, 2, 2, 2], 2)); //=> 2
