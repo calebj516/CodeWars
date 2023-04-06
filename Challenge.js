e35829e81645d9ad0a63183b9b46526a85df1a5f
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-04-06 08:48:46 -0500

    Challenge: Largest Elements (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,21 @@
-// Challenge: Sum even numbers (7 kyu)
+// Challenge: Largest Elements (7 kyu)
 
 // Description:
 
-// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+// Write a program that outputs the top n elements from a list.
 
-// Only numbers without decimals like 4 or 4.0 can be even.
+// Example:
 
-// The input is a sequence of numbers: integers and/or floats.
+// largest(2, [7,6,5,4,3,2,1])
 
-// Examples
-// [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
-// []                               -->  0
+// => [6,7]
 
 // My code below:
 
-const sumEvenNumbers = (numbers) => {
-  let evenSum = 0;
-
-  for(let num of numbers) {
-    if(!(num % 2)) evenSum += num;
-  }
-
-  return evenSum;
-}
-
-// Note on logic:
-
-// For even numbers, num % 2 will equal 0, but since 0 is considered a falsy value in JS the ! operator is needed to reverse it to a truthy value.
+const largest = (n, array) => n ? [...array].sort((a, b) => a - b).slice(-n) : [];
 
 // Tests
 
-console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 30
-console.log(sumEvenNumbers([1337, 374, 849, 22.5, 19, 16, 0, 0, 16, 32])); // 438
-console.log(sumEvenNumbers([-16, -32, 20, 21, 41, 42])); // 14
+console.log(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]); // [9, 10]
+console.log(0, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]); // []
+console.log(2, [-3, -2, -1, 0, -9, -8, -7, -6, -4, -5]); // [-1, 0]
