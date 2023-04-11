65af3ccbb2579774fa3830c8682d46638cc1f7cb
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-04-11 09:08:48 -0500

    Challenge: No oddities here (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,16 @@
-// Challenge: Between Extremes (7 kyu)
+// Challenge: No oddities here (7 kyu)
 
 // Description:
 
-// Given an array of numbers, return the difference between the largest and smallest values.
+// Write a small function that returns the values of an array that are not odd.
 
-// For example:
-
-// [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
-
-// [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
-
-// The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+// All values in the array will be integers. Return the good values in the order they are given.
 
 // My code below:
 
-const betweenExtremes = (numbers) => findMaxNum(numbers) - findMinNum(numbers);
-
-const findMinNum = (nums) => {
-  return [...nums].sort((a, b) => a - b)[0];
-};
-
-const findMaxNum = (nums) => {
-  return [...nums].sort((a, b) => a - b)[nums.length - 1];
-};
+const noOdds = values => values.filter(num => !(num % 2));
 
 // Tests
 
-console.log(betweenExtremes([-23, 23])); // 46
-console.log(betweenExtremes([21, 34, 54, 43, 26, 12])); // 42
-console.log(betweenExtremes([-1, -41, -77, -100])); // 99
+console.log(noOdds([0,1])); // [0]
+console.log(noOdds([0,1,2,3])); // [0,2]
