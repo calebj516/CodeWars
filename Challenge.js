5a9a844821014f9aa9fcab5c6bfcfbe3ebb6a40d
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-04-12 21:40:33 -0500

    Challenge: Fix My Phone Numbers! (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,16 +1,31 @@
-// Challenge: No oddities here (7 kyu)
+// Challenge: Fix My Phone Numbers! (7 kyu)
 
 // Description:
 
-// Write a small function that returns the values of an array that are not odd.
+// Fix My Phone Numbers
+// Oh thank goodness you're here! The last intern has completely ruined everything!
 
-// All values in the array will be integers. Return the good values in the order they are given.
+// All of our customer's phone numbers have been scrambled, and we need those phone numbers to annoy them with endless sales calls!
+
+// The Format
+// Phone numbers are stored as strings and comprise 11 digits, eg '02078834982' and must always start with a 0.
+
+// However, something strange has happened and now all of the phone numbers contain lots of random characters, whitespace and some are not phone numbers at all!
+
+// For example, '02078834982' has somehow become 'efRFS:)0207ERGQREG88349F82!' and there are lots more lines that we need to check.
+
+// The Task
+// Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".
 
 // My code below:
 
-const noOdds = values => values.filter(num => !(num % 2));
+const isItANum = str =>{
+  let nums = str.replace(/\D/g, '');
+  return nums.length == 11 && nums[0] == '0' ? nums : "Not a phone number";
+}
 
 // Tests
 
-console.log(noOdds([0,1])); // [0]
-console.log(noOdds([0,1,2,3])); // [0,2]
+console.log(isItANum("02078834982")); // "02078834982"
+console.log(isItANum("102078834982")); // "Not a phone number"
+console.log(isItANum("efRFS:)0207ERGQREG88349F82!")); // "02078834982"
