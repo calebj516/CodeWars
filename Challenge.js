14cc60740a447e28ef3b46e684f5808bc4b495c3
Caleb Jones <calebj516@gmail.com>
2023-05-05 22:38:46 -0500

    Challenge: Holiday III - Fire on the boat (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,38 +1,31 @@
-// Challenge: Alphabet war (7 kyu)
+// Challenge: Holiday III - Fire on the boat (7 kyu)
 
 // Description:
 
-// There is a war and nobody knows - the alphabet war!
-// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+// Enjoying your holiday, you head out on a scuba diving trip!
 
-// Task
-// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+// Disaster!! The boat has caught fire!!
 
-// The left side letters and their power:
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
 
-//  w - 4
-//  p - 3
-//  b - 2
-//  s - 1
-// The right side letters and their power:
-
-//  m - 4
-//  q - 3
-//  d - 2
-//  z - 1
+// Go to work!
 
 // My code below:
 
-const alphabetWar = fight => {
-  const scoring = {'w' : -4, 'p' : -3, 'b' : -2, 's' : -1, 'm' : 4, 'q' : 3, 'd' : 2, 'z' : 1};
-  let score = fight.replace(/[^wpbsmqdz]/g, '').split('').reduce((total, current) => total + scoring[current], 0);
-
-  return score < 0 ? "Left side wins!" : score > 0 ? "Right side wins!" : "Let's fight again!";
-}
+const fireFight = (s) => s.replace(/Fire/g, "~~");
 
 // Tests
 
-console.log(alphabetWar("z")); //=> Right side wins!
-console.log(alphabetWar("zdqmwpbs")); //=> Let's fight again!
-console.log(alphabetWar("zzzzs")); //=> Right side wins!
-console.log(alphabetWar("wwwwwwz"));  //=> Left side wins!
+console.log(
+  fireFight(
+    "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"
+  )
+); // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
+console.log(fireFight("Mast Deck Engine Water Fire")); // "Mast Deck Engine Water ~~"
+console.log(
+  fireFight(
+    "Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"
+  )
+); // ~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
+console.log(fireFight("Boat Deck Boat")); // "Boat Deck Boat"
+console.log(fireFight("Fire Boat Captain")); // "~~ Boat Captain"
