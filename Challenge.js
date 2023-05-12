0cce7ac1a311ffc849fb5fc8342596ab9ab7deb3
Caleb Jones <calebj516@gmail.com>
2023-05-11 23:29:44 -0500

    Challenge: Sort rectangles and circles by area II (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,31 +1,30 @@
-// Challenge: Holiday III - Fire on the boat (7 kyu)
+// Challenge: Sort rectangles and circles by area II (7 kyu)
 
 // Description:
 
-// Enjoying your holiday, you head out on a scuba diving trip!
+// In this kata you will be given a sequence of the dimensions of rectangles ( sequence with width and length ) and circles ( radius - just a number ).
+// Your task is to return a new sequence of dimensions, sorted ascending by area.
 
-// Disaster!! The boat has caught fire!!
+// For example,
 
-// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
-
-// Go to work!
+// const array = [ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ]; // [ rectangle, circle, circle, rectangle ]
+// sortByArea(array) => [ [ 1.342, 3.212 ], 1.23, [ 4.23, 6.43 ], 3.444 ]
+// This kata inspired by Sort rectangles and circles by area.
 
 // My code below:
 
-const fireFight = (s) => s.replace(/Fire/g, "~~");
+function sortByArea(array) {
+  return [...array].sort((a, b) => area(a) - area(b));
+}
+
+function area(element) {
+  return element.length === 2
+    ? element[0] * element[1]
+    : Math.PI * element ** 2;
+}
 
 // Tests
 
-console.log(
-  fireFight(
-    "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"
-  )
-); // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
-console.log(fireFight("Mast Deck Engine Water Fire")); // "Mast Deck Engine Water ~~"
-console.log(
-  fireFight(
-    "Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"
-  )
-); // ~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
-console.log(fireFight("Boat Deck Boat")); // "Boat Deck Boat"
-console.log(fireFight("Fire Boat Captain")); // "~~ Boat Captain"
+console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]])); // [[1.342, 3.212],1.23,[4.23, 6.43],3.444,]
+console.log(sortByArea([[2, 5], 6])); // [[2, 5], 6]
+console.log(sortByArea([])); // []
