920f6454fd0686935bee2479f659b964cbf60c8d
Caleb Jones <calebj516@gmail.com>
2023-05-12 23:14:56 -0500

    Challenge: The Office I - Outed (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,83 @@
-// Challenge: Sort rectangles and circles by area II (7 kyu)
+// Challenge: The Office I - Outed (7 kyu)
 
 // Description:
 
-// In this kata you will be given a sequence of the dimensions of rectangles ( sequence with width and length ) and circles ( radius - just a number ).
-// Your task is to return a new sequence of dimensions, sorted ascending by area.
+// Your colleagues have been looking over you shoulder. When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
 
-// For example,
+// In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
 
-// const array = [ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ]; // [ rectangle, circle, circle, rectangle ]
-// sortByArea(array) => [ [ 1.342, 3.212 ], 1.23, [ 4.23, 6.43 ], 3.444 ]
-// This kata inspired by Sort rectangles and circles by area.
+// Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. Else return 'Nice Work Champ!'.
+
+// Happiness rating will be total score / number of people in the room.
+
+// Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+
+// The Office II - Boredom Score
+// The Office III - Broken Photocopier
+// The Office IV - Find a Meeting Room
+// The Office V - Find a Chair
 
 // My code below:
 
-function sortByArea(array) {
-  return [...array].sort((a, b) => area(a) - area(b));
-}
+function outed(meet, boss) {
+  const scores = Object.values(meet);
+  let totalScore =
+    scores.reduce((total, current) => total + current, 0) + meet[boss];
+  let happinessRating = totalScore / scores.length;
 
-function area(element) {
-  return element.length === 2
-    ? element[0] * element[1]
-    : Math.PI * element ** 2;
+  return happinessRating <= 5 ? "Get Out Now!" : "Nice Work Champ!";
 }
 
 // Tests
 
-console.log(sortByArea([[4.23, 6.43], 1.23, 3.444, [1.342, 3.212]])); // [[1.342, 3.212],1.23,[4.23, 6.43],3.444,]
-console.log(sortByArea([[2, 5], 6])); // [[2, 5], 6]
-console.log(sortByArea([])); // []
+console.log(
+  outed(
+    {
+      tim: 0,
+      jim: 2,
+      randy: 0,
+      sandy: 7,
+      andy: 0,
+      katie: 5,
+      laura: 1,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 0,
+    },
+    "laura"
+  )); // "Get Out Now!"
+console.log(
+  outed(
+    {
+      tim: 1,
+      jim: 3,
+      randy: 9,
+      sandy: 6,
+      andy: 7,
+      katie: 6,
+      laura: 9,
+      saajid: 9,
+      alex: 9,
+      john: 9,
+      mr: 8,
+    },
+    "katie"
+  )); // "Nice Work Champ!"
+console.log(
+  outed(
+    {
+      tim: 2,
+      jim: 4,
+      randy: 0,
+      sandy: 5,
+      andy: 8,
+      katie: 6,
+      laura: 2,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 8,
+    },
+    "john"
+  )); // "Get Out Now!"
