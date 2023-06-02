06cf7d99299c4e662f7746dc8eaa25967917b62d
Caleb Jones <calebj516@gmail.com>
2023-06-01 21:48:04 -0500

    Challenge: Playing with Sets: Equal or Not? (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,26 +1,66 @@
-// Challenge: Sum - Square Even, Root Odd (7 kyu)
+// Challenge: Playing with Sets: Equal or Not? (7 kyu)
 
 // Description:
 
-// Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+// Set objects are new JavaScript built-in objects defined since ECMAScript 2015
 
-// The list will never be empty and will only contain values that are greater than or equal to zero.
+// A Set lets you store unique values of any type. It comes with some useful methods like .add, .clear, .has . . . BUT some "Set operations" are missing, like . . .
 
-// Good luck!
+// Equality
+
+// Two sets ( A, B ) are considered "equal" if all elements of A are elements of B and all elements of B are elements of A no matter "order" of elements.
+
+// Examples:
+//   {1, 2} == {2, 1}
+//   {1, 2} != {1, 2, 3}
+// Task
+// Create 2 functions, areEqual and notEqual, getting 2 sets as arguments and returning a true or false depending if these 2 sets are "equal" (respectively not equal) ie : if all elements of 1st set are elements of 2d and all elements of 2d set are elements of 1st.
+
+// Examples:
+// A  = new Set([1,2]);
+// A2 = new Set([2,1]);
+// B  = new Set([2,3]);
+
+// areEqual(A,B)  // -> false
+// areEqual(A,A2) // -> true
+// notEqual(A,B)  // -> true
+ 
+
+// " May the Code be with you ! "
 
 // My code below:
 
-const sumSquareEvenRootOdd = numbers => {
-  let total = 0;
+function areEqual(s1, s2) {
+  // Take the result of the notEqual function and reverse the logic:
+  // If notEqual returns true, s1 and s2 are not equal so we need areEqual to return false
+  return !notEqual(s1, s2);
+}
 
-  for (let num of numbers) {
-    total += (num % 2 ? num ** 0.5 : num ** 2);
+function notEqual(s1, s2) {
+  // If the lengths are different we know s1 != s2
+  if(s1.length != s2.length) return true;
+  
+  let s1Arr = Array.from(s1);
+  let s2Arr = Array.from(s2);
+  
+  // Loop through s1. If s2 does not contain any value from s1, s1 != s2
+  for(let num of s1Arr) {
+    if(!s2Arr.includes(num)) return true;
   }
-
-  return total.toFixed(2);
+  // Loop through s2. If s1 does not contain any value from s2, s1 != s2
+  for(let num of s2Arr) {
+    if(!s1Arr.includes(num)) return true;
+  }
+  // If we get to this point, we have established that every value in each set is within the other set. The two sets are NOT equal.
+  return false;
 }
 
 // Tests
 
-console.log(sumSquareEvenRootOdd([1, 14, 9, 8, 17, 21])); // 272.71
-console.log(sumSquareEvenRootOdd([4, 5, 7, 8, 1, 2, 3, 0])); // 91.61
+let A1 = new Set([1,2,3]), A2 = new Set([3,2,1]), B = new Set([1,2,5]), D = new Set(["1","2","3"]);
+  
+console.log(areEqual(A1, A1)); // "A == A" True
+console.log(areEqual(A1, A2)); // "{1,2,3} == {3,2,1}" True
+console.log(areEqual(A2, A1)); // "{3,2,1} == {1,2,3}" True
+console.log(areEqual(A1, B)); // "{1,2,3} != {1,2,5}" False
+console.log(areEqual(A1, D)); // "{1,2,3} != {'1','2','3'}" False
