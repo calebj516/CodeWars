8e39d4fa1ce51f554c7202c677557a3326b5c77e
Caleb Jones <calebj516@gmail.com>
2023-06-17 21:03:44 -0500

    Challenge: Find the position! (8 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,35 +1,23 @@
-// Challenge: Exclusive "or" (xor) Logical Operator (8 kyu)
+// Challenge: Find the position! (8 kyu)
 
 // Description:
 
-// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+// When provided with a letter, return its position in the alphabet.
 
-// false xor false == false // since both are false
-// true xor false == true // exactly one of the two expressions are true
-// false xor true == true // exactly one of the two expressions are true
-// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+// Input :: "a"
 
-// Task:
-
-// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+// Ouput :: "Position of alphabet: 1"
 
 // My code below:
 
-const xor = (a, b) => Boolean((a || b) && !(a && b));
+const position = letter => `Position of alphabet: ${letter.toLowerCase().charCodeAt() - 96}`;
 
 // Notes on logic:
 
-// a || b will return true if a or b evaluates to true; otherwise, false.
-
-// a && b tests if a && b are both the same boolean value; therefore, adding the ! operator tests that a and b are not the same boolean value.
-
-// Wrapping these conditional statements in a Boolean constructor ensures that the function returns a boolean value.
+// The ASCII character codes for the alphabet start at 97, therefore subtracting 96 obtains the position in the alphabet.
 
 // Tests
 
-console.log(xor(false, false)); // false "false xor false"
-console.log(xor(true, false)); // true, "true xor false"
-console.log(xor(false, true)); // true, "false xor true"
-console.log(xor(true, true)); // false, "true xor true"
-console.log(xor('', 5)); // true, "false xor true"
-console.log(xor(0, '')); // false, "false xor false"
+console.log(position("a")); // "Position of alphabet: 1"
+console.log(position("z")); // "Position of alphabet: 26"
+console.log(position("e")); // "Position of alphabet: 5"
