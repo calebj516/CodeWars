5277f1d1fbc3ce0e4f2855ffc9e948b6dfaeb0e1
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-07-01 21:09:38 -0500

    Challenge: Reversed Words (8 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,17 +1,29 @@
-// Challenge: Calculate Average (8 kyu)
+// Challenge: Reversed Words (8 kyu)
 
 // Description:
 
-// Write a function which calculates the average of the numbers in a given list.
+// Complete the solution so that it reverses all of the words within the string passed in.
 
-// Note: Empty arrays should return 0.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
 // My code below:
 
-const findAverage = (array) => array.length > 0 ? array.reduce((total, current) => total + current, 0) / array.length : 0;
+const reverseWords = str => str.split(' ').reverse().join(' ');
+
+// Notes on logic:
+
+// In order to reverse the words, which are separated by spaces in str, we need to specify this as the delimiter in the split and join functions.
+// Not specifying this, and doing something like split('') and join(''), will reverse the letters and cause the solution to fail.
 
 // Tests
 
-console.log(findAverage([1, 1, 1])); // 1
-console.log(findAverage([1, 2, 3])); // 2
-console.log(findAverage([1, 2, 3, 4])); // 2.5
+console.log(reverseWords("hello world!"                 )); //  "world! hello"
+console.log(reverseWords("yoda doesn't speak like this" )); //  "this like speak doesn't yoda"
+console.log(reverseWords("foobar"                       )); //  "foobar"
+console.log(reverseWords("kata editor"                  )); //  "editor kata"
+console.log(reverseWords("row row row your boat"        )); //  "boat your row row row"
+console.log(reverseWords(""                             )); //  ""
