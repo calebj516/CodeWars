50f0aa2ebf37255a3bb3f1df136d147739253cef
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-07-07 21:56:53 -0500

    Challenge: Will there be enough space? (8 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,24 +1,32 @@
-// Challenge: Reverse List Order (8 kyu)
+// Challenge: Will there be enough space? (8 kyu)
 
 // Description:
 
-// Numbers ending with zeros are boring.
+// The Story:
+// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
 
-// They might be fun in your world, but not here.
+// Task Overview:
+// You have to write a function that accepts three parameters:
 
-// Get rid of them. Only the ending ones.
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 
-// 1450 -> 145
-// 960000 -> 96
-// 1050 -> 105
-// -1050 -> -105
-// Zero alone is fine, don't worry about it. Poor guy anyway
+// Usage Examples:
+// cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
+// cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
 // My code below:
 
-const reverseList = (list) => list.reverse();
+function enough(cap, on, wait) {
+  let availableSeats = cap - on;
+  let remainingSeats = wait - availableSeats;
+  return remainingSeats > 0 ? remainingSeats : 0;
+}
 
 // Tests
 
-console.log(reverseList([1, 2, 3, 4])); // [4,3,2,1]
-console.log(reverseList([3, 1, 5, 4])); // [4,5,1,3]
+console.log(enough(10, 5, 5)); // 0
+console.log(enough(100, 60, 50)); // 10
+console.log(enough(20, 5, 5)); // 0
