97196e64229ecc4661468981e98296bf43a2f443
Caleb Jones <calebj516@gmail.com>
2023-07-15 12:44:44 -0500

    Challenge: Help Suzuki count his vegetables.... (7 kyu) - Unoptimized Solution

--- Challenge.js
+++ Challenge.js
@@ -1,56 +1,69 @@
-// Challenge: Suzuki needs help lining up his students! (7 kyu)
+// Challenge: Help Suzuki count his vegetables.... (7 kyu)
 
 // Description:
 
-// Suzuki needs help lining up his students!
+// Help Suzuki count his vegetables....
 
-// Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+// Suzuki is the master monk of his monastery so it is up to him to ensure the kitchen is operating at full capacity to feed his students and the villagers that come for lunch on a daily basis.
 
-// You will be given a string of student names. Sort them and return a list of names in descending order.
+// This week there was a problem with his deliveries and all the vegetables became mixed up. There are two important aspects of cooking in his kitchen, it must be done in harmony and nothing can be wasted. Since the monks are a record keeping people the first order of business is to sort the mixed up vegetables and then count them to ensure there is enough to feed all the students and villagers.
 
-// Here is an example input:
+// You will be given a string with the following vegetables:
 
-// string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
-// Here is an example return from your function:
+// "cabbage", "carrot", "celery", "cucumber", "mushroom", "onion", "pepper", "potato", "tofu", "turnip"
+// Return a list of tuples with the count of each vegetable in descending order. If there are any non vegetables mixed in discard them. If the count of two vegetables is the same sort in reverse alphabetical order (Z->A).
 
-//  lst = ['Takehiko',
-//         'Takayuki',
-//         'Takahiro',
-//         'Takeshi',
-//         'Takeshi',
-//         'Takashi',
-//         'Tadashi',
-//         'Takeo',
-//         'Takao']
-// Names of equal length will be returned in reverse alphabetical order (Z->A) such that:
-
-// string = "xxa xxb xxc xxd xa xb xc xd"
-// Returns
-
-// ['xxd', 'xxc', 'xxb', 'xxa', 'xd', 'xc', 'xb', 'xa']
+// (119, "pepper"),
+// (114, "carrot"),
+// (113, "turnip"),
+// (102, "onion"),
+// (101, "tofu"),
+// (100, "cabbage"),
+// (93, "mushroom"),
+// (90, "cucumber"),
+// (88, "potato"),
+// (80, "celery")
 // Please also try the other Kata in this series..
 
-// Help Suzuki count his vegetables...
 // Help Suzuki purchase his Tofu!
 // Help Suzuki pack his coal basket!
 // Help Suzuki rake his garden!
+// Suzuki needs help lining up his students!
 // How many stairs will Suzuki climb in 20 years?
 
 // My code below:
 
-const lineupStudents = (students) => students.split(' ').sort((a, b) => b.length - a.length || b.localeCompare(a));
-
-// Notes on logic:
-
-// If two names are of equal length, the left half of the logical OR operator will return 0, which will cause the right half to be returned, which is known as short-circuit evaluation in Computer Science. The expression, b.localeCompare(a)), will sort the equal length names in reverse alphabetical order.
+function countVegetables(string){
+  
+  const results = [];
+  const validVegetables = ["cabbage", "carrot", "celery", "cucumber", "mushroom", "onion", "pepper", "potato", "tofu", "turnip"]; 
+  const vegetableList = string.split(' ');
+  
+  // filter out non-vegetables from the vegetableList
+  vegetableList.filter(el => validVegetables.includes(el));
+  
+  // include only the valid vegetables that appear in vegetableList in the valid vegetables array
+  validVegetables.filter(el => vegetableList.includes(el));
+  
+  // step through vegetableList and push the vegetable count and vegetable to results array
+  for(let i = 0; i < validVegetables.length; i++) {
+    results.push([vegetableList.filter(el => el == validVegetables[i]).length, validVegetables[i]]);  
+  }
+  
+  // return the results sorted in desc order based on count, or reverse alphabetical if count is equivalent
+  return results.sort((a, b) => b[0] - a[0] || b[1].localeCompare(a[1]));
+}
 
 // Tests
 
-let s1 = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi';
+let s1 = "potato tofu cucumber cabbage turnip pepper onion carrot celery mushroom potato tofu cucumber cabbage";
+console.log(countVegetables(s1)); // [[2, 'tofu'], [2, 'potato'], [2, 'cucumber'], [2, 'cabbage'], [1, 'turnip'], [1, 'pepper'], [1, 'onion'], [1, 'mushroom'], [1, 'celery'], [1, 'carrot']];
 
-let s2 = 'Michio Miki Mikio Minori Minoru Mitsuo Mitsuru Nao Naoki Naoko Noboru Nobu Nobuo Nobuyuki Nori Norio Osamu Rafu Raiden Ringo Rokuro Ronin Ryo Ryoichi Ryota Ryozo Ryuichi Ryuu Saburo Sadao Samuru Satoru Satoshi Seiichi Seiji Senichi Shichiro Shig Shigekazu Shigeo Shigeru Shima Shin Shinichi Shinji Shiro Shoichi Shoji Shuichi Shuji Shunichi Susumu Tadao Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi Takumi Tama Tamotsu Taro Tatsuo Tatsuya Teruo Tetsip Tetsuya Tomi Tomio Toru Toshi Toshiaki Toshihiro Toshio Toshiyuki Toyo Tsuneo Tsutomu Tsuyoshi Uyeda Yasahiro Yasuhiro Yasuo Yasushi Yemon Yogi Yoichi Yori Yoshi Yoshiaki Yoshihiro Yoshikazu Yoshimitsu Yoshinori Yoshio Yoshiro Yoshito Yoshiyuki Yuichi Yuji Yuki';
+let s2 = "mushroom chopsticks chopsticks turnip mushroom carrot mushroom cabbage mushroom carrot tofu pepper cabbage potato cucumber " +
+"mushroom mushroom mushroom potato turnip chopsticks cabbage celery celery turnip pepper chopsticks potato potato onion cabbage cucumber " +
+"onion pepper onion cabbage potato tofu carrot cabbage cabbage turnip mushroom cabbage cabbage cucumber cabbage chopsticks turnip pepper " +
+"onion pepper onion mushroom turnip carrot carrot tofu onion tofu chopsticks chopsticks chopsticks mushroom cucumber chopsticks carrot " +
+"potato cabbage cabbage carrot mushroom chopsticks mushroom celery turnip onion carrot turnip cucumber carrot potato mushroom turnip " +
+"mushroom cabbage tofu turnip turnip turnip mushroom tofu potato pepper turnip potato turnip celery carrot turnip"
 
-console.log(lineupStudents(s1)); // ['Takehiko','Takayuki','Takahiro','Takeshi','Takeshi','Takashi','Tadashi','Takeo','Takao'];
-console.log(lineupStudents(s2));
-// ['Yoshimitsu', 'Yoshiyuki', 'Yoshinori', 'Yoshikazu', 'Yoshihiro', 'Toshiyuki', 'Toshihiro', 'Shigekazu', 'Yoshiaki', 'Yasuhiro', 'Yasahiro', 'Tsuyoshi', 'Toshiaki', 'Takehiko', 'Takayuki', 'Takahiro', 'Shunichi', 'Shinichi', 'Shichiro', 'Nobuyuki', 'Yoshito', 'Yoshiro', 'Yasushi', 'Tsutomu', 'Tetsuya', 'Tatsuya', 'Tamotsu', 'Takeshi', 'Takeshi', 'Takashi', 'Tadashi', 'Shuichi', 'Shoichi', 'Shigeru', 'Senichi', 'Seiichi', 'Satoshi', 'Ryuichi', 'Ryoichi', 'Mitsuru', 'Yuichi', 'Yoshio', 'Yoichi', 'Tsuneo', 'Toshio', 'Tetsip', 'Tatsuo', 'Takumi', 'Susumu', 'Shinji', 'Shigeo', 'Satoru', 'Samuru', 'Saburo', 'Rokuro', 'Raiden', 'Noboru', 'Mitsuo', 'Minoru', 'Minori', 'Michio', 'Yoshi', 'Yemon', 'Yasuo', 'Uyeda', 'Toshi', 'Tomio', 'Teruo', 'Takeo', 'Takao', 'Tadao', 'Shuji', 'Shoji', 'Shiro', 'Shima', 'Seiji', 'Sadao', 'Ryozo', 'Ryota', 'Ronin', 'Ringo', 'Osamu', 'Norio', 'Nobuo', 'Naoko', 'Naoki', 'Mikio', 'Yuki', 'Yuji', 'Yori', 'Yogi', 'Toyo', 'Toru', 'Tomi', 'Taro', 'Tama', 'Shin', 'Shig', 'Ryuu', 'Rafu', 'Nori', 'Nobu', 'Miki', 'Ryo', 'Nao'];
-
+console.log(countVegetables(s2)); // [[15, 'turnip'], [15, 'mushroom'], [13, 'cabbage'], [10, 'carrot'], [9, 'potato'], [7, 'onion'], [6, 'tofu'], [6, 'pepper'], [5, 'cucumber'], [4, 'celery']];
