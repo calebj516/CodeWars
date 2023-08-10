5e66741952c0d3535ff85bbd54ad6f7b9a3b78d7
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-08-09 22:24:35 -0500

    Challenge: Short Long Short (8 kyu) - Solution 2

--- Challenge.js
+++ Challenge.js
@@ -14,7 +14,7 @@
 
 // Challenge code below:
 
-const solution = (a, b) => a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`;
+const solution = (a, b) => a.length > b.length ? b + a + b : a + b + a;
 
 // Tests
 
