0eb0a09c9280ffbaa4c9563638734593e8dc6293
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-08-14 19:53:37 -0500

    Challenge: Array Array Array (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,25 +1,43 @@
-// Challenge: Printing Array elements with Comma delimiters (8 kyu)
+// Challenge: Array Array Array (7 kyu)
 
 // Description:
 
-// Input: Array of elements
+// You are given an initial 2-value array (x). You will use this to calculate a score.
 
-// ["h","o","l","a"]
+// If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
 
-// Output: String with comma delimited elements of the array in th same order.
+// Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
 
-// "h,o,l,a"
+// For example:
 
-// Note: if this seems too simple for you try the next level
+// if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
 
 // Challenge code below:
 
-const printArray = array => array.join();
+function explode(x){ 
+  const result = [];
+  let length;
+  
+  if(typeof x[0] == "number" && typeof x[1] == "number") {
+    length = x[0] + x[1];
+  } else if(typeof x[0] == "number" || typeof x[1] == "number")  {
+    length = (typeof x[0] == 'number' ? x[0] : x[1]);    
+  } else {
+    return "Void!";
+  }
+  
+  for(let i = 0; i < length; i++) {
+    result.push(x);
+  }
+
+  return result;
+}
 
 // Tests
 
-// array1 = ["hello", "this", "is", "an", "array!"]
-// array2 = [1, 2, 3, 4, 5]
-
-console.log(printArray(["hello", "this", "is", "an", "array!"])); // hello,this,is,an,array!
-console.log(printArray([1, 2, 3, 4, 5])); // 1,2,3,4,5
+console.log(explode([9, 3])); // [[9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3]]
+console.log(explode(['a', 3])); // [['a', 3], ['a', 3], ['a', 3]]
+console.log(explode([6, 'c'])); // [[6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c']]
+console.log(explode(['a', 'b'])); // 'Void!'
+console.log(explode([1, 0])); // [[1,0]] 
+console.log(explode(["a", 0])); // []
