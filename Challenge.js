903edf7eea33d3214bff643d0f7e1ff45404f27f
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-08-19 14:40:04 -0500

    Challenge: NATO Phonetic Alphabet (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,44 @@
-// Challenge: Array Array Array (7 kyu)
+// Challenge: NATO Phonetic Alphabet (7 kyu)
 
 // Description:
 
-// You are given an initial 2-value array (x). You will use this to calculate a score.
+// Complete the function word (string) and returns a string that spells the word using the NATO phonetic alphabet.
 
-// If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+// There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
 
-// Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+// For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
 
-// For example:
-
-// if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
+// Examples
+// "hi"      -->  "Hotel India"
+// "abc"     -->  "Alpha Bravo Charlie"
+// "babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
+// "Banana"  -->  "Bravo Alpha November Alpha November Alpha"
 
 // Challenge code below:
 
-const explode = ([x, y]) => x + 0 != x && y + 0 != y ? 'Void!' : Array((+x || 0) + (+y || 0)).fill([x, y]);
+const nato = (function() {
+  let letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
 
-// Notes on logic:
-
-// x and y are each tested to see if they are numbers by adding a 0 to each. If they are in fact numbers, this will not change the value of the variable.
-// If neither of them are numbers, 'Void!' is returned as required by the challenge instructions.
-// Otherwise, the Array constructor is used to initialize an array of x + y length. If x or y is not a number, 0 wil be used instead.
-// The end result is that an array with the appropriate length is created, and then filled with the original array that was passed in.
+  return function(word) {
+    return word.split('').map(function(letter){
+      return letters[letter.toUpperCase()]
+    }).join(' ');
+  }
+})();
 
 // Tests
 
-console.log(explode([9, 3])); // [[9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3], [9, 3]]
-console.log(explode(['a', 3])); // [['a', 3], ['a', 3], ['a', 3]]
-console.log(explode([6, 'c'])); // [[6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c'], [6, 'c']]
-console.log(explode(['a', 'b'])); // 'Void!'
-console.log(explode([1, 0])); // [[1,0]] 
-console.log(explode(["a", 0])); // []
+console.log(nato('hi')); // 'Hotel India'
+console.log(nato('abc')); // 'Alpha Bravo Charlie'
+console.log(nato('babble')); // 'Bravo Alpha Bravo Bravo Lima Echo'
+console.log(nato('Banana')); // 'Bravo Alpha November Alpha November Alpha'
