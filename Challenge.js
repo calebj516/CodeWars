95a691a033a18aa3e9c2424d5ea233613477f952
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-08-28 22:23:54 -0500

    Challenge: Valid Parenthesis (7 kyu)

--- Challenge.js
+++ Challenge.js
@@ -1,34 +1,49 @@
-// Challenge: Converting 12-hour time to 24-hour time (7 kyu)
+// Challenge: Valid Parenthesis (7 kyu)
 
 // Description:
 
-// Converting a 12-hour time like "8:30 am" or "8:30 pm" to 24-hour time (like "0830" or "2030") sounds easy enough, right? Well, let's see if you can do it!
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
-// You will have to define a function, which will be given an hour (always in the range of 1 to 12, inclusive), a minute (always in the range of 0 to 59, inclusive), and a period (either "am" or "pm") as input.
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= length of input <= 100
 
-// Your task is to return a four-digit string that encodes that time in 24-hour time.
+// All inputs will be strings, consisting only of characters ( and ).
+// Empty strings are considered balanced (and therefore valid), and will be tested.
+// For languages with mutable strings, the inputs should not be mutated.
 
-// Notes
-// By convention, noon is 12:00 pm, and midnight is 12:00 am.
-// On 12-hours clock, there is no 0 hour, and time just after midnight is denoted as, for example, 12:15 am. On 24-hour clock, this translates to 0015.
+// Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
 
 // Challenge code below:
 
-const to24hourtime = (hour, minute, period) => {
-  if (hour == 12 && period == 'am') hour = 0;
-  if (hour != 12 && period == 'pm') hour += 12;
+function validParenthesis(parenStr) {
+  let counter = 0;
 
-  hour = hour.toString().padStart(2, 0);
-  minute = minute.toString().padStart(2, 0);
+  for(let paren of parenStr) {
+    paren == '(' ? counter++ : counter--;
+    if(counter < 0) return false;
+  }
 
-  return hour + minute;
+  return counter === 0;
 }
 
 // Tests
 
-console.log(to24hourtime( 1,  0, "am")); // "0100"
-console.log(to24hourtime( 1,  0, "pm")); // "1300"
-console.log(to24hourtime(12,  0, "am")); // "0000"
-console.log(to24hourtime(12,  0, "pm")); // "1200"
-console.log(to24hourtime( 6, 30, "am")); // "0630"
-console.log(to24hourtime( 9, 45, "pm")); // "2145"
+console.log(validParenthesis("()")); // true 
+console.log(validParenthesis("((()))")); // true
+console.log(validParenthesis("()()()")); // true
+console.log(validParenthesis("(()())()")); // true
+console.log(validParenthesis("()(())((()))(())()")); // true
+
+console.log(validParenthesis(")(")); // false
+console.log(validParenthesis("()()(")); // false
+console.log(validParenthesis("((())")); // false
+console.log(validParenthesis("())(()")); // false
+console.log(validParenthesis(")()")); // false
+console.log(validParenthesis(")")); // false
+
+console.log(validParenthesis("")); // true
