b214e4a1dd89eb16ab61a72fb301a4b19abc5e1a
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-09-03 12:39:14 -0500

    Challenge: Beginner Series #2 Clock (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,45 @@
-// Challenge: Beginner Series #1 School Paperwork (8 kyu)
-// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+// Challenge: Beginner Series #2 Clock (8 kyu)
+
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/javascript
 
 // Description:
 
-// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Clock shows h hours, m minutes and s seconds after midnight.
 
-// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+// Your task is to write a function which returns the time since midnight in milliseconds.
 
 // Example:
 
-// n= 5, m=5: 25
-// n=-5, m=5:  0
+// h = 0
+// m = 1
+// s = 1
 
-// Waiting for translations and Feedback! Thanks!
+// result = 61000
+
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
 
 // Challenge code below:
 
-const paperwork = (n, m) => n < 0 || m < 0 ? 0 : m * n;
+function past(h, m, s){
+
+  // 1000 x 60 x 60 x h = ms to m to h
+  let hoursToMs = h > 0 ? 3600000 * h : 0;
+  // 1000 x 60 x m = ms to m
+  let minToMs = m > 0 ? 60000 * m : 0;
+  // 1000 x s = ms to s
+  let secToMs = s > 0 ? 1000 * s : 0;
+  
+  return hoursToMs + minToMs + secToMs;
+}
 
 // Tests
 
-console.log(paperwork(5, 5)); // 25
-console.log(paperwork(5, -5)); // 0
-console.log(paperwork(-5, -5)); // 0
-console.log(paperwork(5, 0)); // 0
-console.log(paperwork(5, 11)); // 55
+console.log(past(0,1,1)); // 6100
+console.log(past(1,1,1)); // 3661000
+console.log(past(0,0,0)); // 0
+console.log(past(1,0,1)); // 3601000
+console.log(past(1,0,0)); // 3600000
