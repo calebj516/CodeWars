dd1eaecc7b516bd48831cb7a04c23d7c0e72680c
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-09-06 22:04:51 -0500

    Challenge: FIXME: Replace all dots (8 kyu) - Solution 2

--- Challenge.js
+++ Challenge.js
@@ -16,11 +16,11 @@
 
 // Challenge code below:
 
-var replaceDots = function(str) {
-  // added the \ to escape special characters
-  // added the g so that replace is run for all occurences in the string
-    return str.replace(/\./g, '-');
-  }
+const replaceDots = str => str.replace(/\./g, '-');
+
+// added the \ to escape special characters
+// added the g so that replace is run for all occurences in the string
+
 
 // Tests
 
