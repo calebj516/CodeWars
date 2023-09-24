1efbd788989d37b6718bf8087918a26147f8e1ce
Caleb Jones <calebj516@gmail.com>
2023-09-23 20:08:05 -0500

    Challenge: Get Planet Name By ID (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,44 +1,49 @@
-// Challenge: Find the first non-consecutive number (8 kyu)
-
-// Description:
-
-// Your task is to find the first element of an array that is not consecutive.
-
-// By not consecutive we mean not exactly 1 larger than the previous element of the array.
-
-// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
-
-// If the whole array is consecutive then return null2.
-
-// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
-
-// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
-
-// 1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
-
-// 2
-// Swift, Ruby and Crystal: nil
-// Haskell: Nothing
-// Python, Rust, Scala: None
-// Julia: nothing
-// Nim: none(int) (See options)
+// Challenge: Get Planet Name By ID (8 kyu)
 
 // Challenge code below:
 
-function firstNonConsecutive (arr) {
-  
-  for(let i = 0; i < arr.length - 1; i++) {
-    if(arr[i] !== arr[i + 1] - 1) {
-      return arr[i + 1];
-    }
+function getPlanetName(id){
+  let name;
+  switch(id){
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+      break;
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+      break;
+    default:
+      name = 'Invalid ID entry! Please try again.';
   }
   
-  return null;
+  return name;
 }
 
 // Tests
 
-console.log(firstNonConsecutive([1,2,3,4,6,7,8])); // 6
-console.log(firstNonConsecutive([-3,-2,0,1,2])); // 0
-console.log(firstNonConsecutive([-3,-1,0,1,2])); // -1
-console.log(firstNonConsecutive([1,2,3,4])); // null
+console.log(getPlanetName(1)); // Mercury
+console.log(getPlanetName(2)); // Venus
+console.log(getPlanetName(3)); // Earth
+console.log(getPlanetName(4)); // Mars
+console.log(getPlanetName(5)); // Jupiter
+console.log(getPlanetName(6)); // Saturn
+console.log(getPlanetName(7)); // Uranus
+console.log(getPlanetName(8)); // Neptune
+console.log(getPlanetName(9)); // Invalid ID entry! Please try again.
