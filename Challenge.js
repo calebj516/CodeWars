2ffc30d985e32600d4f16b7d7f31e8e894236981
Caleb Jones <calebj516@gmail.com>
2023-11-04 21:55:42 -0500

    Challenge: Merging sorted integer arrays (without duplicates) (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,43 +1,15 @@
-// Challenge: Leap Years (7 kyu)
+// Challenge: Merging sorted integer arrays (without duplicates) (7 kyu)
 
-// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
-
-// Years divisible by 4 are leap years,
-// but years divisible by 100 are not leap years,
-// but years divisible by 400 are leap years.
-// Tested years are in range 1600 ≤ year ≤ 4000.
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
 
 // Challenge code below:
 
-function isLeapYear(year) {
-  return divisibleBy400(year) || divisibleBy4(year) && !divisibleBy100(year);
-}
-
-function divisibleBy4(year) {
-  return year % 4 == 0;
-}
-
-function divisibleBy400(year) {
-  return year % 400 == 0;
-}
-
-function divisibleBy100(year) {
-  return year % 100 == 0;
-}
+const mergeArrays = (a, b) => [...new Set([...a,...b].sort((a, b) => a - b))];
 
 // Tests
 
-console.log(isLeapYear(2020)); //true
-console.log(isLeapYear(1824)); //true
-console.log(isLeapYear(2152)); //true
-console.log(isLeapYear(1600)); //true
-console.log(isLeapYear(2000)); //true
-console.log(isLeapYear(4000)); //true
-console.log(isLeapYear(1800)); //false
-console.log(isLeapYear(1900)); //false
-console.log(isLeapYear(2100)); //false
-console.log(isLeapYear(2200)); //false
-console.log(isLeapYear(1821)); //false
-console.log(isLeapYear(1942)); //false
-console.log(isLeapYear(2113)); //false
-console.log(isLeapYear(2254)); //false
+console.log(mergeArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(mergeArrays([2, 4, 8], [2, 4, 6])); // [2, 4, 6, 8]
+console.log(mergeArrays([1, 2, 3], [])); // [1, 2, 3]
+console.log(mergeArrays([1, 1, 2], [])); // [1, 2]
+console.log(mergeArrays([], [])); // []
