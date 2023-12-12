7b438c0d43a60b590696b3fa97d524f143039fa8
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-12-12 10:23:36 -0600

    Challenge: Compare within margin (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,27 +1,55 @@
-// Challenge: Simple validation of a username with regex (8 kyu)
+// Challenge: Compare within margin (8 kyu)
 
 // Description:
 
-// Write a simple regex to validate a username. Allowed characters are:
+// Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
 
-// lowercase letters,
-// numbers,
-// underscore
-// Length should be between 4 and 16 characters (both included).
+// Please note the following:
+
+// When a is close to b, return 0.
+// For this challenge, a is considered "close to" b if margin is greater than or equal to the absolute distance between a and b.
+// Otherwise...
+
+// When a is less than b, return -1.
+
+// When a is greater than b, return 1.
+
+// If margin is not given, treat it as if it were zero.
+
+// Assume: margin >= 0
+
+// Tip: Some languages have a way to make parameters optional.
+
+// Example 1
+// If a = 3, b = 5, and margin = 3, then close_compare(a, b, margin) should return 0.
+
+// This is because a and b are no more than 3 numbers apart.
+
+// Example 2
+// If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin) should return -1.
+
+// This is because the distance between a and b is greater than 0, and a is less than b.
 
 // Challenge code below:
 
-const validateUsr = username => /^[a-z\d_]{4,16}$/.test(username);
+function closeCompare(a, b, margin = 0){
+    let absDistance = Math.abs(a - b);
+    
+    if(margin >= absDistance) return 0;  
+    if(a < b) return -1;
+    if(a > b) return 1;
+}
 
 // Tests
 
-console.log(validateUsr('asddsa')); // true
-console.log(validateUsr('a')); // false
-console.log(validateUsr('Hass')); // false
-console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas')); // false
-console.log(validateUsr('')); // false
-console.log(validateUsr('____')); // true
-console.log(validateUsr('012')); // false
-console.log(validateUsr('p1pp1')); // true
-console.log(validateUsr('asd43 34')); // false
-console.log(validateUsr('asd43_34')); // true
+console.log(closeCompare(4, 5)); //-1
+console.log(closeCompare(5, 5)); // 0
+console.log(closeCompare(6, 5)); // 1
+console.log(closeCompare(-4, -3)); //-1
+console.log(closeCompare(-5, -5)); // 0
+console.log(closeCompare(-5, -6)); // 1
+console.log(closeCompare(2, 5, 3)); // 0
+console.log(closeCompare(5, 5, 3)); // 0
+console.log(closeCompare(8, 5, 3)); // 0
+console.log(closeCompare(8.1, 5, 3)); // 1
+console.log(closeCompare(1.99, 5, 3)); // -1
