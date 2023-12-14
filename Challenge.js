17ebb6082e24ce332b831a770273696077d2b2c1
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-12-14 14:42:51 -0600

    Challenge: Slice the middle of a list backwards (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,30 +1,19 @@
-// Challenge: Grasshopper - Array Mean (8 kyu)
+// Challenge: Slice the middle of a list backwards (7 kyu)
 
 // Description:
 
-// Find Mean
-// Find the mean (average) of a list of numbers in an array.
-
-// Information
-// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
-
-// For an example list of 1, 3, 5, 7
-
-// 1. Add all of the numbers
-
-// 1+3+5+7 = 16
-// 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
-
-// 16/4 = 4
-// 3. The mean (or average) of this list is 4
+// Write a function that takes a list of at least four elements as an argument and returns a list of the middle two or three elements in reverse order.
 
 // Challenge code below:
 
-const findAverage = nums => nums.reduce((total, current) => total + current, 0) / nums.length;
+function reverseMiddle(array) {
+    let midIndex = Math.ceil(array.length / 2) - 1;
+    return (array.length % 2 ? array.slice(midIndex - 1, midIndex + 2): array.slice(midIndex, midIndex + 2)).reverse();
+}
 
 // Tests
 
-console.log(findAverage([1])); // 1
-console.log(findAverage([1, 3, 5, 7])); // 4
-console.log(findAverage([-10, -5, -5, 0])); // -5
-console.log(findAverage([0])); // 0
+console.log(reverseMiddle([1, 2, 3, 4])); // [3, 2]
+console.log(reverseMiddle([1, 2, 3, 4, 5])); // [4, 3, 2]
+console.log(reverseMiddle([1, 2, 3, 4, 5, 6])); // [4, 3]
+console.log(reverseMiddle([1, 2, 3, 4, 5, 6, 7])); // [5, 4, 3]
