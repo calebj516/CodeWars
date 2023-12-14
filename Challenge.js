41937ae3a97dd7c9c5be42cb2c25a385f3bd7dec
Caleb Jones <calebj516@gmail.com>
2023-12-13 21:28:27 -0600

    Challenge: Grasshopper - Array Mean (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,55 +1,30 @@
-// Challenge: Compare within margin (8 kyu)
+// Challenge: Grasshopper - Array Mean (8 kyu)
 
 // Description:
 
-// Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+// Find Mean
+// Find the mean (average) of a list of numbers in an array.
 
-// Please note the following:
+// Information
+// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
 
-// When a is close to b, return 0.
-// For this challenge, a is considered "close to" b if margin is greater than or equal to the absolute distance between a and b.
-// Otherwise...
+// For an example list of 1, 3, 5, 7
 
-// When a is less than b, return -1.
+// 1. Add all of the numbers
 
-// When a is greater than b, return 1.
+// 1+3+5+7 = 16
+// 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
 
-// If margin is not given, treat it as if it were zero.
-
-// Assume: margin >= 0
-
-// Tip: Some languages have a way to make parameters optional.
-
-// Example 1
-// If a = 3, b = 5, and margin = 3, then close_compare(a, b, margin) should return 0.
-
-// This is because a and b are no more than 3 numbers apart.
-
-// Example 2
-// If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin) should return -1.
-
-// This is because the distance between a and b is greater than 0, and a is less than b.
+// 16/4 = 4
+// 3. The mean (or average) of this list is 4
 
 // Challenge code below:
 
-function closeCompare(a, b, margin = 0){
-    let absDistance = Math.abs(a - b);
-    
-    if(margin >= absDistance) return 0;  
-    if(a < b) return -1;
-    if(a > b) return 1;
-}
+const findAverage = nums => nums.reduce((total, current) => total + current, 0) / nums.length;
 
 // Tests
 
-console.log(closeCompare(4, 5)); //-1
-console.log(closeCompare(5, 5)); // 0
-console.log(closeCompare(6, 5)); // 1
-console.log(closeCompare(-4, -3)); //-1
-console.log(closeCompare(-5, -5)); // 0
-console.log(closeCompare(-5, -6)); // 1
-console.log(closeCompare(2, 5, 3)); // 0
-console.log(closeCompare(5, 5, 3)); // 0
-console.log(closeCompare(8, 5, 3)); // 0
-console.log(closeCompare(8.1, 5, 3)); // 1
-console.log(closeCompare(1.99, 5, 3)); // -1
+console.log(findAverage([1])); // 1
+console.log(findAverage([1, 3, 5, 7])); // 4
+console.log(findAverage([-10, -5, -5, 0])); // -5
+console.log(findAverage([0])); // 0
