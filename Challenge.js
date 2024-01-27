1e0e9e23e6151ee017a590c23be750b34bcb48ea
Caleb Jones <calebj516@gmail.com>
2024-01-26 21:22:19 -0600

    Challenge: Pizza Payments (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,36 +1,32 @@
-// Challenge: Basic Math (Add or Subtract) - (7 kyu)
+// Challenge: Pizza Payments (7 kyu)
 
 // Description:
 
-// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+// Kate and Michael want to buy a pizza and share it. Depending on the price of the pizza, they are going to divide the costs:
 
-// Note: the input will not be empty.
-
-// Examples
-
-// "1plus2plus3plus4"  --> "10"
-// "1plus2plus3minus4" -->  "2"
+// If the pizza is less than €5,- Michael invites Kate, so Michael pays the full price.
+// Otherwise Kate will contribute 1/3 of the price, but no more than €10 (she's broke :-) and Michael pays the rest.
+// How much is Michael going to pay? Calculate the amount with two decimals, if necessary.
 
 // Challenge code below:
 
-const calculate = str => {
-    const nums = str.split(/plus|minus/);
-    const ops = str.match(/plus|minus/g);
-
-    let sum = Number(nums[0]);
-
-    for(let i = 0; i < ops.length; i++) {
-        if(ops[i] == 'plus') sum += Number(nums[i + 1]);
-        if(ops[i] == 'minus') sum -= Number(nums[i + 1]);
-    }
-
-    return sum.toString();
-}
+function michaelPays(costs) {
+  
+    if(costs < 5) return Math.round(100 * costs) / 100;
+    if((costs / 3) > 10) return Math.round(100 * (costs - 10)) / 100;
+    
+    return Math.round(100 * (costs - (costs / 3))) / 100;
+  }
 
 // Tests
 
-console.log(calculate("1plus2plus3plus4")); // '10'
-console.log(calculate('1minus2minus3minus4')); // '-8'
-console.log(calculate('1plus2plus3minus4')); // '2'
-console.log(calculate('1minus2plus3minus4')); // '-2'
-console.log(calculate('1plus2minus3plus4minus5')); // '-1'
+console.log(michaelPays(15)); // 10
+console.log(michaelPays(4)); // 4
+console.log(michaelPays(4.99)); // 4.99
+console.log(michaelPays(5)); // 3.33
+console.log(michaelPays(30)); // 20
+console.log(michaelPays(80)); // 70
+console.log(michaelPays(22)); // 14.67
+console.log(michaelPays(5.9181)); // 3.95
+console.log(michaelPays(28.789)); // 19.19
+console.log(michaelPays(4.325)); // 4.33
