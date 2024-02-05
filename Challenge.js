38366c0f4216abf7711eb6b8ebe357412415bed0
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-02-05 08:58:22 -0600

    Challenge: Array plus array (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,34 +1,20 @@
-// Challenge: Sum without highest and lowest number (8 kyu)
+// Challenge: Array plus array (8 kyu)
 
 // Description:
 
-// Sum all the numbers of a given array ( cq. list )); // except the highest and the lowest element ( by value, not by index! ).
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
 
-// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
-
-// Mind the input validation.
-
-// Example
-
-// { 6, 2, 1, 8, 10 } => 16
-// { 1, 1, 11, 2, 3 } => 6
-
-// Input validation
-
-// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+// P.S. Each array includes only integer numbers. Output is a number too.
 
 // Challenge code below:
 
-function sumArray(array) {  
-  return array && array.length > 2 ? array.reduce((total, current) => total + current, 0) - Math.max(...array) - Math.min(...array) : 0;
+function arrayPlusArray(arr1, arr2) {
+  return arr1.reduce((x, y) => x + y, 0) + arr2.reduce((x, y) => x + y, 0);
 }
+
 // Tests
 
-console.log(sumArray(null)); // 0
-console.log(sumArray([ ])); // 0
-console.log(sumArray([ 3 ])); // 0
-console.log(sumArray([ 3, 5 ])); // 0
-console.log(sumArray([ 6, 2, 1, 8, 10 ])); // 16
-console.log(sumArray([ 0, 1, 6, 10, 10 ])); // 17
-console.log(sumArray([ -6, -20, -1, -10, -12 ])); // -28
-console.log(sumArray([ -6, 20, -1, 10, -12 ])); // 3
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); // 21
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])); // -21
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6])); // 15
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600])); // 2100
