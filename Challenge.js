1591764db827766d55512a9be495be0b9a0053ea
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-02-13 08:43:38 -0600

    Challenge: Guess the Word: Count Matching Letters (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,40 +1,51 @@
-// Challenge: Flick Switch (8 kyu)
+// Challenge: Guess the Word: Count Matching Letters (7 kyu)
 
 // Description:
 
-// Create a function that always returns True/true for every item in a given list.
-// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+// Consider a game, wherein the player has to guess a target word. All the player knows is the length of the target word.
 
-// Examples
+// To help them in their goal, the game will accept guesses, and return the number of letters that are in the correct position.
 
-// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+// Write a method that, given the correct word and the player's guess, returns this number.
 
-// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+// For example, here's a possible thought process for someone trying to guess the word "dog":
 
-// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+// countCorrectCharacters("dog", "car"); //0 (No letters are in the correct position)
+// countCorrectCharacters("dog", "god"); //1 ("o")
+// countCorrectCharacters("dog", "cog"); //2 ("o" and "g")
+// countCorrectCharacters("dog", "cod"); //1 ("o")
+// countCorrectCharacters("dog", "bog"); //2 ("o" and "g")
+// countCorrectCharacters("dog", "dog"); //3 (Correct!)
+// The caller should ensure that the guessed word is always the same length as the correct word, but since it could cause problems if this were not the case, you need to check for this eventuality:
 
-// Notes
-
-// "flick" will always be given in lowercase.
-// A list may contain multiple flicks.
-// Switch the boolean value on the same element as the flick itself.
+// //Throw an error if the two parameters are of different lengths.
+// You may assume, however, that the two parameters will always be in the same case.
 
 // Challenge code below:
 
-const flickSwitch = arr => {
-    let status = true;
+function countCorrectCharacters(correctWord, guess){
 
-    return arr.map(el => el == 'flick' ? status = !status : status);
-}
+    if(correctWord.length != guess.length) throw new Error();
+
+    let count = 0;
+
+    for(let i = 0; i < correctWord.length; i++) {
+        if(correctWord[i] == guess[i]) count++;
+    }
+
+    return count;
+} 
 
 // Tests
 
-console.log(flickSwitch(["codewars", "flick", "code", "wars"])); // [true, false, false, false]
-console.log(flickSwitch(["flick", "chocolate", "adventure", "sunshine"])); // [false, false, false, false]
-console.log(flickSwitch(["bicycle", "jarmony", "flick", "sheep", "flick"])); // [true, true, false, false, true]
-console.log(flickSwitch(["flick", "flick", "flick", "flick", "flick"])); // [false, true, false, true, false]
-console.log(flickSwitch(["codewars", "chocolate", "adventure", "sunshine"])); // [true, true, true, true]
-console.log(flickSwitch(["john, smith, susan", "flick"])); // [true, false]
-console.log(flickSwitch(["bicycle"])); // [true]
-console.log(flickSwitch(["flick"])); // [false]
-console.log(flickSwitch([])); // []
+console.log(countCorrectCharacters("dog", "car")); // 0, "Expected 0"
+console.log(countCorrectCharacters("dog", "god")); // 1, "Expected 1"
+console.log(countCorrectCharacters("dog", "cog")); // 2, "Expected 2"
+console.log(countCorrectCharacters("dog", "cod")); // 1, "Expected 1"
+console.log(countCorrectCharacters("dog", "bog")); // 2, "Expected 2"
+console.log(countCorrectCharacters("dog", "dog")); // 3, "Expected 3"
+console.log(countCorrectCharacters("abcde", "abcde")); // 5, "Expected 5"
+console.log(countCorrectCharacters("same", "same")); // 4, "Expected 4"
+console.log(countCorrectCharacters("z", "z")); // 1, "Expected 1"
+console.log(countCorrectCharacters("len", "lengh")); // Error
+console.log(countCorrectCharacters("abcde", "dea")); // Error
