db8d085cb0b453bb88f483a89a35e69c72d200ef
Caleb Jones <calebj516@gmail.com>
2024-02-17 15:39:28 -0600

    Challenge: Gravity Flip (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,52 +1,38 @@
-// Challenge: Guess the Word: Count Matching Letters (7 kyu)
+// Challenge: Gravity Flip (8 kyu)
 
 // Description:
 
-// Consider a game, wherein the player has to guess a target word. All the player knows is the length of the target word.
+// If you've completed this kata already and want a bigger challenge, here's the 3D version
 
-// To help them in their goal, the game will accept guesses, and return the number of letters that are in the correct position.
+// Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
 
-// Write a method that, given the correct word and the player's guess, returns this number.
+// There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
 
-// For example, here's a possible thought process for someone trying to guess the word "dog":
+// +---+                                       +---+
+// |   |                                       |   |
+// +---+                                       +---+
+// +---++---+     +---+              +---++---++---+
+// |   ||   |     |   |   -->        |   ||   ||   |
+// +---++---+     +---+              +---++---++---+
+// +---++---++---++---+         +---++---++---++---+
+// |   ||   ||   ||   |         |   ||   ||   ||   |
+// +---++---++---++---+         +---++---++---++---+
+// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
 
-// countCorrectCharacters("dog", "car"); //0 (No letters are in the correct position)
-// countCorrectCharacters("dog", "god"); //1 ("o")
-// countCorrectCharacters("dog", "cog"); //2 ("o" and "g")
-// countCorrectCharacters("dog", "cod"); //1 ("o")
-// countCorrectCharacters("dog", "bog"); //2 ("o" and "g")
-// countCorrectCharacters("dog", "dog"); //3 (Correct!)
-
-// The caller should ensure that the guessed word is always the same length as the correct word, but since it could cause problems if this were not the case, you need to check for this eventuality:
-
-// //Throw an error if the two parameters are of different lengths.
-// You may assume, however, that the two parameters will always be in the same case.
+// Examples (input -> output:
+// * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+// * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
 
 // Challenge code below:
 
-function countCorrectCharacters(correctWord, guess){
-
-    if(correctWord.length != guess.length) throw new Error();
-
-    let count = 0, i = 0;
-
-    for(let word of correctWord) {
-      if(word == guess[i]) count++;
-      i++;
-    }
-
-    return count;
-}  
+const flip=(d, a)=>{
+  return d == 'R' ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a);
+}
 
 // Tests
 
-console.log(countCorrectCharacters("dog", "car")); // 0, "Expected 0"
-console.log(countCorrectCharacters("dog", "god")); // 1, "Expected 1"
-console.log(countCorrectCharacters("dog", "cog")); // 2, "Expected 2"
-console.log(countCorrectCharacters("dog", "cod")); // 1, "Expected 1"
-console.log(countCorrectCharacters("dog", "bog")); // 2, "Expected 2"
-console.log(countCorrectCharacters("dog", "dog")); // 3, "Expected 3"
-console.log(countCorrectCharacters("abcde", "abcde")); // 5, "Expected 5"
-console.log(countCorrectCharacters("same", "same")); // 4, "Expected 4"
-console.log(countCorrectCharacters("z", "z")); // 1, "Expected 1"
-console.log(countCorrectCharacters("len", "lengh")); // Error
+console.log(flip('R', [4, 5, 6, 7, 1])); // [1, 4, 5, 6, 7]
+console.log(flip('L', [3, 0, 9, 8, 1, 2])); // [9, 8, 3, 2, 1, 0]
+console.log(flip('L', [0, 0, 12, 0, 1])); // [12, 1, 0, 0, 0]
+console.log(flip('R', [13, 2, 4, 7, 93])); // [2, 4, 7, 13, 93]
+console.log(flip('R', [5, 4, 3, 2, 1])); // [1, 2, 3, 4, 5]
