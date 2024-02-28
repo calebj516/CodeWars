80da631ee1c99b4af11d8a65b4d3eb9005443644
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-02-28 14:40:54 -0600

    Challenge: Days in the year (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,18 +1,44 @@
-// Challenge: Easy logs (8 kyu)
+// Challenge: Days in the year (8 kyu)
 
 // Description:
 
-// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X
+// A variation of determining leap years, assuming only integers are used and years can be negative and positive.
+
+// Write a function which will return the days in the year and the year entered in a string. For example:
+
+// yearDays(2000) returns "2000 has 366 days"
+
+// There are a few assumptions we will accept the year 0, even though there is no year 0 in the Gregorian Calendar.
+
+// Also the basic rule for validating a leap year are as follows
+
+// Most years that can be divided evenly by 4 are leap years.
+
+// Exception: Century years are NOT leap years UNLESS they can be evenly divided by 400.
+
+// So the years 0, -64 and 2016 will return 366 days. Whilst 1974, -10 and 666 will return 365 days.
 
 // Challenge code below:
 
-const logs = (x, a, b) => Math.log(a*b) / Math.log(x);
+function yearDays(year){
+ 
+    let isLeapYear = false;
+    
+    if(year % 100 === 0) {
+      if(year % 400 === 0) isLeapYear = true;
+    } else if(year % 4 === 0) {
+      isLeapYear = true;
+    }
+    
+    return `${year} has ${isLeapYear ? 366 : 365} days`;
+  }
 
 // Tests
 
-console.log(logs(10, 2, 3)); // 0.7781512503836435
-console.log(logs(5, 2, 3)); // 1.1132827525593785
-console.log(logs(1000, 2, 3)); // 0.25938375012788123
-console.log(logs(2, 1, 2)); // 1
-console.log(logs(0.00001, 0.002, 0.01)); // 0.9397940008672038
-console.log(logs(0.1, 0.002, 0.01)); // 4.69897000433602
+console.log(yearDays(0)); // '0 has 366 days'
+console.log(yearDays(-64)); // '-64 has 366 days'
+console.log(yearDays(2016)); // '2016 has 366 days'
+console.log(yearDays(1974)); // '1974 has 365 days'
+console.log(yearDays(-10)); // '-10 has 365 days'
+console.log(yearDays(666)); // '666 has 365 days'
+console.log(yearDays(1857)); // '1857 has 365 days'
