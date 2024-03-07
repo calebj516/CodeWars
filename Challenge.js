7de26f3c8d288fd811051e2f154acf51c4a498f9
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-03-07 08:51:05 -0600

    Challenge: Grasshopper - Combine strings (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,44 +1,20 @@
-// Challenge: Fundamentals: Return (8 kyu)
+// Challenge: Grasshopper - Combine strings (8 kyu)
 
 // Description:
 
-// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
 
-// Please use the following function names:
+// Example:
 
-// addition = add
+// combineNames('James', 'Stevens')
+// returns:
 
-// multiply = multiply
-
-// division = divide (both integer and float divisions are accepted)
-
-// modulus = mod
-
-// exponential = exponent
-
-// subtraction = subt
-
-// Note: All math operations will be: a (operation) b
+// 'James Stevens'
 
 // Challenge code below:
 
-const add = (a,b) =>  a + b;
-
-const divide = (a,b) =>  a / b;
-
-const multiply = (a,b) =>  a * b;
-
-const mod = (a,b) =>  a % b;
-
-const exponent = (a,b) =>  a ** b;
-  
-const subt = (a,b) =>  a - b;
+const combineNames = (str1, str2) => str1.concat(' ', str2);
 
 // Tests
 
-console.log(add(1,2)); // 3
-console.log(multiply(1,2)); // 2
-console.log(divide(2,1)); // 2
-console.log(mod(1,2)); // 1
-console.log(exponent(1,2)); // 1
-console.log(subt(1,2)); // -1
+console.log(combineNames('James', 'Stevens')); //  'James Stevens'
