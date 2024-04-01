70c0a5885d948b7c42d588878b50b0a7d24aa8ea
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-04-01 10:26:49 -0500

    Challenge: Count the Monkeys! (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,18 +1,32 @@
-// Challenge: Is it a letter? (7 kyu)
+// Challenge: Count the Monkeys! (8 kyu)
 
 // Description:
 
-// Write a function, isItLetter or is_it_letter, which tells us if a given character is an, uppercase or lowercase, letter.
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n); // but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n); // populate an array with all numbers up to and including that number, but excluding zero.
+
+// For example(Input --> Output):
+
+// 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  1 --> [1]
 
 // Challenge code below:
 
-function isItLetter(character) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    return alphabet.includes(character.toLowerCase());
+function monkeyCount(n) {
+    const result = [];
+    
+    for(let i = 1; i <= n; i++) {
+      result.push(i);
+    }
+    
+    return result;
 }
 
 // Tests
 
-console.log( isItLetter('a')); // true
-console.log( isItLetter('A')); // true
-console.log( isItLetter('1')); // false
+console.log(monkeyCount(5)); // [1, 2, 3, 4, 5]
+console.log(monkeyCount(3)); // [1, 2, 3]
+console.log(monkeyCount(9)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(monkeyCount(10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(monkeyCount(20)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
