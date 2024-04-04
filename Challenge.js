345a7e6b7629fa2d2bd94c5aba7a6089923c9d52
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-04-04 08:29:18 -0500

    Challenge: Powers of 2 (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,26 +1,23 @@
-// Challenge: Count the Monkeys! (8 kyu)
+// Challenge: Powers of 2 (8 kyu)
 
 // Description:
 
-// You take your son to the forest to see the monkeys. You know that there are a certain number there (n); // but your son is too young to just appreciate the full number, he has to start counting them from 1.
-
-// As a good parent, you will sit and count with him. Given the number (n); // populate an array with all numbers up to and including that number, but excluding zero.
-
-// For example(Input --> Output):
-
-// 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//  1 --> [1]
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
 
 // Challenge code below:
 
-function monkeyCount(n) {
-  return Array.from({length : n}, (_, i) => i + 1);
+function powersOfTwo(n){
+  const nums = [];
+  
+  for(let i = 0; i <= n; i++) {
+    nums.push(2 ** i);
+  }
+  
+  return nums;
 }
 
 // Tests
 
-console.log(monkeyCount(5)); // [1, 2, 3, 4, 5]
-console.log(monkeyCount(3)); // [1, 2, 3]
-console.log(monkeyCount(9)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(monkeyCount(10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(monkeyCount(20)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+console.log(powersOfTwo(0)); // [1]
+console.log(powersOfTwo(1)); // [1, 2]
+console.log(powersOfTwo(4)); // [1, 2, 4, 8, 16]
