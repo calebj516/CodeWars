37eabc80a78b6965849c815ad6355b016feedaf1
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-04-23 15:12:43 -0500

    Challenge: Count Odd Numbers below n (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,15 +1,20 @@
-// Challenge: Remove exclamation marks (8 kyu)
+// Challenge: Count Odd Numbers below n (8 kyu)
 
 // Description:
 
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 
 // Challenge code below:
 
-const removeExclamationMarks = s => s.split('!').join('');
+function oddCount(n){
+    return Math.floor(n / 2);
+}
 
 // Tests
 
-console.log(removeExclamationMarks("Hello World!")); // "Hello World"
-console.log(removeExclamationMarks("Hello World!!!")); // "Hello World"
-console.log(removeExclamationMarks("Hi! Hello!")); // "Hi Hello"
+console.log(oddCount(15)); // 7
+console.log(oddCount(15023)); // 7511
