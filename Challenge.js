4734ef4a972885d4874738a1cb65d994b83509fd
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-04-25 08:40:19 -0500

    Challenge: Well of Ideas - Easy Version (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,20 +1,21 @@
-// Challenge: Count Odd Numbers below n (8 kyu)
+// Challenge: Well of Ideas - Easy Version (8 kyu)
 
 // Description:
 
-// Given a number n, return the number of positive odd numbers below n, EASY!
+// For every good kata idea there seem to be quite a few bad ones!
 
-// Examples (Input -> Output)
-// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
-// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
+// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
+// If there are no good ideas, as is often the case, return 'Fail!'.
 
 // Challenge code below:
 
-function oddCount(n){
-    return Math.floor(n / 2);
+function well(x){
+    let count = x.filter(el => el == 'good').length;
+    return count > 2 ? 'I smell a series!' : count > 0 ? 'Publish!' : 'Fail!';
 }
-
 // Tests
 
-console.log(oddCount(15)); // 7
-console.log(oddCount(15023)); // 7511
+console.log(well(['bad', 'bad', 'bad'])); // 'Fail!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad'])); // 'Publish!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])); // 'I smell a series!'
