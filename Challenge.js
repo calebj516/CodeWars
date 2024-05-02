760af2131d5b4bfcbc1a1d4ae9de7f8f6c082c8c
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-05-02 08:41:05 -0500

    Challenge: Collinearity (8 kyu) - Solution 2

--- Challenge.js
+++ Challenge.js
@@ -21,7 +21,7 @@
 // Challenge code below:
 
 function collinearity( x1,y1, x2,y2 ) {
-  return ((x1 == 0 && y1 == 0) || (x2 == 0 && y2 == 0)) || ((x2 == 0 && x1 == 0) || (y2 == 0 && y1 == 0)) || (x2 / x1) == (y2 / y1);
+  return x1 * y2 == x2 * y1;
 }
 
 // Tests
