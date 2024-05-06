f788b50e65cf1956ef6e9f5a4f5db46a2bf52169
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-05-06 08:53:20 -0500

    Challenge: Evens times last (7 kyu) - Solution 2

--- Challenge.js
+++ Challenge.js
@@ -11,7 +11,15 @@
 // Challenge code below:
 
 function evenLast(numbers) {
-  return (numbers[numbers.length - 1] || 0) * numbers.reduce((total, current, i) => i % 2 == 0 ? total + current : total + 0, 0);
+  if(numbers.length === 0) return 0;
+  
+  let sum = 0;
+  
+  for(let i = 0; i < numbers.length; i += 2) {
+    sum += numbers[i];
+  }
+  
+  return sum * numbers[numbers.length - 1];
 }
 
 // Tests
