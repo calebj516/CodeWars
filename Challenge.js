c7c5a764955e95d23a514be66fe5dd2bfd60b211
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-05-28 17:05:08 -0500

    Challenge: Sort an array by value and index (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,33 +1,43 @@
-// Challenge: Even numbers in an array (7 kyu)
+// Challenge: Sort an array by value and index (7 kyu)
 
 // Description:
 
-// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
 
-// For example:
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
 
-// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
-// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
-// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+//  2 =>  2 * 2 = 4   -> Output-Pos 1
+//  3 =>  3 * 3 = 9   -> Output-Pos 2
+//  4 =>  4 * 4 = 16  -> Output-Pos 3
+//  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
 
 // Challenge code below:
 
-function evenNumbers(array, number) {
-    const result = [];
-    
-    for(let i = array.length - 1; i >= 0; i--) {
-      if(array[i] % 2 === 0) {
-        result.push(array[i]);
-        number--;
-        if(number === 0) break;
-      }
-    }
-    
-    return result.reverse();
+function sortByValueAndIndex(array) {
+  return array
+		.map((x, i) => [x, x * i + x])
+		.sort((a, b) => a[1] - b[1])
+		.map((a) => a[0])
 }
 
 // Tests
  
-console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // [4, 6, 8]
-console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)); // [-8, 26]
-console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1)); // [6]
+console.log(sortByValueAndIndex([ 1, 2, 3, 4, 5 ])); // [ 1, 2, 3, 4, 5 ]
+console.log(sortByValueAndIndex([ 23, 2, 3, 4, 5 ])); // [ 2, 3, 4, 23, 5 ]
+console.log(sortByValueAndIndex([ 26, 2, 3, 4, 5 ])); // [ 2, 3, 4, 5, 26 ]
+console.log(sortByValueAndIndex([ 9, 5, 1, 4, 3 ])); // [ 1, 9, 5, 3, 4 ] 
