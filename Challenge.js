e603eae337d455ef0091041a619a0bf7c07431ec
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-06-11 08:55:53 -0500

    Challenge: Transportation on vacation (8 kyu) - Solution 2

--- Challenge.js
+++ Challenge.js
@@ -13,11 +13,8 @@
 // Challenge code below:
 
 function rentalCarCost(d) {
-	if(d >= 7) {
-	  return 40 * d - 50;
-	} else if(d >= 3) {
-	  return 40 * d - 20;
-	}
+	if(d >= 7) return 40 * d - 50;
+	if(d >= 3) return 40 * d - 20;
 	
 	return d * 40;
 }
