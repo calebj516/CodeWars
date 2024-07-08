70b3b080c49e1d99da6dc0176c9638ea6ea43e01
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-07-08 10:05:18 -0500

    Challenge: How many are smaller than me? (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,28 +1,41 @@
-// Challenge: Parse nice int from char problem (8 kyu)
+// Challenge: How many are smaller than me? (7 kyu)
 
 // Description:
 
-// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+// Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
 
-// Write a program that returns the girl's age (0-9) as an integer.
+// For example:
 
-// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+// * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+// * Input [1, 2, 0] => Output [1, 1, 0]
+
+// If you've completed this one and you feel like testing your performance tuning of this same kata, head over to the much tougher version How many are smaller than me II?
 
 // Challenge code below:
 
-function getAge(inputString){
-  return +inputString[0];
+function smaller(nums) {
+  
+  const results = [];
+  let count = 0;
+  
+  for(let i = 0; i < nums.length; i++) {
+    
+    for(let j = i + 1; j < nums.length; j++) {
+      if(nums[i] > nums[j]) count++;
+    }
+    
+    results.push(count);
+    count = 0;
+  }
+  
+  return results;
 }
 
 // Tests
 
-console.log(getAge("4 years old")); // 4
-console.log(getAge("5 years old")); // 5
-console.log(getAge("7 years old")); // 7
-console.log(getAge("6 years old")); // 6
-console.log(getAge("0 years old")); // 0
-console.log(getAge("8 years old")); // 8
-console.log(getAge("9 years old")); // 9
-console.log(getAge("1 year old")); // 1
-console.log(getAge("2 years old")); // 2
-console.log(getAge("3 years old")); // 3
+console.log(smaller([5, 4, 3, 2, 1])); // [4, 3, 2, 1, 0]
+console.log(smaller([1, 2, 3])); // [0, 0, 0]
+console.log(smaller([1, 2, 0])); // [1, 1, 0]
+console.log(smaller([1, 2, 1])); // [0, 1, 0]
+console.log(smaller([1, 1, -1, 0, 0])); // [3, 3, 0, 0, 0]
+console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6])); // [4, 1, 5, 5, 0, 0, 0, 0, 0]
