bc7021202fb3a733256f770bedd79d0114f2d2a8
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-07-22 09:00:44 -0500

    Challenge: Finding length of the sequence (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,23 +1,31 @@
-// Challenge: Nothing special (7 kyu)
+// Challenge: Finding length of the sequence (7 kyu)
 
 // Description:
 
-// The notorious Captain Schneider has given you a very straightforward mission. Any data that comes through the system make sure that only non-special characters see the light of day.
+// As part of this kata, you need to find the length of the sub-array that begins and ends with the specified number.
 
-// Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
+// For example, if the array arr is [0, -3, 7, 4, 0, 3, 7, 9], finding the length of the sub-array that begins and ends with 7s would return 5.
+
+// For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+
+// If there are less or more than two occurrences of the number to search for, return 0.
 
 // Challenge code below:
 
-function nothingSpecial(str) {
-  if(typeof str != "string") return "Not a string!";  
-  return str.replace(/[^A-Za-z\d\s]/g, '');
-}
+var lengthOfSequence = function (arr, n) {  
+  let x = arr.slice(arr.indexOf(n), arr.lastIndexOf(n) + 1);  
+  if(x.filter(num => num == n).length != 2) return 0;
+  
+  return x.length; 
+};
 
 // Tests
 
-console.log(nothingSpecial('St@arboard P^ort')); // 'Starboard Port'
-console.log(nothingSpecial('$A$ ***little k@$ata fo&r ^my ^trou%bl$es')); // 'A little kata for my troubles'
-console.log(nothingSpecial('Steamroller')); // 'Steamroller'
-console.log(nothingSpecial(2.55)); // 'Not a string!'
-console.log(nothingSpecial({ 2: 5 })); // 'Not a string!'
-console.log(nothingSpecial([1, 2, 3, 4])); // 'Not a string!'
+console.log(lengthOfSequence([1], 0)); // 0, 'Returns zero when the element is not found in the array'
+console.log(lengthOfSequence([1], 1)); // 0, 'Returns zero with only one element in the array'
+console.log(lengthOfSequence([1, 1], 1)); // 2, 'Returns two when there are only two elements in the array'
+console.log(lengthOfSequence([1, 2, 3, 1], 1)); // 4, 'Returns four for an array of length four and the number to be searched at the boundaries'
+console.log(lengthOfSequence([-7, 5, 0, 2, 9, 5], 5)); // 5, 'Returns five'
+console.log(lengthOfSequence([-7, 6, 2, -7, 4], -7)); // 4, 'Returns four'
+console.log(lengthOfSequence([0, 8, -7, 6, 1, 2, -7, 4, 8, 9], 8)); // 8, 'Returns eight'
+console.log(lengthOfSequence([-7, 3, -7, -7, 2, 1], -7)); // 0, 'Returns zero as when there are more than two instances'
