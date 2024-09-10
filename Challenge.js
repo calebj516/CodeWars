072e9078e367150dbe4561dee43fd38c2f52a758
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-09-10 08:36:48 -0500

    Challenge: Grader (8 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,26 +1,58 @@
-// Challenge: Reversing Words in a String (8 kyu)
+// Challenge: Grader (8 kyu)
 
 // Description:
 
-// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+// Create a function that takes a number as an argument and returns a grade based on that number.
 
-// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+// Score	Grade
 
-// Example (Input --> Output)
+// Anything greater than 1 or less than 0.6	"F"
+// 0.9 or greater	"A"
+// 0.8 or greater	"B"
+// 0.7 or greater	"C"
+// 0.6 or greater	"D"
 
-// "Hello World" --> "World Hello"
-// "Hi There." --> "There. Hi"
+// Examples:
+
+// grader(0)   should be "F"
+// grader(1.1) should be "F"
+// grader(0.9) should be "A"
+// grader(0.8) should be "B"
+// grader(0.7) should be "C"
+// grader(0.6) should be "D"
 
 // Challenge code below:
 
-function reverse(string){
-    return string.split(' ').reverse().join(' ');
+function grader(score) {
+    switch(true) {
+        case score > 1 || score < 0.6:
+          return "F";
+          break;
+        case score >= 0.9:
+          return "A";
+          break;
+        case score >= 0.8:
+          return "B";
+          break;
+        case score >= 0.7:
+          return "C";
+          break;
+        case score >= 0.6:
+          return "D";
+          break;
+        default:
+          return "Invalid input. Please try again.";
+    }
 }
 
 // Tests
 
-console.log(reverse('I am an expert at this')); // 'this at expert an am I'
-console.log(reverse('This is so easy')); // 'easy so is This'
-console.log(reverse('no one cares')); // 'cares one no'
-console.log(reverse('')); // ''
-console.log(reverse('CodeWars')); // 'CodeWars'
+console.log(grader(1)); // "A"
+console.log(grader(1.01)); // "F"
+console.log(grader(0.2)); // "F"
+console.log(grader(0.7)); // "C"
+console.log(grader(0.8)); // "B"
+console.log(grader(0.9)); // "A"
+console.log(grader(0.6)); // "D"
+console.log(grader(0.5)); // "F"
+console.log(grader(0)); // "F"
