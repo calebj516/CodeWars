3df5ba1d92267dd60223046d8730013384f6c452
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2024-09-12 13:51:23 -0500

    Challenge: Some (but not all) (7 kyu) - Solution 1

--- Challenge.js
+++ Challenge.js
@@ -1,58 +1,28 @@
-// Challenge: Grader (8 kyu)
+// Challenge: Some (but not all) (7 kyu)
 
 // Description:
 
-// Create a function that takes a number as an argument and returns a grade based on that number.
+// Your task is to create a function that given a sequence and a predicate, returns True if only some (but not all) the elements in the sequence are True after applying the predicate
 
-// Score	Grade
+// Examples
 
-// Anything greater than 1 or less than 0.6	"F"
-// 0.9 or greater	"A"
-// 0.8 or greater	"B"
-// 0.7 or greater	"C"
-// 0.6 or greater	"D"
+// ('abcdefg&%$', x -> isLetter(x)) == true
+// ('&%$=', x -> isLetter x) == false
+// ('abcdefg', x -> isLetter x) == false
 
-// Examples:
-
-// grader(0)   should be "F"
-// grader(1.1) should be "F"
-// grader(0.9) should be "A"
-// grader(0.8) should be "B"
-// grader(0.7) should be "C"
-// grader(0.6) should be "D"
+// ([4, 1], x -> x > 3) == true
+// ([1, 1], x -> x > 3) == false
+// ([4, 4], x -> x > 3) == false
 
 // Challenge code below:
 
-function grader(score) {
-    switch(true) {
-        case score > 1 || score < 0.6:
-          return "F";
-          break;
-        case score >= 0.9:
-          return "A";
-          break;
-        case score >= 0.8:
-          return "B";
-          break;
-        case score >= 0.7:
-          return "C";
-          break;
-        case score >= 0.6:
-          return "D";
-          break;
-        default:
-          return "Invalid input. Please try again.";
-    }
-}
+const someButNotAll = ([...seq], pred) => seq.some(pred) && !seq.every(pred);
 
 // Tests
 
-console.log(grader(1)); // "A"
-console.log(grader(1.01)); // "F"
-console.log(grader(0.2)); // "F"
-console.log(grader(0.7)); // "C"
-console.log(grader(0.8)); // "B"
-console.log(grader(0.9)); // "A"
-console.log(grader(0.6)); // "D"
-console.log(grader(0.5)); // "F"
-console.log(grader(0)); // "F"
+console.log(someButNotAll('abcdefg&%$', str => /[a-z]/i.test(str))); //true
+console.log(someButNotAll('&%$=', str => /[a-z]/i.test(str))); //false
+console.log(someButNotAll('abcdefg', str => /[a-z]/i.test(str))); // false
+console.log(someButNotAll([4, 1], x => x>3)); // true
+console.log(someButNotAll([1, 1], x => x>3)); // false
+console.log(someButNotAll([4, 4], x => x>3)); // false
