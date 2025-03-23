ea8dd3ddc3d611bae10bcc11f56efe6a3c68ba43
Caleb Jones <cjo3774@wgu.edu>
2025-03-22 22:04:51 -0500

    Quarter of the year (8 kyu) - Python

---
+++ Challenge.py
@@ -0,0 +1,18 @@
+# Quarter of the year
+ 
+# Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+ 
+# For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+def quarterOf(month):
+    if month > 9:
+        return 4
+    elif month > 6:
+        return 3
+    elif month > 3:
+        return 2
+    return 1
+
+print(quarterOf(3)) # 1
+print(quarterOf(8)) # 3
+print(quarterOf(11)) # 4
